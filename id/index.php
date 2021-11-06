ed248f7123680ebc5b4d009be8762042232112d2
Boga Sebastian Nicolae <sebastian.boga@endava.com>
2021-11-06 20:00:35 +0000

    Add files via upload

---
+++ id/index.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$url = 'http://zimbor.go.ro/solr/romania/select?q.op=OR&q=*%3A*&omitHeader=true';
+$response = new stdClass();
+if(isset($_GET['localitate']))  {
+	  $localitate = $_GET['localitate'];
+	  $url .= "&fq=localitate%3A".$localitate; 
+								}
+	  
+if(isset($_GET['judet'])) 		{
+		$judet = $_GET['judet'];
+		$url .= "&fq=judet%3A".$judet;
+
+		$string = file_get_contents($url);
+		$json = json_decode($string, true);
+		
+		$response -> id = $json["response"]["docs"][0]["id"];
+		                    } else {
+    $response -> error = "not enough info";
+
+							}
+
+	
+
+
+
+echo json_encode($response);
+
+?>
