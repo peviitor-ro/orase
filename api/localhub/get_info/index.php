91a79bc8421d7dcb9db828e5f14e033adf0505af
Sebastian Boga <sebastian.boga@outlook.com>
2021-11-07 12:19:37 +0000

    adding local hub get info

---
+++ api/localhub/get_info/index.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$url = 'http://zimbor.go.ro/solr/romania/select?q=*%3A*&q.op=OR&omitHeader=true';
+
+if(isset($_GET['id']))  {
+	  $localitate = $_GET['id'];
+	  $url .= '&fq=localitate:"'.$localitate.'"'; 
+	  $string = file_get_contents($url);
+      echo $string;
+
+	                    } else {
+	   $response = new stdClass();
+       $response -> error = "not enough info";
+	   echo json_encode($response);
+							}
+
+
+?>
