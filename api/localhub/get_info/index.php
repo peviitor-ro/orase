c81a80a2507adfe9e673661707df14b5218d5297
Sebastian Boga <sebastian.boga@outlook.com>
2021-11-07 12:45:50 +0000

    moving to localhub core

--- api/localhub/get_info/index.php
+++ api/localhub/get_info/index.php
@@ -1,18 +1,19 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$url = 'http://zimbor.go.ro/solr/romania/select?q=*%3A*&q.op=OR&omitHeader=true';
+$url = 'http://zimbor.go.ro/solr/localhub/select?q=*%3A*&q.op=OR&omitHeader=true';
 
 if(isset($_GET['id']))  {
 	  $localitate = $_GET['id'];
 	  $url .= '&fq=location_id:"'.$localitate.'"'; 
 	  $string = file_get_contents($url);
       echo $string;
-      var_dump($url);
+      
 	                    } else {
 	   $response = new stdClass();
        $response -> error = "not enough info";
 	   echo json_encode($response);
+	   var_dump($url);
 							}
 
 
