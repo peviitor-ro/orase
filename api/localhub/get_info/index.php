19e9e5ffc286022d6382dd22104f63a633426886
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2021-11-07 18:24:53 +0200

    adding urlencoder

--- api/localhub/get_info/index.php
+++ api/localhub/get_info/index.php
@@ -5,14 +5,16 @@ $url = 'http://zimbor.go.ro/solr/localhub/select?q=*%3A*&q.op=OR&omitHeader=true
 
 if(isset($_GET['id']))  {
 	  $localitate = $_GET['id'];
-	  $url .= '&fq=location_id:'.$localitate; 
+	  $url .= '&fq=location_id:'.urlencode($localitate); 
 	  $string = file_get_contents($url);
       echo $string;
-      var_dump($string);
+     
 	                    } else {
 	   $response = new stdClass();
        $response -> error = "not enough info";
 	   echo json_encode($response);
+
+	   var_dump($string);
 	 
 							}
 
