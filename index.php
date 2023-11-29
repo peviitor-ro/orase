2cfb2a42a9fe4a54a981032f72dc0874f32d9d9e
DianaDascalu2 <118655267+DianaDascalu2@users.noreply.github.com>
2023-11-29 17:34:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1113,6 +1113,33 @@ $tara = createTara(
 					createLoc("Stârci", "sat"),
 				]),
 			]),
+			createOras("MIOVENI", [
+			    createLoc("MIOVENI", "oras",[
+					createLoc("Clucereasa", "sat"),
+					createLoc("Colibași", "sat"),
+					createLoc("Făgetu", "sat"),
+					createLoc("Racovița", "sat"),
+				]),
+			]),
+			createOras("ȘTEFĂNEȘTI", [
+			    createLoc("ȘTEFĂNEȘTI", "oras",[
+					createLoc("Enculești", "sat"),
+					createLoc("Golești", "sat"),
+					createLoc("Izvorani", "sat"),
+					createLoc("Ștefăneștii Noi", "sat"),
+					createLoc("Valea Mare-Podgoria", "sat"),
+					createLoc("Viișoara", "sat"),
+					createLoc("Zăvoi", "sat"),
+				]),
+			]),
+			createOras("TOPOLOVENI", [
+			    createLoc("TOPOLOVENI", "oras",[
+					createLoc("Boțârcani", "sat"),
+					createLoc("Crintești", "sat"),
+					createLoc("Gorănești", "sat"),
+					createLoc("Țigănești", "sat"),
+				]),
+			]),
 		]),
 		createJudet("BACĂU"),
 		createJudet("BIHOR"),
