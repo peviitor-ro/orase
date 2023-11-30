335cbfc6a02d71e892ba9864a3809237a097fe48
DianaDascalu2 <118655267+DianaDascalu2@users.noreply.github.com>
2023-11-30 14:36:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1218,7 +1218,51 @@ $tara = createTara(
 					createLoc("Valea Ciuciului", "sat"),						
 								  ]),								  
 			]),
-		createJudet("ARAD"),
+		createJudet("ARAD", [
+			createMunicipiu("ARAD"),	
+		],
+	    [ 
+		createOras("CHIȘINEU-CRIȘ",[
+			createLoc("CHIȘINEU-CRIȘ", "oras"),
+			createLoc("Nădab", "sat"),
+						   ]),
+		createOras("CURTICI",[
+			createLoc("CURTICI", "oras"),
+						   ]),
+		createOras("INEU",[
+			createLoc("INEU", "oras"),
+			createLoc("Mocrea", "sat"),
+						   ]),
+		createOras("LIPOVA",[
+			createLoc("LIPOVA", "oras"),
+			createLoc("Radna", "oras"),
+			createLoc("Șoimoș", "oras"),
+						   ]),
+		createOras("NĂDLAC",[
+			createLoc("NĂDLAC", "oras"),
+						   ]),
+		createOras("PECICA",[
+			createLoc("PECICA", "oras"),
+			createLoc("Bodrogu Vechi", "sat"),
+			createLoc("Sederhat", "sat"),
+			createLoc("Turnu", "sat"),
+						   ]),
+		createOras("PÂNCOTA",[
+			createLoc("PÂNCOTA", "oras"),
+			createLoc("Măderat", "sat"),
+						   ]),
+		createOras("SÂNTANA",[
+			createLoc("SÂNTANA", "oras"),
+			createLoc("Caporal Alexa", "sat"),
+						   ]),
+		createOras("SEBIȘ",[
+			createLoc("SEBIȘ", "oras"),
+			createLoc("Donceni", "sat"),
+			createLoc("Prunișor", "sat"),
+			createLoc("Sălăjeni", "sat"),
+						   ]),
+	    ]
+	),
 		createJudet("ARGEȘ"),
 		createJudet("BACĂU"),
 		createJudet("BIHOR"),
