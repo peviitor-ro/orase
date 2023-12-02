2c96036cd76bc16e8c48d1eb935095b9239ca4bf
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-02 14:40:35 +0200

    Update braila.php

--- ROMANIA/BRAILA/braila.php
+++ ROMANIA/BRAILA/braila.php
@@ -4,9 +4,9 @@ require_once 'municipii.php';
 require_once 'orase.php';
 require_once 'comune.php';
 
-$brasov =
+$braila =
 createJudet(
-            "BRAȘOV",$municipii,$orase,$comune
+            "BRĂILA",$municipii,$orase,$comune
             );
 			
 ?>
