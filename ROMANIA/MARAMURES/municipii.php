63f7bcb71c66f5c17e02d8f7f36142a6f5995f40
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-02 18:19:20 +0200

    Update municipii.php

--- ROMANIA/MARAMURES/municipii.php
+++ ROMANIA/MARAMURES/municipii.php
@@ -1,7 +1,20 @@
 <?php
 $municipii =
 [
-			
+            createMunicipiu("BAIA MARE", [
+                    createLoc("BAIA MARE", "oras"),
+                    createLoc("Blidari", "sat"),
+                    createLoc("Firiza", "sat"),
+                    createLoc("Valea Neagră", "sat"),
+                ]),
+            createMunicipiu("SIGHETU MARMAȚIEI", [
+                    createLoc("SIGHETU MARMAȚIEI", "oras"),
+                    createLoc("Iapa", "sat"),
+                    createLoc("Lazu Baciului", "sat"),
+                    createLoc("Șugău", "sat"),
+					createLoc("Valea Cufundoasă", "sat"),
+                    createLoc("Valea Hotarului", "sat"),
+                ]),				
 			
 ];
 ?>
