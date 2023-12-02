bdef9832164c0ac6c545808a810926c7f8f70f76
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-02 13:09:04 +0200

    updates ARGES

--- ROMANIA/ARGES/municipiiarges.php
+++ ROMANIA/ARGES/municipiiarges.php
@@ -1,6 +1,17 @@
 <?php
 $municipii =
 [
-
+			createMunicipiu("PITEȘTI", [
+				createLoc("PITEȘTI", "oras"),
+			]),
+			createMunicipiu("CÂMPULUNG", [
+				createLoc("CÂMPULUNG", "oras"),
+				createLoc("Valea Rumâneștilor", "oras"),
+			]),
+			createMunicipiu("CURTEA DE ARGEȘ", [
+				createLoc("CURTEA DE ARGEȘ", "oras"),
+				createLoc("Noapteș", "oras"),
+			]),
+			
 ];
 ?>
--- ROMANIA/ARGES/orasearges.php
+++ ROMANIA/ARGES/orasearges.php
@@ -1,6 +1,15 @@
 <?php
 $orase=[
-        
+			createOras("COSTEȘTI", [
+			    createLoc("COSTEȘTI", "oras",[
+					createLoc("Broșteni", "sat"),
+					createLoc("Lăceni", "sat"),
+					createLoc("Pârvu Roșu", "sat"),
+					createLoc("Podu Broșteni", "sat"),
+					createLoc("Smei", "sat"),
+					createLoc("Stârci", "sat"),
+				]),
+			]),       
 			
 ];
 
