ca599424d771d888264f8ad472c892631f7c5dc6
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-02 12:56:55 +0200

    ARGES

---
+++ ROMANIA/ARGES/arges.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'municipiiarges.php';
+require_once 'orasearges.php';
+require_once 'comunearges.php';
+
+$arad =
+createJudet(
+            "ARGES",$municipii,$orase,$comune
+            );
+			
+?>
---
+++ ROMANIA/ARGES/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "../functions.php";
+require_once "arges.php";
+
+			
+echo json_encode($arges, JSON_PRETTY_PRINT);
+
+?>
