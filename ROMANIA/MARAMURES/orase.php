61312134dd4d34699ad54200d5e32bf525f69e97
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-02 18:43:30 +0200

    Update orase.php

--- ROMANIA/MARAMURES/orase.php
+++ ROMANIA/MARAMURES/orase.php
@@ -1,7 +1,84 @@
 <?php
 $orase=[
-						  
-			
+            createOras("BAIA SPRIE",[
+			    createLoc("BAIA SPRIE", "oras",[
+						createLoc("Satu Nou de Sus", "sat"),
+				]),
+				createLoc("Chiuzbaia", "sat"),
+				createLoc("Tăuții de Sus", "sat"),
+			                   ]),
+            createOras("BORȘA",[
+			    createLoc("BORȘA", "oras",),
+				createLoc("Băile Borșa", "sat"),
+			                   ]),	
+            createOras("CAVNIC",[
+			    createLoc("CAVNIC", "oras",),
+			                   ]),
+            createOras("DRAGOMIREȘTI",[
+			    createLoc("DRAGOMIREȘTI", "oras",),
+			                   ]),
+            createOras("SĂLIȘTEA DE SUS",[
+			    createLoc("SĂLIȘTEA DE SUS", "oras",),
+			                   ]),
+            createOras("SEINI",[
+			    createLoc("SEINI", "oras", [
+					createLoc("Săbișa", "sat",),
+					createLoc("Viile Apei", "sat",),
+				]),
+			                   ]),		
+            createOras("ȘOMCUTA MARE",[
+			    createLoc("ȘOMCUTA MARE", "oras", [
+					createLoc("Buteasa", "sat",),
+					createLoc("Codru Butesii", "sat",),
+					createLoc("Finteușu Mare", "sat",),
+				]),
+				createLoc("Buciumi", "sat"),
+				createLoc("Ciolt", "sat"),
+				createLoc("Hovrila", "sat"),
+				createLoc("Vălenii Șomcutei", "sat"),
+			                   ]),	
+            createOras("TĂUȚII-MĂGHERĂUȘ",[
+			    createLoc("TĂUȚII-MĂGHERĂUȘ", "oras", [
+					createLoc("Bozânta Mare", "sat",),
+					createLoc("Merișor", "sat",),
+					createLoc("Ulmoasa", "sat",),
+				]),
+				createLoc("Băița", "sat"),
+				createLoc("Bușag", "sat"),
+				createLoc("Nistru", "sat"),
+			                   ]),							   
+            createOras("TÂRGU LĂPUȘ",[
+			    createLoc("TÂRGU LĂPUȘ", "oras", [
+					createLoc("Boiereni", "sat",),
+					createLoc("Borcut", "sat",),
+					createLoc("Cufoaia", "sat",),
+					createLoc("Dămăcușeni", "sat",),
+					createLoc("Dobricu Lăpușului", "sat",),
+					createLoc("Dumbrava", "sat",),
+					createLoc("Fântânele", "sat",),
+					createLoc("Groape", "sat",),
+					createLoc("Inău", "sat",),
+					createLoc("Răzoare", "sat",),
+					createLoc("Rogoz", "sat",),
+					createLoc("Rohia", "sat",),
+					createLoc("Stoiceni ", "sat",),
+				]),
+			                   ]),
+            createOras("ULMENI",[
+			    createLoc("ULMENI", "oras", [
+					createLoc("Arduzel", "sat",),
+					createLoc("Chelința", "sat",),
+					createLoc("Mânău", "sat",),
+					createLoc("Someș-Uileac", "sat",),
+					createLoc("Țicău", "sat",),
+					createLoc("Vicea", "sat",),
+				]),
+				createLoc("Tohat", "sat"),
+			                   ]),	
+            createOras("VIȘEU DE SUS",[
+			    createLoc("VIȘEU DE SUS", "oras"),
+				createLoc("Vișeu de Mijloc", "sat"),
+			                   ]),							   
 ];
 
 ?>
