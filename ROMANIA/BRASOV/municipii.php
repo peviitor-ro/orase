8f562e604067f1dd8997c86f016b08a734298104
Mede375 <144273209+Mede375@users.noreply.github.com>
2023-12-02 21:59:00 +0200

    Update municipii.php

--- ROMANIA/BRASOV/municipii.php
+++ ROMANIA/BRASOV/municipii.php
@@ -1,7 +1,17 @@
 <?php
 $municipii =
 [
-			
-			
+    createMunicipiu("BRAȘOV", [
+        createLoc("BRAȘOV", "oras"),
+    ]),	
+createMunicipiu("CODLEA", [
+        createLoc("CODLEA", "oras"),
+    ]),
+createMunicipiu("FĂGĂRAȘ", [
+        createLoc("FĂGĂRAȘ", "oras"),
+    ]),	
+createMunicipiu("SĂCELE", [
+        createLoc("SĂCELE", "oras"),
+    ]),			
 ];
 ?>
