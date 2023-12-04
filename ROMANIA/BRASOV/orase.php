a428eb27c10df4c2cccbe80f473cac9f6bd3e295
Mede375 <144273209+Mede375@users.noreply.github.com>
2023-12-04 19:16:22 +0200

    Update orase.php

--- ROMANIA/BRASOV/orase.php
+++ ROMANIA/BRASOV/orase.php
@@ -1,7 +1,28 @@
 <?php
 $orase=[
-						  
-			
+            createOras("GHIMBAV",[
+			    createLoc("GHIMBAV", "oras"),
+			                   ]),						  
+            createOras("PREDEAL",[
+			    createLoc("PREDEAL", "oras"),
+				createLoc("Pârâul Rece", "oras"),
+				createLoc("Timișu de Jos ", "oras"),
+				createLoc("Timișu de Sus", "oras"),
+			                   ]),		
+            createOras("RÂȘNOV",[
+			    createLoc("RÂȘNOV", "oras"),
+			                   ]),	
+            createOras("RUPEA",[
+			    createLoc("RUPEA", "oras"),
+			    createLoc("Fișer", "oras"),
+			                   ]),	
+            createOras("VICTORIA",[
+			    createLoc("VICTORIA", "oras"),
+			                   ]),
+            createOras("ZĂRNEȘTI",[
+			    createLoc("ZĂRNEȘTI", "oras"),
+			    createLoc("Tohanu Nou", "oras"),
+			                   ]),							   
 ];
 
 ?>
