f0a8c1b399610d73a7130122ef7dee638fa98ba5
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-04 16:40:45 +0200

    Update municipii.php

--- ROMANIA/CLUJ/municipii.php
+++ ROMANIA/CLUJ/municipii.php
@@ -7,7 +7,21 @@ $municipii =
 	 createMunicipiu("CÂMPIA TURZII", [
                     createLoc("CÂMPIA TURZII", "oras"),
                 ]),
-			
+         createMunicipiu("DEJ", [
+                    createLoc("DEJ", "oras"), 
+                    createLoc("Peștera", "sat"),
+                    createLoc("Pintic", "sat"),
+                    createLoc("Șomcutu Mic", "sat"),
+                ]),
+         createMunicipiu("GHERLA", [
+                    createLoc("GHERLA", "oras"),
+                    createLoc("Băița", "sat"),
+                    createLoc("Hășdate", "sat"),
+                    createLoc("Silivaș", "sat"),
+                ]),
+	 createMunicipiu("TURDA", [
+                    createLoc("TURDA", "oras"),
+                ]),
 			
 ];
 ?>
