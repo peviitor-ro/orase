d179250034a5f136532e48f589102bb2ca752559
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-04 16:47:25 +0200

    Update comune.php

--- ROMANIA/CLUJ/comune.php
+++ ROMANIA/CLUJ/comune.php
@@ -1,6 +1,20 @@
 <?php
 $comune =[
-
+			createComuna("Aghireșu", [
+					createLoc("Aghireșu", "sat"),
+					createLoc("Aghireșu-Fabrici", "sat"),
+					createLoc("Arghișu", "sat"),
+					createLoc("Băgara", "sat"),
+					createLoc("Dâncu", "sat"),
+					createLoc("Dorolțu", "sat"),
+					createLoc("Inucu", "sat"),
+					createLoc("Leghia", "sat"),
+					createLoc("Fața", "sat"),
+					createLoc("Pleșești", "sat"),
+					createLoc("Macău", "sat"),
+					createLoc("Ticu", "sat"),
+					createLoc("Ticu-Colonie", "sat"),
+								  ]),
 									  
 			
 ];
