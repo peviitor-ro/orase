64efaa1361e70dbccb66927d1fdef686db66c496
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-05 12:52:32 +0200

    bacau

--- ROMANIA/BACAU/municipii.php
+++ ROMANIA/BACAU/municipii.php
@@ -1,7 +1,18 @@
 <?php
 $municipii =
 [
-			
+                createMunicipiu("BACĂU", [
+                    createLoc("BACĂU", "oras"),
+                ]),
+                createMunicipiu("MOINEȘTI", [
+                    createLoc("MOINEȘTI", "oras"),
+                    createLoc("Găzărie ", "sat"),
+                ]),
+                createMunicipiu("ONEȘTI", [
+                    createLoc("ONEȘTI", "oras"),
+                    createLoc("Borzești", "sat"),
+                    createLoc("Slobozia", "sat"),
+                ]),				
 			
 ];
 ?>
--- ROMANIA/BACAU/orase.php
+++ ROMANIA/BACAU/orase.php
@@ -1,6 +1,34 @@
 <?php
 $orase=[
-						  
+            createOras("BUHUȘI",[
+			    createLoc("BUHUȘI", "oras"),
+				createLoc("Marginea", "sat"),
+				createLoc("Runcu", "sat"),
+			                   ]),
+			createOras("COMĂNEȘTI",[
+			    createLoc("COMĂNEȘTI", "oras",[
+				     createLoc("Podei", "sat"),
+					 createLoc("Vermești", "sat"),
+				                                  ]),
+            createOras("DĂRMĂNEȘTI",[
+			    createLoc("DĂRMĂNEȘTI", "oras" [
+											createLoc("Dărmăneasca", "sat"),
+											createLoc("Lapoș", "sat"),
+											createLoc("Păgubeni", "sat"),
+											createLoc("Plopu", "sat"),
+											createLoc("Sălătruc", "sat"),
+												]),
+			                   ]),
+			createOras("SLĂNIC-MOLDOVA",[
+			    createLoc("SLĂNIC-MOLDOVA", "oras",[
+				     createLoc("Cerdac", "sat"),
+					 createLoc("Cireșoaia", "sat"),
+				                                  ]),
+            createOras("TÂRGU OCNA",[
+			    createLoc("TÂRGU OCNA", "oras"),
+				createLoc("Poieni", "sat"),
+				createLoc("Vâlcele", "sat"),
+			                   ]),												  
 			
 ];
 
