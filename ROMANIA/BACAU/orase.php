8035c5afa6e5a2a8f10585de26548c5727ac571c
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-05 13:21:22 +0200

    fixing 500

--- ROMANIA/BACAU/orase.php
+++ ROMANIA/BACAU/orase.php
@@ -23,7 +23,7 @@ $orase=[
 			    createLoc("SLĂNIC-MOLDOVA", "oras",[
 				     createLoc("Cerdac", "sat"),
 					 createLoc("Cireșoaia", "sat"),
-				                                  ]),
+				                                  ],]),
             createOras("TÂRGU OCNA",[
 			    createLoc("TÂRGU OCNA", "oras"),
 				createLoc("Poieni", "sat"),
