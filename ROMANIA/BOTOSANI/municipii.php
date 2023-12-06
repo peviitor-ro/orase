279ae8744d736c2a3fe6773b2c7a9d86728ef6e4
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-06 14:27:20 +0200

    updates BOTOSANI

--- ROMANIA/BOTOSANI/municipii.php
+++ ROMANIA/BOTOSANI/municipii.php
@@ -1,7 +1,15 @@
 <?php
 $municipii =
 [
-			
+		createMunicipiu("BOTOȘANI", [
+                    createLoc("BOTOȘANI", "oras"),
+                ]),	
+		createMunicipiu("DOROHOI", [
+                    createLoc("DOROHOI", "oras"),
+                    createLoc("Dealu Mare", "sat"),
+					createLoc("Loturi Enescu", "sat"),
+                    createLoc("Progresul", "sat"),
+                ]),					
 			
 ];
 ?>
--- ROMANIA/BOTOSANI/orase.php
+++ ROMANIA/BOTOSANI/orase.php
@@ -1,7 +1,49 @@
 <?php
 $orase=[
-						  
-			
+            createOras("BUCECEA",[
+			    createLoc("BUCECEA", "oras",[
+						createLoc("Bohoghina", "sat"),
+				  ]),
+				createLoc("Călinești", "sat"),
+			                   ]),
+							   
+            createOras("DARABANI",[
+			    createLoc("DARABANI", "oras", [
+						createLoc("Bajura ", "sat"),
+						createLoc("Eșanca ", "sat"),
+						createLoc("Lișmănița ", "sat"),
+				]),
+			                   ]),
+							   
+            createOras("FLĂMÂNZI",[
+			    createLoc("FLĂMÂNZI", "oras",[
+						createLoc("Chițoveni", "sat"),
+						createLoc("Prisăcani", "sat"),
+				]),
+				createLoc("Nicolae Bălcescu", "sat"),
+				createLoc("Poiana", "sat"),
+			                   ]),
+							   
+            createOras("SĂVENI",[
+			    createLoc("SĂVENI", "oras", [
+						createLoc("Bodeasa ", "sat"),
+						createLoc("Bozieni ", "sat"),
+						createLoc("Chișcăreni", "sat"),
+						createLoc("Petricani", "sat"),
+						createLoc("Sat Nou", "sat"),
+				]),
+				
+			                   ]),							   
+
+            createOras("ȘTEFĂNEȘTI",[
+			    createLoc("ȘTEFĂNEȘTI", "oras", [
+						createLoc("Bădiuți ", "sat"),
+						createLoc("Bobulești ", "sat"),
+						createLoc("Stânca", "sat"),
+						createLoc("Ștefănești-Sat", "sat"),
+				]),
+				
+			                   ]),		
 ];
 
 ?>
