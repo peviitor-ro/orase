ad03242adaec8f47c264e10eefbabf58d6f71193
DianaDascalu2 <118655267+DianaDascalu2@users.noreply.github.com>
2023-12-06 18:41:04 +0200

    Update comune.php

--- ROMANIA/HARGHITA/comune.php
+++ ROMANIA/HARGHITA/comune.php
@@ -1,7 +1,70 @@
 <?php
 $comune =[
 
-									  
+	createComuna("Atid", [
+        createLoc("Atid", "sat"),
+        createLoc("Crișeni", "sat"),
+        createLoc("Cușmed", "sat"),
+        createLoc("Inlăceni", "sat"),
+        createLoc("Șiclod", "sat")
+    ]);
+    
+    createComuna("Avrămești", [
+        createLoc("Avrămești", "sat"),
+        createLoc("Andreeni", "sat"),
+        createLoc("Cechești", "sat"),
+        createLoc("Goagiu", "sat"),
+        createLoc("Firtănuș", "sat"),
+        createLoc("Laz-Firtănuș", "sat"),
+        createLoc("Laz-Șoimuș", "sat"),
+        createLoc("Medișoru Mic", "sat")
+    ]);
+    
+    createComuna("Bilbor", [
+        createLoc("Bilbor", "sat"),
+        createLoc("Răchitiș", "sat")
+    ]);
+    
+    createComuna("Brădești", [
+        createLoc("Brădești", "sat"),
+        createLoc("Târnovița", "sat")
+    ]);
+    
+    createComuna("Căpâlnița", [
+        createLoc("Căpâlnița", "sat")
+    ]);
+    
+    createComuna("Cârța", [
+        createLoc("Cârța", "sat"),
+        createLoc("Ineu", "sat")
+    ]);
+    
+    createComuna("Ciceu", [
+        createLoc("Ciceu", "sat"),
+        createLoc("Ciaracio", "sat")
+    ]);
+    
+    createComuna("Ciucsângeorgiu", [
+        createLoc("Ciucsângeorgiu", "sat"),
+        createLoc("Armășeni", "sat"),
+        createLoc("Armășenii Noi", "sat"),
+        createLoc("Bancu", "sat"),
+        createLoc("Ciobăniș", "sat"),
+        createLoc("Cotormani", "sat"),
+        createLoc("Eghersec", "sat"),
+        createLoc("Ghiurche", "sat"),
+        createLoc("Potiond", "sat")
+    ]);
+    
+    createComuna("Ciumani", [
+        createLoc("Ciumani", "sat")
+    ]);
+    
+    createComuna("Corbu", [
+        createLoc("Corbu", "sat"),
+        createLoc("Capu Corbului", "sat")
+    ]),
+    								  
 			
 ];
 ?>
