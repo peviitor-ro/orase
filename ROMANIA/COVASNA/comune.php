50d0d813d659506c59894bb3341739ba5d3e8dbd
DianaDascalu2 <118655267+DianaDascalu2@users.noreply.github.com>
2023-12-06 17:13:37 +0200

    Update comune.php

--- ROMANIA/COVASNA/comune.php
+++ ROMANIA/COVASNA/comune.php
@@ -1,7 +1,197 @@
 <?php
 $comune =[
 
-									  
+    createComuna("Aita Mare", [
+        createLoc("Aita Mare", "sat"),
+        createLoc("Aita Medie", "sat")
+    ]),
+    createComuna("Arcuș", [
+        createLoc("Arcuș", "sat")
+    ]),
+    createComuna("Barcani", [
+        createLoc("Barcani", "sat"),
+        createLoc("Lădăuți", "sat"),
+        createLoc("Sărămaș", "sat")
+    ]),
+    createComuna("Bățani", [
+        createLoc("Bățanii Mari", "sat"),
+        createLoc("Aita Seacă", "sat"),
+        createLoc("Bățanii Mici", "sat"),
+        createLoc("Herculian", "sat"),
+        createLoc("Ozunca-Băi", "sat")
+    ]),
+    createComuna("Belin", [
+        createLoc("Belin", "sat"),
+        createLoc("Belin-Vale", "sat")
+    ]),
+    createComuna("Bixad", [
+        createLoc("Bixad", "sat")
+    ]),
+    createComuna("Bodoc", [
+        createLoc("Bodoc", "sat"),
+        createLoc("Olteni", "sat"),
+        createLoc("Zălan", "sat")
+    ]),
+    createComuna("Boroșneu Mare", [
+        createLoc("Boroșneu Mare", "sat"),
+        createLoc("Boroșneu Mic", "sat"),
+        createLoc("Dobolii de Sus", "sat"),
+        createLoc("Leț", "sat"),
+        createLoc("Țufalău", "sat"),
+        createLoc("Valea Mică", "sat")
+    ]),
+    createComuna("Brateș", [
+        createLoc("Brateș", "sat"),
+        createLoc("Pachia", "sat"),
+        createLoc("Telechia", "sat")
+    ]),
+    createComuna("Brăduț", [
+        createLoc("Brăduț", "sat"),
+        createLoc("Doboșeni", "sat"),
+        createLoc("Filia", "sat"),
+        createLoc("Tălișoara", "sat")
+    ]),
+    createComuna("Brețcu", [
+        createLoc("Brețcu", "sat"),
+        createLoc("Mărtănuș", "sat"),
+        createLoc("Oituz", "sat")
+    ]),
+    createComuna("Catalina", [
+        createLoc("Catalina", "sat"),
+        createLoc("Hătuica", "sat"),
+        createLoc("Imeni", "sat"),
+        createLoc("Mărcușa", "sat"),
+        createLoc("Mărtineni", "sat")
+    ]),
+    createComuna("Cernat", [
+        createLoc("Cernat", "sat"),
+        createLoc("Albiș", "sat"),
+        createLoc("Icafalău", "sat")
+    ]),
+    createComuna("Chichiș", [
+        createLoc("Chichiș", "sat"),
+        createLoc("Băcel", "sat")
+    ]),
+    createComuna("Comandău", [
+        createLoc("Comandău", "sat")
+    ]),
+    createComuna("Dalnic", [
+        createLoc("Dalnic", "sat")
+    ]),
+    createComuna("Dobârlău", [
+        createLoc("Dobârlău", "sat"),
+        createLoc("Lunca Mărcușului", "sat"),
+        createLoc("Mărcuș", "sat"),
+        createLoc("Valea Dobârlăului", "sat")
+    ]),
+    createComuna("Estelnic", [
+        createLoc("Estelnic", "sat"),
+        createLoc("Cărpinenii", "sat"),
+        createLoc("Valea Scurtă", "sat")
+    ]),
+    createComuna("Ghelința", [
+        createLoc("Ghelința", "sat"),
+        createLoc("Harale", "sat")
+    ]),
+    createComuna("Ghidfalău", [
+        createLoc("Ghidfalău", "sat"),
+        createLoc("Angheluș", "sat"),
+        createLoc("Fotoș", "sat"),
+        createLoc("Zoltan", "sat")
+    ]),
+    createComuna("Hăghig", [
+        createLoc("Hăghig", "sat"),
+        createLoc("Iarăș", "sat")
+    ]),
+    createComuna("Ilieni", [
+        createLoc("Ilieni", "sat"),
+        createLoc("Dobolii de Jos", "sat"),
+        createLoc("Sâncraiu", "sat")
+    ]),
+    createComuna("Lemnia", [
+        createLoc("Lemnia", "sat")
+    ]),
+    createComuna("Malnaș", [
+        createLoc("Malnaș", "sat"),
+        createLoc("Malnaș-Băi", "sat"),
+        createLoc("Valea Zălanului", "sat")
+    ]),
+    createComuna("Mereni", [
+        createLoc("Mereni", "sat"),
+        createLoc("Lutoasa", "sat")
+    ]),
+    createComuna("Micfalău", [
+        createLoc("Micfalău", "sat")
+    ]),
+    createComuna("Moacșa", [
+        createLoc("Moacșa", "sat"),
+        createLoc("Pădureni", "sat")
+    ]),
+    createComuna("Ojdula", [
+        createLoc("Ojdula", "sat"),
+        createLoc("Hilib", "sat")
+    ]),
+    createComuna("Ozun", [
+        createLoc("Ozun", "sat"),
+        createLoc("Bicfalău", "sat"),
+        createLoc("Lisnău", "sat"),
+        createLoc("Lisnău-Vale", "sat"),
+        createLoc("Lunca Ozunului", "sat"),
+        createLoc("Măgheruș", "sat"),
+        createLoc("Sântionlunca", "sat")
+    ]),
+    createComuna("Poian", [
+        createLoc("Poian", "sat"),
+        createLoc("Belani", "sat")
+    ]),
+    createComuna("Reci", [
+        createLoc("Reci", "sat"),
+        createLoc("Aninoasa", "sat"),
+        createLoc("Bita", "sat"),
+        createLoc("Saciova", "sat")
+    ]),
+    createComuna("Sita Buzăului", [
+        createLoc("Sita Buzăului", "sat"),
+        createLoc("Crasna", "sat"),
+        createLoc("Merișor", "sat"),
+        createLoc("Zăbrătău", "sat")
+    ]),
+    createComuna("Sânzieni", [
+        createLoc("Sânzieni", "sat"),
+        createLoc("Cașinu Mic", "sat"),
+        createLoc("Petriceni", "sat"),
+        createLoc("Valea Seacă", "sat")
+    ]),
+    createComuna("Turia", [
+        createLoc("Turia", "sat"),
+        createLoc("Alungeni", "sat")
+    ]),
+    createComuna("Valea Crișului", [
+        createLoc("Valea Crișului", "sat"),
+        createLoc("Calnic", "sat")
+    ]),
+    createComuna("Valea Mare", [
+        createLoc("Valea Mare", "sat")
+    ]),
+    createComuna("Vâlcele", [
+        createLoc("Araci", "sat"),
+        createLoc("Ariușd", "sat"),
+        createLoc("Hetea", "sat"),
+        createLoc("Vâlcele", "sat")
+    ]),
+    createComuna("Vârghiș", [
+        createLoc("Vârghiș", "sat")
+    ]),
+    createComuna("Zagon", [
+        createLoc("Zagon", "sat"),
+        createLoc("Păpăuți", "sat")
+    ]),
+    createComuna("Zăbala", [
+        createLoc("Zăbala", "sat"),
+        createLoc("Peteni", "sat"),
+        createLoc("Surcea", "sat"),
+        createLoc("Tamașfalău", "sat")
+    ]),							  
 			
 ];
 ?>
