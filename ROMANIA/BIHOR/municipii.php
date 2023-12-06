b8e100af327b51e42df8a3c16a39368337bc8b1d
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-06 13:53:49 +0200

    updates in BIHOR

--- ROMANIA/BIHOR/municipii.php
+++ ROMANIA/BIHOR/municipii.php
@@ -1,7 +1,21 @@
 <?php
 $municipii =
 [
-			
+		createMunicipiu("ORADEA", [
+                    createLoc("ORADEA", "oras"),
+                ]),	
+		createMunicipiu("BEIUȘ", [
+                    createLoc("BEIUȘ", "oras"),
+                    createLoc("Delani", "sat"),
+                ]),	
+		createMunicipiu("MARGHITA", [
+                    createLoc("MARGHITA", "oras"),
+                    createLoc("Cheț", "sat"),
+					createLoc("Ghenetea", "sat"),
+                ]),	
+		createMunicipiu("SALONTA", [
+                    createLoc("SALONTA", "oras"),
+                ]),
 			
 ];
 ?>
--- ROMANIA/BIHOR/orase.php
+++ ROMANIA/BIHOR/orase.php
@@ -1,7 +1,40 @@
 <?php
 $orase=[
-						  
-			
+            createOras("ALEȘD",[
+			    createLoc("ALEȘD", "oras"),
+				createLoc("Pădurea Neagră", "sat"),
+				createLoc("Peștiș", "sat"),
+				createLoc("Tinăud", "sat"),
+			                   ]),
+            createOras("NUCET",[
+			    createLoc("NUCET", "oras"),
+				createLoc("Băița", "sat"),
+				createLoc("Băița-Plai", "sat"),
+			                   ]),
+            createOras("SĂCUENI",[
+			    createLoc("SĂCUENI", "oras", [
+								createLoc("Cadea", "sat"),
+								createLoc("Ciocaia", "sat"),
+								createLoc("Cubulcut", "sat"),
+								createLoc("Olosig", "sat"),
+								createLoc("Sânnicolau de Munte", "sat"),
+				]),
+
+			                   ]),
+            createOras("ȘTEI",[
+			    createLoc("ȘTEI", "oras"),
+			                   ]),
+            createOras("VALEA LUI MIHAI",[
+			    createLoc("VALEA LUI MIHAI", "oras"),
+			                   ]),
+            createOras("VAȘCĂU",[
+			    createLoc("VAȘCĂU", "oras"),
+				createLoc("Câmp", "sat"),
+				createLoc("Câmp-Moți", "sat"),
+				createLoc("Colești", "sat"),
+				createLoc("Vărzarii de Jos", "sat"),
+				createLoc("Vărzarii de Sus", "sat"),
+			                   ]),							   		
 ];
 
 ?>
