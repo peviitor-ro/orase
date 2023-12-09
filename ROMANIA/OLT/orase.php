229ed9023a97a2b8f3e3801d5b73b7ad9ca1a091
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 23:02:26 +0200

    Update orase.php

--- ROMANIA/OLT/orase.php
+++ ROMANIA/OLT/orase.php
@@ -1,7 +1,64 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("BALȘ", [
+        createLoc("BALȘ", "oras", [
+            createLoc("Teiș", "sat"),
+            
+        ]),
+	       createLoc("Corbeni", "sat"),
+            createLoc("Româna", "sat"),
+    ]),
+
+    createOras("CORABIA", [
+        createLoc("CORABIA", "oras", [
+            createLoc("Vârtopu", "sat"),
+            
+        ]),
+	       createLoc("Tudor Vladimirescu", "sat"),
+    ]),
+
+    createOras("DRĂGĂNEȘTI-OLT", [
+        createLoc("DRĂGĂNEȘTI-OLT", "oras", [
+            createLoc("Comani", "sat"),
+        ]),
+    ]),
+
+    createOras("PIATRA-OLT", [
+        createLoc("PIATRA-OLT", "oras", [
+            createLoc("Bistrița Nouă", "sat"),
+            createLoc("Criva de Jos", "sat"),
+            createLoc("Criva de Sus", "sat"),
+            createLoc("Enoșești", "sat"),
+            createLoc("Piatra", "sat"),
+        ]),
+    ]),
+
+    createOras("POTCOAVA", [
+        createLoc("POTCOAVA", "oras", [
+            createLoc("Potcoava-Fălcoeni", "sat"),
+            createLoc("Sinești", "sat"),
+            createLoc("Trufinești", "sat"),
+            createLoc("Valea Merilor", "sat"),
+        ]),
+    ]),
+
+    createOras("SCORNICEȘTI", [
+        createLoc("SCORNICEȘTI", "oras", [
+            createLoc("Bălțați", "sat"),
+            createLoc("Bircii", "sat"),
+            createLoc("Chițeasca", "sat"),
+            createLoc("Constantinești", "sat"),
+            createLoc("Jitaru", "sat"),
+            createLoc("Mărgineni-Slobozia", "sat"),
+            createLoc("Mihăilești-Popești", "sat"),
+            createLoc("Mogoșești", "sat"),
+            createLoc("Negreni", "sat"),
+            createLoc("Piscani", "sat"),
+            createLoc("Rusciori", "sat"),
+            createLoc("Șuica", "sat"),
+            createLoc("Teiuș", "sat"),
+        ]),
+    ]),
 ];
 
 ?>
