5ac22ec957464aa6884a1fffa8d76b35fd6da844
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:41:41 +0200

    Update orase.php

--- ROMANIA/IALOMITA/orase.php
+++ ROMANIA/IALOMITA/orase.php
@@ -1,7 +1,27 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("AMARA", [
+        createLoc("AMARA", "oras", [
+            createLoc("Amara Nouă", "sat"),
+        ]),
+    ]),
+
+    createOras("CĂZĂNEȘTI", [
+        createLoc("CĂZĂNEȘTI", "oras"),
+    ]),
+
+    createOras("FIERBINȚI-TÂRG", [
+        createLoc("FIERBINȚI-TÂRG", "oras", [
+            createLoc("Fierbinții de Jos"),
+            createLoc("Fierbinții de Sus"),
+            createLoc("Grecii de Jos"),
+        ]),
+    ]),
+
+    createOras("ȚĂNDĂREI", [
+        createLoc("ȚĂNDĂREI", "oras"),
+    ]),
 ];
+
 
 ?>
