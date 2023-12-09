67000f83a40b7a10fa6c331431dd8a4cb0dde166
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 20:12:52 +0200

    Update orase.php

--- ROMANIA/DAMBOVITA/orase.php
+++ ROMANIA/DAMBOVITA/orase.php
@@ -1,6 +1,49 @@
 <?php
 $orase=[
-						  
+            createOras("FIENI",[
+			    createLoc("FIENI", "oras",[
+						createLoc("Berevoești", "sat"),
+						createLoc("Costești", "sat"),
+										]),
+				
+				  ]),
+            createOras("GĂEȘTI",[
+			    createLoc("GĂEȘTI", "oras"),
+				
+				  ]),
+            createOras("PUCIOASA",[
+			    createLoc("PUCIOASA", "oras"),
+				createLoc("Bela", "sat"),
+				createLoc("Diaconești", "sat"),
+				createLoc("Glodeni", "sat"),
+				createLoc("Malurile", "sat"),
+				createLoc("Miculești", "sat"),
+				createLoc("Pucioasa-Sat", "sat"),
+										]),
+				
+				  ]),            
+				createOras("RĂCARI",[
+			    createLoc("RĂCARI", "oras"),
+				createLoc("Bălănești", "sat"),
+				createLoc("Colacu", "sat"),
+				createLoc("Ghergani", "sat"),
+				createLoc("Ghimpați", "sat"),
+				createLoc("Mavrodin", "sat"),
+				createLoc("Săbiești", "sat"),
+				createLoc("Stănești", "sat"),
+										
+				
+				  ]),
+            createOras("TITU",[
+			    createLoc("TITU", "oras",[
+						createLoc("Fusea", "sat"),
+						createLoc("Hagioaica", "sat"),
+						createLoc("Mereni", "sat"),
+						createLoc("Plopu", "sat"),
+						createLoc("Sălcuța", "sat"),
+										]),
+				
+				  ]),				  
 			
 ];
 
