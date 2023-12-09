451b7898e981620cbf7fab671e6e7453f21e072b
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 19:02:59 +0200

    Update orase.php

--- ROMANIA/CALARASI/orase.php
+++ ROMANIA/CALARASI/orase.php
@@ -1,6 +1,28 @@
 <?php
 $orase=[
-						  
+            createOras("BUDEȘTI",[
+			    createLoc("BUDEȘTI", "oras",[
+						createLoc("Aprozi", "sat"),
+						createLoc("Buciumeni", "sat"),
+						createLoc("Gruiu", "sat"),
+										]),
+				
+				  ]),	
+            createOras("FUNDULEA",[
+			    createLoc("FUNDULEA", "oras",[
+						createLoc("Alexandru I. Cuza", "sat"),
+						createLoc("Gostilele", "sat"),
+										]),
+				
+				  ]),	
+            createOras("LEHLIU-GARĂ",[
+			    createLoc("LEHLIU-GARĂ", "oras",[
+						createLoc("Buzoeni", "sat"),
+						createLoc("Răzvani", "sat"),
+						createLoc("Valea Seacă", "sat"),
+										]),
+				
+				  ]),					  
 			
 ];
 
