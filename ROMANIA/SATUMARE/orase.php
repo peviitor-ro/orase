e3f47de2b671dc669e8aefe43c64ac4399c18188
RaresCode <irimusrares7@gmail.com>
2023-12-09 22:17:47 +0200

    Update orase.php SATUMARE

--- ROMANIA/SATUMARE/orase.php
+++ ROMANIA/SATUMARE/orase.php
@@ -1,6 +1,39 @@
 <?php
 $orase=[
-						  
+	
+    createOras("ARDUD", [
+        createLoc("ARDUD", "oras", [
+            createLoc("Ardud-Vii", "sat"),
+            createLoc("Baba Novac", "sat"),
+            createLoc("Gerăușa", "sat"),
+            createLoc("Sărătura", "sat"),
+		  
+        ]),
+        createLoc("Mădăras", "sat"),
+	
+    ]),
+
+    createOras("LIVADA", [
+        createLoc("LIVADA", "oras"),
+	createLoc("Adrian", "sat"),
+	createLoc("Dumbrava", "sat"),
+	createLoc("Livada Mică", "sat"),
+    ]),	
+	
+    createOras("NEGREȘTI-OAȘ", [
+        createLoc("NEGREȘTI-OAȘ", "oras"),
+	createLoc("Luna", "sat"),
+	createLoc("Tur", "sat"),
+    ]),		
+	
+    createOras("TĂȘNAD", [
+        createLoc("Blaja", "sat"),
+	createLoc("Cig", "sat"),
+	createLoc("Rațiu", "sat"),
+	createLoc("Sărăuad", "sat"),
+	createLoc("Valea Morii", "sat"),
+    ]),		
+	
 			
 ];
 
