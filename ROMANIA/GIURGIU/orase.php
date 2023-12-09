0b4ff1d180def53773f18b8b15bdb367047ed7c2
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 21:39:33 +0200

    Update orase.php

--- ROMANIA/GIURGIU/orase.php
+++ ROMANIA/GIURGIU/orase.php
@@ -1,6 +1,19 @@
 <?php
 $orase=[
-						  
+	        createOras("BOLINTIN-VALE",[
+			    createLoc("BOLINTIN-VALE", "oras",[
+						createLoc("Crivina", "sat"),
+						createLoc("Malu Spart", "sat"),
+				      		createLoc("Suseni ", "sat"),
+				  ]),				  
+			]),
+		createOras("MIHĂILEȘTI",[
+			    createLoc("MIHĂILEȘTI", "oras",[
+						createLoc("Drăgănescu", "sat"),
+						createLoc("Novaci", "sat"),
+				      		createLoc("Popești ", "sat"),
+				  ]),				  
+			]),						  
 			
 ];
 
