8b6252c0af9466785ce9a7b24d7e7ceb8088b7be
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 20:29:47 +0200

    Update orase.php

--- ROMANIA/DOLJ/orase.php
+++ ROMANIA/DOLJ/orase.php
@@ -1,6 +1,28 @@
 <?php
 $orase=[
-						  
+
+			createOras("BECHET", [
+			    createLoc("BECHET", "oras"),
+			                     ]),
+	
+			createOras("DĂBULENI", [
+			    createLoc("DĂBULENI", "oras",[
+					createLoc("Chiașu", "sat"),
+							]),
+			                     ]),
+			createOras("FILIAȘI", [
+			    createLoc("FILIAȘI", "oras",[
+					createLoc("Almăjel", "sat"),
+				        createLoc("Bâlta", "sat"),
+				      	createLoc("Braniște", "sat"),
+				      	createLoc("Fratoștița", "sat"),
+				      	createLoc("Răcarii de Sus", "sat"),
+				      	createLoc("Uscăci", "sat"),
+							]),
+			                     ]),
+			createOras("ȘEGARCEA", [
+			    createLoc("ȘEGARCEA", "oras"),
+			                     ]),
 			
 ];
 
