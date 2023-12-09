c79bae930bb8edc5a7341182b9924c287709bf36
RaresCode <irimusrares7@gmail.com>
2023-12-09 21:54:26 +0200

    Update orase.php suceava

--- ROMANIA/SUCEAVA/orase.php
+++ ROMANIA/SUCEAVA/orase.php
@@ -1,7 +1,112 @@
 <?php
 $orase=[
-						  
-			
+			createOras("BROȘTENI", [
+				createLoc("BROȘTENI", "oras",[
+						createLoc("Cotârgași", "sat"),
+											]),
+				createLoc("Hăleasa", "sat",[
+						createLoc("Dârmoxa", "sat"),
+											]),
+				createLoc("Lungeni", "sat",[
+						createLoc("Frasin", "sat"),
+											]),
+				createLoc("Neagra", "sat",[
+						createLoc("Holda", "sat"),
+					  	createLoc("Holdița", "sat"),
+					  	createLoc("Pietroasa", "sat"),
+											]),
+			                     ]),
+	
+			createOras("CAJVANA", [
+				createLoc("CAJVANA", "oras",[
+						createLoc("Codru", "sat"),
+											]),
+
+			                     ]),
+	
+			createOras("DOLHASCA", [
+				createLoc("DOLHASCA", "oras",[
+						createLoc("Budeni", "sat"),
+					  	createLoc("Gulia", "sat"),
+					  	createLoc("Poiana", "sat"),
+					  	createLoc("Poienari", "sat"),
+					  	createLoc("Probota", "sat"),
+					  	createLoc("Siliștea Nouă", "sat"),
+					  	createLoc("Valea Poienei", "sat"),
+											]),
+				
+			                     ]),
+	
+			createOras("FRASIN", [
+				createLoc("FRASIN", "oras",[
+						createLoc("Doroteia", "sat"),
+											]),
+				createLoc("Bucșoaia", "sat",[
+						createLoc("Plutonița", "sat"),
+											]),
+				
+			                     ]),
+	
+			createOras("GURA HUMORULUI", [
+				createLoc("GURA HUMORULUI", "oras",[
+						createLoc("Doroteia", "sat"),
+											]),
+				createLoc("Voroneț", "sat",[
+						createLoc("Plutonița", "sat"),
+											]),
+				
+			                     ]),
+	
+			createOras("LITENI", [
+				createLoc("LITENI", "oras",[
+						createLoc("Corni", "sat"),
+					  	createLoc("Roșcani", "sat"),
+					  	createLoc("Rotunda", "sat"),
+					  	createLoc("Siliștea", "sat"),
+					  	createLoc("Vercicani", "sat"),
+											]),
+				
+			                     ]),
+	
+			createOras("MILIȘĂUȚI", [
+				createLoc("MILIȘĂUȚI", "oras",[
+						createLoc("Gara", "sat"),
+											]),
+				createLoc("Bădeuți", "sat",[
+						createLoc("Lunca", "sat"),
+											]),
+				
+			                     ]),
+	
+			createOras("SALCEA", [
+				createLoc("SALCEA", "oras",[
+						createLoc("Prelipca", "sat"),
+											]),
+				createLoc("Mereni", "sat"),
+				createLoc("Plopeni", "sat"),
+				createLoc("Văratec", "sat"),
+				
+			                     ]),
+	
+			createOras("SIRET", [
+				createLoc("SIRET", "oras"),
+				createLoc("Mănăstioara", "sat"),
+				createLoc("Pădureni", "sat"),
+				
+			                     ]),
+			createOras("SOLCA", [
+				createLoc("SOLCA", "oras"),
+				
+			                     ]),
+	
+			createOras("VICOVU DE SUS", [
+				createLoc("VICOVU DE SUS", "oras"),
+				createLoc("Bivolăria", "sat"),
+				
+			                     ]),
+
+	
+
 ];
 
 ?>
