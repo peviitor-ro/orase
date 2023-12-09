672454e128b93abdec97bb838df44ef4ca591c96
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:43:44 +0200

    Update orase.php

--- ROMANIA/IASI/orase.php
+++ ROMANIA/IASI/orase.php
@@ -1,7 +1,24 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("HÂRLĂU", [
+        createLoc("HÂRLĂU", "oras", [
+            createLoc("Pârcovaci", "sat"),
+        ]),
+    ]),
+
+    createOras("PODU ILOAIEI", [
+        createLoc("Podu Iloaiei", "oras", [
+            createLoc("Cosițeni", "sat"),
+            
+            createLoc("Holm", "sat"),
+            createLoc("Scobâlțeni", "sat"),
+        ]),
+	       createLoc("Budăi", "sat"),
+    ]),
+
+    createOras("TÂRGU FRUMOS", [
+        createLoc("TÂRGU FRUMOS", "oras"),
+    ]),
 ];
 
 ?>
