f214d569a87b7eaceb38a172d4f724a65893b602
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 18:45:22 +0200

    Update orase.php

--- ROMANIA/BUZAU/orase.php
+++ ROMANIA/BUZAU/orase.php
@@ -1,7 +1,42 @@
 <?php
 $orase=[
-						  
-			
+            createOras("NEHOIU",[
+			    createLoc("NEHOIU", "oras",,[
+						createLoc("Bâsca Rozilei", "sat"),
+						createLoc("Chirlești", "sat"),
+						createLoc("Curmătura", "sat"),
+						createLoc("Lunca Priporului", "sat"),
+						createLoc("Mlăjet", "sat"),
+						createLoc("Nehoiașu", "sat"),
+						createLoc("Păltineni", "sat"),
+						createLoc("Stănila", "sat"),
+						createLoc("Vinețișu", "sat"),
+							]),
+				]),
+            createOras("PĂTÂRLAGELE",[
+			    createLoc("PĂTÂRLAGELE", "oras",[
+						createLoc("Mușcel", "sat"),
+						createLoc("Poienile", "sat"),
+						createLoc("Sibiciu de Sus", "sat"),
+						createLoc("Stroești", "sat"),
+						createLoc("Valea Lupului", "sat"),
+						createLoc("Valea Sibiciului", "sat"),
+						createLoc("Valea Viei", "sat"),
+										]),
+				createLoc("Calea Chiojdului", "sat"),
+				createLoc("Crâng", "sat"),
+				createLoc("Fundăturile", "sat"),
+				createLoc("Gornet", "sat"),
+				createLoc("Lunca", "sat"),
+				createLoc("Mănăstirea", "sat"),
+				createLoc("Mărunțișu", "sat"),
+				  ]),
+
+            createOras("POGOANELE",[
+			    createLoc("POGOANELE", "oras",[
+						createLoc("Căldărăști", "sat"),
+				  ]),				  
+			]),
 ];
 
 ?>
