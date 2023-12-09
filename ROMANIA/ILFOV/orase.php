3b41e7d182e772f7811a5ec31c6c3a98c3ee09d1
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:48:07 +0200

    Update orase.php

--- ROMANIA/ILFOV/orase.php
+++ ROMANIA/ILFOV/orase.php
@@ -1,7 +1,46 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("BRAGADIRU", [
+        createLoc("BRAGADIRU", "oras"),
+    ]),
+
+    createOras("BUFTEA", [
+        createLoc("BUFTEA", "oras", [
+            createLoc("Buciumeni", "sat"),
+        ]),
+    ]),
+
+    createOras("CHITILA", [
+        createLoc("CHITILA", "oras"),
+        createLoc("Rudeni", "sat"),
+    ]),
+
+    createOras("MĂGURELE", [
+        createLoc("MĂGURELE", "oras"),
+        createLoc("Alunișu", "sat"),
+        createLoc("Dumitrana", "sat"),
+        createLoc("Pruni", "sat"),
+        createLoc("Vârteju", "sat"),
+    ]),
+
+    createOras("OTOPENI", [
+        createLoc("OTOPENI", "oras", [
+            createLoc("Odăile", "sat"),
+        ]),
+    ]),
+
+    createOras("PANTELIMON", [
+        createLoc("PANTELIMON", "oras"),
+    ]),
+
+    createOras("POPEȘTI-LEORDENI", [
+        createLoc("POPEȘTI-LEORDENI", "oras"),
+    ]),
+
+    createOras("VOLUNTARI", [
+        createLoc("VOLUNTARI", "oras"),
+    ]),
 ];
+
 
 ?>
