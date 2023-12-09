636c889c53e66923826fe4bac1f39042c9f0830f
RaresCode <irimusrares7@gmail.com>
2023-12-09 22:10:32 +0200

    Update orase.php SIBIU

--- ROMANIA/SIBIU/orase.php
+++ ROMANIA/SIBIU/orase.php
@@ -1,78 +1,67 @@
 <?php
-$orase=[
-			createOras("AGNITA", [
-			    	createLoc("AGNITA", "oras"),
-				createLoc("Coveș", "sat"),
-				createLoc("Ruja", "sat"),
-			                     ]),	
-	
-			createOras("AVRIG", [
-			    	createLoc("AVRIG", "oras", [
-					  createLoc("Bradu", "sat"),
-					  createLoc("Glâmboaca", "sat"),
-					  createLoc("Mârșa", "sat"),
-					  createLoc("Săcădate", "sat"),
-					  
-					  ]),
+$orase = [
+    createOras("AGNITA", [
+        createLoc("AGNITA", "oras"),
+        createLoc("Coveș", "sat"),
+        createLoc("Ruja", "sat"),
+    ]),
 
-			                     ]),
+    createOras("AVRIG", [
+        createLoc("AVRIG", "oras", [
+            createLoc("Bradu", "sat"),
+            createLoc("Glâmboaca", "sat"),
+            createLoc("Mârșa", "sat"),
+            createLoc("Săcădate", "sat"),
+        ]),
+    ]),
 
-			createOras("CISNĂDIE", [
-			    	createLoc("CISNĂDIE", "oras"),
-				createLoc("Cisnădioara", "sat"),
-			                     ]),	
+    createOras("CISNĂDIE", [
+        createLoc("CISNĂDIE", "oras"),
+        createLoc("Cisnădioara", "sat"),
+    ]),
 
-			createOras("COPȘA MICĂ", [
-			    	createLoc("COPȘA MICĂ", "oras"),
-			                     ]),	
-	
-			createOras("DUMBRĂVENI", [
-			    	createLoc("DUMBRĂVENI", "oras"),
-				createLoc("Ernea", "sat"),
-				createLoc("Șaroș pe Târnave", "sat"),
-			                     ]),
-	
-			createOras("MIERCUREA SIBIULUI", [
-			    	createLoc("MIERCUREA SIBIULUI", "oras", [
-					  createLoc("Apoldu de Sus", "sat"),
-					  createLoc("Dobârca", "sat"),
-					  
-					  ]),
+    createOras("COPȘA MICĂ", [
+        createLoc("COPȘA MICĂ", "oras"),
+    ]),
 
-			                     ]),
-				   
-			createOras("OCNA SIBIULUI", [
-			    	createLoc("OCNA SIBIULUI", "oras", [
-					  createLoc("Topârcea", "sat"),
-					  
-					  ]),
-				   
-			createOras("SĂLIȘTE", [
-			    	createLoc("SĂLIȘTE", "oras", [
-					  createLoc("Aciliu", "sat"),
-					  createLoc("Amnaș", "sat"),
-					  createLoc("Fântânele", "sat"),
-					  createLoc("Galeș", "sat"),
-					  createLoc("Mag", "sat"),
-					  createLoc("Săcel", "sat"),
-					  createLoc("Sibiel", "sat"),
-					  createLoc("Vale", "sat"),
-					  
-					  ]),
+    createOras("DUMBRĂVENI", [
+        createLoc("DUMBRĂVENI", "oras"),
+        createLoc("Ernea", "sat"),
+        createLoc("Șaroș pe Târnave", "sat"),
+    ]),
 
-			                     ]),
-				   
-			createOras("TĂLMACIU", [
-			    	createLoc("TĂLMACIU", "oras", [
-					  createLoc("Colonia Tălmaciu", "sat"),
-					  createLoc("Tălmăcel", "sat"),
-					  
-					  ]),
+    createOras("MIERCUREA SIBIULUI", [
+        createLoc("MIERCUREA SIBIULUI", "oras", [
+            createLoc("Apoldu de Sus", "sat"),
+            createLoc("Dobârca", "sat"),
+        ]),
+    ]),
 
-			                     ]),
-			
+    createOras("OCNA SIBIULUI", [
+        createLoc("OCNA SIBIULUI", "oras", [
+            createLoc("Topârcea", "sat"),
+        ]),
+    ]),
 
-			
+    createOras("SĂLIȘTE", [
+        createLoc("SĂLIȘTE", "oras", [
+            createLoc("Aciliu", "sat"),
+            createLoc("Amnaș", "sat"),
+            createLoc("Fântânele", "sat"),
+            createLoc("Galeș", "sat"),
+            createLoc("Mag", "sat"),
+            createLoc("Săcel", "sat"),
+            createLoc("Sibiel", "sat"),
+            createLoc("Vale", "sat"),
+        ]),
+    ]),
+
+    createOras("TĂLMACIU", [
+        createLoc("TĂLMACIU", "oras", [
+            createLoc("Colonia Tălmaciu", "sat"),
+            createLoc("Tălmăcel", "sat"),
+        ]),
+    ]),
 ];
 
 ?>
