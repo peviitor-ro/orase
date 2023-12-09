cd4a1783d1affd3d08cd7c66d0a848b5e326d653
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:18:03 +0200

    Update orase.php

--- ROMANIA/HARGHITA/orase.php
+++ ROMANIA/HARGHITA/orase.php
@@ -1,6 +1,30 @@
 <?php
 $orase=[
-						  
+		createOras("BĂILE TUȘNAD",[
+			    createLoc("BĂILE TUȘNAD", "oras",[
+				createLoc("Carpitus", "sat"),
+				  ]),				  
+			]),
+
+		createOras("BĂLAN",[
+			    createLoc("BĂLAN", "oras"),				  
+			]),
+		createOras("BORSEC",[
+			    createLoc("BORSEC", "oras"),				  
+			]),
+		createOras("CRISTURU SECUIESC",[
+			    createLoc("CRISTURU SECUIESC", "oras",[
+				createLoc("Betești", "sat"),
+				createLoc("Filiaș", "sat"),
+				  ]),				  
+			]),
+
+		createOras("VLĂHIȚA",[
+			    createLoc("VLĂHIȚA", "oras",[
+				createLoc("Băile Homorod", "sat"),
+				createLoc("Minele Lueta", "sat"),
+				  ]),				  
+			]),
 			
 ];
 
