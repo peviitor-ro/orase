01c73b0373458feae66c4f5e8725d82d5f61516f
RaresCode <irimusrares7@gmail.com>
2023-12-09 21:08:58 +0200

    Update orase.php

--- ROMANIA/VASLUI/orase.php
+++ ROMANIA/VASLUI/orase.php
@@ -1,6 +1,25 @@
 <?php
 $orase=[
-						  
+			createOras("MURGENI", [
+			    createLoc("MURGENI", "oras",[
+					createLoc("Cârja", "sat"),
+					createLoc("Floreni", "sat"),
+					createLoc("Lățești", "sat"),
+				      	createLoc("Raiu", "sat"),
+				      	createLoc("Sărățeni", "sat"),
+				      	createLoc("Schineni", "sat"),
+										]),
+			                     ]),
+			createOras("NEGREȘTI", [
+			    createLoc("NEGREȘTI", "oras",[
+					createLoc("Căzănești", "sat"),
+					createLoc("Cioatele", "sat"),
+					createLoc("Glodeni", "sat"),
+					createLoc("Parpanița", "sat"),
+				      	createLoc("Poiana", "sat"),
+				      	createLoc("Valea Mare", "sat"),
+										]),
+			                     ]),					  
 			
 ];
 
