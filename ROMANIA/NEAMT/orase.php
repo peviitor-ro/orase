e35b237545edbf0a9227401a32dbed2c98461fd7
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 23:00:58 +0200

    Update orase.php

--- ROMANIA/NEAMT/orase.php
+++ ROMANIA/NEAMT/orase.php
@@ -1,7 +1,30 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("BICAZ", [
+        createLoc("BICAZ", "oras", [
+            createLoc("Izvoru Alb", "sat"),
+   	    createLoc("Potoci", "sat"),      
+        ]),
+	       createLoc("Capșa", "sat"),
+	       createLoc("Dodeni", "sat"),
+	       createLoc("Izvoru Muntelui", "sat"),
+    ]),
+
+    createOras("TÂRGU-NEAMȚ", [
+        createLoc("TÂRGU-NEAMȚ", "oras"),
+            createLoc("Blebea", "sat"),
+            createLoc("Humulești", "sat"),
+            createLoc("Humuleștii Noi", "sat"),
+        
+    ]),
+
+    createOras("ROZNOV", [
+        createLoc("ROZNOV", "oras", [
+            createLoc("Chintinici", "sat"),
+            createLoc("Slobozia", "sat"),
+        ]),
+    ]),
 ];
+
 
 ?>
