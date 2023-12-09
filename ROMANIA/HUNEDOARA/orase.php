765877e888e527bd856ca2694d9b7d49d58b5f63
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:34:33 +0200

    Update orase.php

--- ROMANIA/HUNEDOARA/orase.php
+++ ROMANIA/HUNEDOARA/orase.php
@@ -1,76 +1,76 @@
 <?php
 $orase = [
     createOras("ANINOASA", [
-        createLocatie("ANINOASA", "oras", [
-            createLocatie("Iscroni", "sat"),
+        createLoc("ANINOASA", "oras", [
+            createLoc("Iscroni", "sat"),
         ]),
     ]),
 
     createOras("CĂLAN", [
-        createLocatie("CĂLAN", "oras", [
-            createLocatie("Batiz", "sat"),      
-            createLocatie("Călanu Mic", "sat"),
-            createLocatie("Grid", "sat"),
-            createLocatie("Nădăștia de Jos", "sat"),
-            createLocatie("Nădăștia de Sus", "sat"),
-            createLocatie("Ohaba Streiului", "sat"),
-            createLocatie("Sâncrai", "sat"),
-            createLocatie("Sântămăria de Piatră", "sat"),
-            createLocatie("Strei", "sat"),
-            createLocatie("Strei-Săcel", "sat"),
-            createLocatie("Valea Sângeorgiului", "sat"),
+        createLoc("CĂLAN", "oras", [
+            createLoc("Batiz", "sat"),      
+            createLoc("Călanu Mic", "sat"),
+            createLoc("Grid", "sat"),
+            createLoc("Nădăștia de Jos", "sat"),
+            createLoc("Nădăștia de Sus", "sat"),
+            createLoc("Ohaba Streiului", "sat"),
+            createLoc("Sâncrai", "sat"),
+            createLoc("Sântămăria de Piatră", "sat"),
+            createLoc("Strei", "sat"),
+            createLoc("Strei-Săcel", "sat"),
+            createLoc("Valea Sângeorgiului", "sat"),
         ]),
-	        createLocatie("Streisângeorgiu", "sat"),
+        createLoc("Streisângeorgiu", "sat"),
     ]),
 
     createOras("GEOAGIU", [
-        createLocatie("GEOAGIU", "oras", [
-            createLocatie("Aurel Vlaicu", "sat"),
-            createLocatie("Băcâia", "sat"),
-            createLocatie("Bozeș", "sat"),
-            createLocatie("Cigmău", "sat"),
-            createLocatie("Gelmar", "sat"),
-            createLocatie("Geoagiu-Băi", "sat"),
-            createLocatie("Homorod", "sat"),
-            createLocatie("Mermezeu-Văleni", "sat"),
-            createLocatie("Renghet", "sat"),
-            createLocatie("Văleni", "sat"),
+        createLoc("GEOAGIU", "oras", [
+            createLoc("Aurel Vlaicu", "sat"),
+            createLoc("Băcâia", "sat"),
+            createLoc("Bozeș", "sat"),
+            createLoc("Cigmău", "sat"),
+            createLoc("Gelmar", "sat"),
+            createLoc("Geoagiu-Băi", "sat"),
+            createLoc("Homorod", "sat"),
+            createLoc("Mermezeu-Văleni", "sat"),
+            createLoc("Renghet", "sat"),
+            createLoc("Văleni", "sat"),
         ]),
     ]),
 
     createOras("HAȚEG", [
-        createLocatie("HAȚEG", "oras", [
-            createLocatie("Silvașu de Jos", "sat"),           
-            createLocatie("Silvașu de Sus", "sat"),
+        createLoc("HAȚEG", "oras", [
+            createLoc("Silvașu de Jos", "sat"),           
+            createLoc("Silvașu de Sus", "sat"),
         ]),
-	       createLocatie("Nălațvad", "sat"),
+        createLoc("Nălațvad", "sat"),
     ]),
 
     createOras("PETRILA", [
-        createLocatie("PETRILA", "oras"),
-	       createLocatie("Cimpa", "sat"),
-	       createLocatie("Jieț", "sat"),
-	       createLocatie("Răscoala", "sat"),
-	       createLocatie("Tirici", "sat"),
+        createLoc("PETRILA", "oras"),
+        createLoc("Cimpa", "sat"),
+        createLoc("Jieț", "sat"),
+        createLoc("Răscoala", "sat"),
+        createLoc("Tirici", "sat"),
     ]),
 
     createOras("SIMERIA", [
-        createLocatie("SIMERIA", "oras", [
-            createLocatie("Bârcea Mare", "sat"),
-            createLocatie("Cărpiniș", "sat"),
-            createLocatie("Simeria Veche", "sat"),
-            createLocatie("Sântandrei", "sat"),
-            createLocatie("Șăulești", "sat"),
-            createLocatie("Uroi", "sat"),
+        createLoc("SIMERIA", "oras", [
+            createLoc("Bârcea Mare", "sat"),
+            createLoc("Cărpiniș", "sat"),
+            createLoc("Simeria Veche", "sat"),
+            createLoc("Sântandrei", "sat"),
+            createLoc("Șăulești", "sat"),
+            createLoc("Uroi", "sat"),
         ]),
     ]),
 
     createOras("URICANI", [
-        createLocatie("URICANI", "oras"),
-	       createLocatie("Câmpu lui Neag", "sat"),
-	       createLocatie("Valea de Brazi", "sat"),
+        createLoc("URICANI", "oras"),
+        createLoc("Câmpu lui Neag", "sat"),
+        createLoc("Valea de Brazi", "sat"),
     ]),
-	
 ];
+
 
 ?>
