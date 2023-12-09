91b8285c8b3ff1c62032a71420472df6bd9ade6b
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 19:25:28 +0200

    Update orase.php

--- ROMANIA/COVASNA/orase.php
+++ ROMANIA/COVASNA/orase.php
@@ -1,6 +1,29 @@
 <?php
 $orase=[
-						  
+            createOras("BARAOLT",[
+			    createLoc("BARAOLT", "oras",[
+						createLoc("Biborțeni", "sat"),
+						createLoc("Bodoș", "sat"),
+						createLoc("Căpeni", "sat"),
+						createLoc("Micloșoara", "sat"),
+						createLoc("Racoșul de Sus", "sat"),
+										]),
+				
+				  ]),
+            createOras("COVASNA",[
+			    createLoc("COVASNA", "oras"),
+				createLoc("Chiuruș", "sat"),
+										
+				
+				  ]),
+            createOras("ÎNTORSURA BUZĂULUI",[
+			    createLoc("ÎNTORSURA BUZĂULUI", "oras",[
+						createLoc("Brădet", "sat"),
+						createLoc("Floroaia", "sat"),
+						createLoc("Scrădoasa", "sat"),
+										]),
+				
+				  ]),				  
 			
 ];
 
