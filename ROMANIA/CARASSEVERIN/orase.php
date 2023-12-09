25a424f3cd7829bcfd4b295029a78d9e3f413c97
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 18:57:00 +0200

    Update orase.php

--- ROMANIA/CARASSEVERIN/orase.php
+++ ROMANIA/CARASSEVERIN/orase.php
@@ -1,6 +1,42 @@
 <?php
 $orase=[
-						  
+
+            createOras("ANINA",[
+			    createLoc("ANINA", "oras"),
+				createLoc("Steierdorf ", "sat"),
+				  ]),
+
+            createOras("BĂILE HERCULANE",[
+			    createLoc("BĂILE HERCULANE", "oras"),
+				createLoc("Pecinișca", "sat"),
+				  ]),
+
+            createOras("BOCȘA",[
+			    createLoc("BOCȘA", "oras"),
+				  ]),
+
+            createOras("MOLDOVA NOUĂ",[
+			    createLoc("MOLDOVA NOUĂ", "oras"),
+				createLoc("Măcești", "sat"),
+				createLoc("Moldova Veche", "sat"),
+				createLoc("Moldovița", "sat"),
+				  ]),
+
+            createOras("ORAVIȚA",[
+			    createLoc("ORAVIȚA", "oras",[
+						createLoc("Agadici", "sat"),
+						createLoc("Brădișoru de Jos", "sat"),
+						createLoc("Broșteni", "sat"),
+						createLoc("Răchitova", "sat"),
+										]),
+				  ]),
+
+            createOras("OȚELU ROȘU",[
+			    createLoc("OȚELU ROȘU", "oras",[
+						createLoc("Mal", "sat"),
+										]),
+				createLoc("Cireșa", "sat"),
+				  ]),				  
 			
 ];
 
