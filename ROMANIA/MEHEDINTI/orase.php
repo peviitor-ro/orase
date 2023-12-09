379379ddc61f5000b2ac0ae2c28f81bb769601f3
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:53:34 +0200

    Update orase.php

--- ROMANIA/MEHEDINTI/orase.php
+++ ROMANIA/MEHEDINTI/orase.php
@@ -1,7 +1,40 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("BAIA DE ARAMĂ", [
+        createLoc("BAIA DE ARAMĂ", "oras", [
+            createLoc("Bratilovu", "sat"),
+            createLoc("Brebina", "sat"),
+            createLoc("Dealu Mare", "sat"),
+            createLoc("Mărășești", "sat"),
+            createLoc("Negoești", "sat"),
+            createLoc("Pistrița", "sat"),
+            createLoc("Stănești", "sat"),
+            createLoc("Titerlești", "sat"),
+        ]),
+    ]),
+    createOras("STREHAIA", [
+        createLoc("STREHAIA", "oras", [
+            createLoc("Menți", "sat"),
+            createLoc("Motruleni", "sat"),
+            createLoc("Stăncești", "sat"),
+        ]),
+        createLoc("Ciochiuța", "sat"),
+        createLoc("Comanda", "sat"),
+        createLoc("Hurducești", "sat"),
+        createLoc("Lunca Banului", "sat"),
+        createLoc("Slătinicu Mare", "sat"),
+        createLoc("Slătinicu Mic", "sat"),
+    ]),
+    createOras("VÂNJU MARE", [
+        createLoc("VÂNJU MARE", "oras", [
+            createLoc("Bucura", "sat"),
+            createLoc("Nicolae Bălcescu", "sat"),
+            createLoc("Orevița Mare", "sat"),
+            createLoc("Traian", "sat"),
+        ]),
+    ]),
 ];
+
+
 
 ?>
