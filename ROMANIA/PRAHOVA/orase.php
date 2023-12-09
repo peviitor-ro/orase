38e01131293c005bee201312bc891404d7de31ec
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 23:06:45 +0200

    Update orase.php

--- ROMANIA/PRAHOVA/orase.php
+++ ROMANIA/PRAHOVA/orase.php
@@ -1,7 +1,93 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("AZUGA", [
+        createLoc("AZUGA", "oras"),
+    ]),
+
+    createOras("BĂICOI", [
+        createLoc("BĂICOI", "oras"),
+            createLoc("Dâmbu", "sat"),
+            createLoc("Liliești", "sat"),
+            createLoc("Schela", "sat"),
+            createLoc("Tufeni", "sat"),
+            createLoc("Țintea", "sat"),
+        
+    ]),
+
+    createOras("BOLDEȘTI-SCĂENI", [
+        createLoc("BOLDEȘTI-SCĂENI", "oras"),
+        createLoc("Seciu", "sat"),
+    ]),
+
+    createOras("BREAZA", [
+        createLoc("BREAZA DE JOS", "oras"),
+        createLoc("Breaza de Sus", "sat"),
+        createLoc("Frăsinet", "sat"),
+        createLoc("Gura Beliei", "sat"),
+        createLoc("Irimești", "sat"),
+        createLoc("Nistorești", "sat"),
+        createLoc("Podu Corbului", "sat"),
+        createLoc("Podu Vadului", "sat"),
+        createLoc("Surdești", "sat"),
+        createLoc("Valea Târsei", "sat"),
+    ]),
+
+    createOras("BUȘTENI", [
+        createLoc("BUȘTENI", "oras"),
+        createLoc("Poiana Țapului", "sat"),
+    ]),
+
+    createOras("COMARNIC", [
+        createLoc("COMARNIC", "oras"),
+        createLoc("Ghioșești", "sat"),
+        createLoc("Podu Lung", "sat"),
+        createLoc("Poiana", "sat"),
+        createLoc("Posada", "sat"),
+    ]),
+
+    createOras("MIZIL", [
+        createLoc("MIZIL", "oras"),
+        createLoc("Fefelei", "sat"),
+    ]),
+
+    createOras("PLOPENI", [
+        createLoc("PLOPENI", "oras"),
+    ]),
+
+    createOras("SINAIA", [
+        createLoc("SINAIA", "oras"),
+    ]),
+
+    createOras("SLĂNIC", [
+        createLoc("SLĂNIC", "oras"),
+        createLoc("Groșani", "sat"),
+        createLoc("Prăjani", "sat"),
+    ]),
+
+    createOras("URLAȚI", [
+        createLoc("URLAȚI", "oras"),
+        createLoc("Arioneștii Noi", "sat"),
+        createLoc("Arioneștii Vechi", "sat"),
+        createLoc("Cherba", "sat"),
+        createLoc("Jercălăi", "sat"),
+        createLoc("Mărunțiș", "sat"),
+        createLoc("Orzoaia de Jos", "sat"),
+        createLoc("Orzoaia de Sus", "sat"),
+        createLoc("Schiau", "sat"),
+        createLoc("Ulmi", "sat"),
+        createLoc("Valea Bobului", "sat"),
+        createLoc("Valea Crângului", "sat"),
+        createLoc("Valea Mieilor", "sat"),
+        createLoc("Valea Nucetului", "sat"),
+        createLoc("Valea Pietrei", "sat"),
+        createLoc("Valea Seman", "sat"),
+        createLoc("Valea Urloii", "sat"),
+    ]),
+
+    createOras("VĂLENII DE MUNTE", [
+        createLoc("VĂLENII DE MUNTE", "oras"),
+    ]),
 ];
+
 
 ?>
