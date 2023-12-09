e5c5ab467370222290a6bac1e2a3b60044495a0e
RaresCode <irimusrares7@gmail.com>
2023-12-09 21:28:18 +0200

    Update orase.php timis

--- ROMANIA/TIMIS/orase.php
+++ ROMANIA/TIMIS/orase.php
@@ -1,6 +1,65 @@
 <?php
 $orase=[
-						  
+			createOras("BUZIAȘ", [
+				createLoc("BUZIAȘ", "oras",[
+					createLoc("Bacova", "sat"),
+										]),
+				createLoc("Silagiu", "sat"),
+			
+			                     ]),
+			createOras("CIACOVA", [
+			    createLoc("CIACOVA", "oras",[
+					createLoc("Cebza", "sat"),
+					createLoc("Macedonia", "sat"),
+					createLoc("Obad", "sat"),
+					createLoc("Petroman", "sat"),
+										]),
+			                     ]),	
+	
+			createOras("DETA", [
+				createLoc("DETA", "oras",[
+					createLoc("Opatița", "sat"),
+										]),
+			createOras("FĂGET", [
+			    createLoc("FĂGET", "oras",[
+					createLoc("Bătești", "sat"),
+					createLoc("Begheiu Mic", "sat"),
+					createLoc("Bichigi", "sat"),
+					createLoc("Brănești", "sat"),
+				      	createLoc("Bunea Mare", "sat"),
+				      	createLoc("Bunea Mică", "sat"),
+				      	createLoc("Colonia Mică", "sat"),
+				      	createLoc("Jupânești", "sat"),
+				      	createLoc("Povârgina", "sat"),
+				      	createLoc("Temerești ", "sat"),
+										]),
+			                     ]),
+				   
+			createOras("GĂTAIA", [
+			    createLoc("GĂTAIA", "oras",[
+					createLoc("Butin", "sat"),
+					createLoc("Percosova", "sat"),
+					createLoc("Sculia", "sat"),
+					createLoc("Șemlacu Mare", "sat"),
+				      	createLoc("Șemlacu Mic", "sat"),
+										]),
+			                     ]),
+			createOras("JIMBOLIA", [
+				createLoc("JIMBOLIA", "oras"),
+										]),
+			createOras("RECAȘ", [
+			    createLoc("RECAȘ", "oras",[
+					createLoc("Bazoș", "sat"),
+					createLoc("Herneacova", "sat"),
+					createLoc("Izvin", "sat"),
+					createLoc("Nadăș", "sat"),
+				      	createLoc("Petrovaselo", "sat"),
+				      	createLoc("Stanciova", "sat"),
+										]),
+			                     ]),
+			createOras("SÂNNICOLAU MARE", [
+				createLoc("SÂNNICOLAU MARE", "oras"),
+										]),
 			
 ];
 
