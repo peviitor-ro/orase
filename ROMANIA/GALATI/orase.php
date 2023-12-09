86dc7e5e282fab2b7bb1ec0a931d521d1bacc233
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 21:37:12 +0200

    Update orase.php

--- ROMANIA/GALATI/orase.php
+++ ROMANIA/GALATI/orase.php
@@ -1,6 +1,15 @@
 <?php
 $orase=[
-						  
+
+	        createOras("BEREȘTI",[
+			    createLoc("BEREȘTI", "oras"),				  
+			]),
+		createOras("TÂRGU BUJOR",[
+			    createLoc("TÂRGU BUJOR", "oras",[
+						createLoc("Moscu", "sat"),
+						createLoc("Umbrărești", "sat"),
+				  ]),				  
+			]),
 			
 ];
 
