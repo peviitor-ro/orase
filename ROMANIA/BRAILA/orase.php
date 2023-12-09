d0b1f2347be55917a4fe4e805a46041a2f424da0
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 18:35:05 +0200

    Update orase.php

--- ROMANIA/BRAILA/orase.php
+++ ROMANIA/BRAILA/orase.php
@@ -1,6 +1,24 @@
 <?php
 $orase=[
-						  
+            createOras("FĂUREI",[
+			    createLoc("FĂUREI", "oras",),
+
+            createOras("IANCA",[
+			    createLoc("IANCA", "oras",[
+						createLoc("Berlești", "sat"),
+						createLoc("Gara Ianca", "sat"),
+						createLoc("Oprișenești", "sat"),
+						createLoc("Perișoru", "sat"),
+						createLoc("Plopu", "sat"),
+						createLoc("Târlele Filiu", "sat"),
+				  ]),
+
+            createOras("ÎNSURĂȚEI",[
+			    createLoc("ÎNSURĂȚEI", "oras",[
+						createLoc("Lacu Rezii", "sat"),
+						createLoc("Măru Roșu", "sat"),
+						createLoc("Valea Călmățuiului", "sat"),
+				  ]),				  
 			
 ];
 
