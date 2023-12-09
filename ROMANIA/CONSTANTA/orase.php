0b98a6bc5041fef030f8b8914d7b437ebee6d668
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 19:16:37 +0200

    Update orase.php

--- ROMANIA/CONSTANTA/orase.php
+++ ROMANIA/CONSTANTA/orase.php
@@ -1,6 +1,44 @@
 <?php
 $orase=[
-						  
+            createOras("CERNAVODĂ",[
+			    createLoc("CERNAVODĂ", "oras",[
+										]),
+				
+				  ]),	
+            createOras("EFORIE",[
+			    createLoc("EFORIE SUD", "oras"),
+				createLoc("Eforie Nord", "sat"),
+				  ]),	
+            createOras("HÂRȘOVA",[
+			    createLoc("HÂRȘOVA", "oras"),
+						createLoc("Vadu Oii", "sat"),
+				  ]),	
+            createOras("MURFATLAR",[
+			    createLoc("MURFATLAR", "oras"),
+				createLoc("Siminoc", "sat"),
+				  ]),
+            createOras("NĂVODARI",[
+			    createLoc("NĂVODARI", "oras"),
+				createLoc("Mamaia-Sat", "sat"),
+				  ]),	
+            createOras("NEGRU VODĂ",[
+			    createLoc("NEGRU VODĂ", "oras",[
+						createLoc("Darabani", "sat"),
+						createLoc("Grăniceru", "sat"),
+						createLoc("Vâlcelele", "sat"),
+										]),
+				
+				  ]),	
+            createOras("OVIDIU",[
+			    createLoc("OVIDIU", "oras",[
+						createLoc("Culmea", "sat"),
+						createLoc("Poiana", "sat"),
+										]),
+				
+				  ]),	
+            createOras("TECHIRGHIOL",[
+			    createLoc("TECHIRGHIOL", "oras"),		
+				  ]),				  
 			
 ];
 
