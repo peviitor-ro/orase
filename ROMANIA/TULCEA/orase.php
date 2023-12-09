bbafdd33221fdb0d2ce02f19518146c13376c9bc
RaresCode <irimusrares7@gmail.com>
2023-12-09 21:18:39 +0200

    Update orase.php tulcea

--- ROMANIA/TULCEA/orase.php
+++ ROMANIA/TULCEA/orase.php
@@ -1,6 +1,19 @@
 <?php
 $orase=[
-						  
+			createOras("BABADAG", [
+			    	createLoc("BABADAG", "oras"),
+			                     ]),
+			createOras("ISACCEA", [
+				createLoc("ISACCEA", "oras"),
+				createLoc("Revărsarea", "oras"),
+				createLoc("Tichilești", "oras"),
+			                     ]),	
+			createOras("MĂCIN", [
+			    	createLoc("MĂCIN", "oras"),
+			                     ]),
+			createOras("SULINA", [
+			    	createLoc("SULINA", "oras"),
+			                     ]),
 			
 ];
 
