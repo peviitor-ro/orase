392c16bbb6151eb2835239855b3af62c58843b7a
RaresCode <irimusrares7@gmail.com>
2023-12-09 23:58:52 +0200

    Update orase.php Salaj

--- ROMANIA/SALAJ/orase.php
+++ ROMANIA/SALAJ/orase.php
@@ -1,6 +1,35 @@
 <?php
 $orase=[
-						  
+
+    createOras("CEHU SILVANIEI", [
+        createLoc("CEHU SILVANIEI", "oras", [
+            createLoc("Horoatu Cehului", "sat"),
+            createLoc("Motiș", "sat"),
+            createLoc("Nadiș", "sat"),
+            createLoc("Ulciug", "sat"),
+		  
+        ]),
+	
+    ]),
+	
+    createOras("JIBOU", [
+        createLoc("JIBOU", "oras", [
+            createLoc("Cuceu", "sat"),
+            createLoc("Husia", "sat"),
+            createLoc("Rona", "sat"),
+            createLoc("Var", "sat"),
+		  
+        ]),
+	
+    ]),
+    createOras("ȘIMLEU SILVANIEI", [
+        createLoc("ȘIMLEU SILVANIEI", "oras"),
+	createLoc("Bic", "sat"),
+	createLoc("Cehei", "sat"),
+	createLoc("Pusta", "sat"),
+	       
+	
+    ]),
 			
 ];
 
