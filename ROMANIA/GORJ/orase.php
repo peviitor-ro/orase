7e3df3473c2e89ac7d1f30bbba8ad01512adf808
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 21:56:12 +0200

    Update orase.php

--- ROMANIA/GORJ/orase.php
+++ ROMANIA/GORJ/orase.php
@@ -1,6 +1,69 @@
 <?php
 $orase=[
-						  
+
+		        createOras("BUMBEȘTI-JIU",[
+			    createLoc("BUMBEȘTI-JIU", "oras",[
+						createLoc("Curtișoara", "sat"),
+						createLoc("Lăzărești", "sat"),
+				      		createLoc("Pleșa ", "sat"),
+				      		createLoc("Tetila ", "sat"),
+				  ]),				  
+			]),
+		createOras("NOVACI",[
+			    createLoc("NOVACI", "oras",[
+						createLoc("Bercești", "sat"),
+						createLoc("Hirișești", "sat"),
+				      		createLoc("Pociovaliștea", "sat"),
+				      		createLoc("Sitești", "sat"),
+				  ]),				  
+			]),
+	        createOras("ROVINARI",[
+			createLoc("ROVINARI", "oras"),
+			createLoc("Vârț", "sat"),
+				  ]),				  
+			]),
+		createOras("TISMANA",[
+			    createLoc("TISMANA", "oras",[
+				createLoc("Celei", "sat"),
+				createLoc("Costeni", "sat"),
+				createLoc("Gornovița ", "sat"),
+				createLoc("Isvarna", "sat"),
+				createLoc("Pocruia", "sat"),
+				createLoc("Racoți", "sat"),
+				createLoc("Sohodol", "sat"),
+				createLoc("Topești", "sat"),
+				createLoc("Vâlcele", "sat"),
+				createLoc("Vânăta", "sat"),
+				  ]),				  
+			]),
+
+	        createOras("TÂRGU CĂRBUNEȘTI",[
+			    createLoc("TÂRGU CĂRBUNEȘTI", "oras",[
+				createLoc("Blahnița de Jos", "sat"),
+				createLoc("Cărbunești-Sat", "sat"),
+				createLoc("Cojani", "sat"),
+				createLoc("Crețești", "sat"),
+				createLoc("Curteana", "sat"),
+			      createLoc("Floreșteni", "sat"),
+			      createLoc("Măceșu", "sat"),
+			      createLoc("Floreșteni", "sat"),
+			      createLoc("Pojogeni", "sat"),
+				      createLoc("Rogojeni", "sat"),
+				      createLoc("Ștefănești", "sat"),
+				  ]),				  
+			]),
+		createOras("TURCENI",[
+			    createLoc("TURCENI", "oras",[
+						createLoc("Gârbovu", "sat"),
+						createLoc("Murgești", "sat"),
+				      		createLoc("Strâmba-Jiu", "sat"),
+				      createLoc("Valea Viei", "sat"),
+				  ]),				  
+			]),
+	        createOras("ȚICLENI",[
+			    createLoc("ȚICLENI", "oras"),				  
+			]),
+		
 			
 ];
 
