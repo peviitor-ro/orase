0a68a2bb0fb9fd36bfdce4ce6ea054076a4fda47
RaresCode <irimusrares7@gmail.com>
2023-12-09 20:36:47 +0200

    Update orase.php

--- ROMANIA/VRANCEA/orase.php
+++ ROMANIA/VRANCEA/orase.php
@@ -1,7 +1,31 @@
 <?php
 $orase=[
-						  
-			
+			createOras("MĂRĂȘEȘTI", [
+			    createLoc("MĂRĂȘEȘTI", "oras",[
+					createLoc("Călimănești", "sat"),
+					createLoc("Haret", "sat"),
+					createLoc("Modruzeni", "sat"),
+				      	createLoc("Pădureni", "sat"),
+				      
+										]),
+				createLoc("Siretu", "sat"),
+				createLoc("Tișița", "sat"),
+			                     ]),
+			createOras("ODOBEȘTI", [
+			    createLoc("ODOBEȘTI", "oras",[
+					createLoc("Unirea", "sat"),
+										]),
+			                     ]),
+			createOras("PANCIU", [
+			    	createLoc("PANCIU", "oras"),
+				   
+				createLoc("Crucea de Jos", "sat"),
+				createLoc("Crucea de Sus", "sat"),
+				createLoc("Dumbrava", "sat"),
+				createLoc("Neicu", "sat"),
+				createLoc("Satu Nou", "sat"),
+										]),
+			                     ]),
 ];
 
 ?>
