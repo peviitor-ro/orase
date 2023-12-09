9abc9946ed27448f03e55116ce801f70b637f424
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-09 22:58:37 +0200

    Update orase.php

--- ROMANIA/MURES/orase.php
+++ ROMANIA/MURES/orase.php
@@ -1,7 +1,78 @@
 <?php
-$orase=[
-						  
-			
+$orase = [
+    createOras("IERNUT", [
+        createLoc("IERNUT", "oras", [
+            createLoc("Cipău", "sat"),
+            createLoc("Deag", "sat"),
+            createLoc("Lechința", "sat"),
+            createLoc("Oarba de Mureș", "sat"),
+            createLoc("Porumbac", "sat"),
+            createLoc("Racameț", "sat"),
+            createLoc("Sălcud", "sat"),
+            createLoc("Sfântu Gheorghe", "sat"),
+        ]),
+    ]),
+
+    createOras("LUDUȘ", [
+        createLoc("LUDUȘ", "oras"),
+        createLoc("Avrămești", "sat"),
+        createLoc("Cioarga", "sat"),
+        createLoc("Ciurgău", "sat"),
+        createLoc("Fundătura", "sat"),
+        createLoc("Gheja", "sat"),
+        createLoc("Roșiori", "sat"),
+    ]),
+
+    createOras("MIERCUREA NIRAJULUI", [
+        createLoc("MIERCUREA NIRAJULUI", "oras", [
+            createLoc("Beu", "sat"),
+            createLoc("Dumitreștii", "sat"),
+            createLoc("Lăureni", "sat"),
+            createLoc("Moșuni", "sat"),
+            createLoc("Șardu Nirajului", "sat"),
+            createLoc("Tâmpa", "sat"),
+            createLoc("Veța", "sat"),
+        ]),
+    ]),
+
+    createOras("SĂRMAȘU", [
+        createLoc("SĂRMAȘU", "oras", [
+            createLoc("Balda", "sat"),
+            createLoc("Larga", "sat"),
+            createLoc("Moruț", "sat"),
+            createLoc("Sărmășel", "sat"),
+            createLoc("Sarmășel-Gară", "sat"),
+            createLoc("Titiana", "sat"),
+            createLoc("Vișinelu", "sat"),
+        ]),
+    ]),
+
+    createOras("SÂNGEORGIU DE PĂDURE", [
+        createLoc("SÂNGEORGIU DE PĂDURE", "oras", [
+            createLoc("Bezid", "sat"),
+            createLoc("Bezidu Nou", "sat"),
+            createLoc("Loțu", "sat"),
+        ]),
+    ]),
+
+    createOras("SOVATA", [
+        createLoc("SOVATA", "oras"),
+        createLoc("Căpeți", "sat"),
+        createLoc("Ilieși", "sat"),
+        createLoc("Săcădat", "sat"),
+    ]),
+
+    createOras("UNGHENI", [
+        createLoc("Ungheni", "oras", [
+            createLoc("Cerghid", "sat"),
+            createLoc("Cerghizel", "sat"),
+            createLoc("Morești", "sat"),
+            createLoc("Recea", "sat"),
+            createLoc("Șăușa", "sat"),
+            createLoc("Vidrasău", "sat"),
+        ]),
+    ]),
 ];
+
 
 ?>
