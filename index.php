fd0eeb5385a0a6e97b834720d27c80fb8362d087
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-30 15:45:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 // based on https://legislatie.just.ro/Public/DetaliiDocument/189
 // created by sebiboga && dianadascalu2
 
+header("Access-Control-Allow-Origin: *");
 require_once 'ROMANIA/index.php';
 ?>
