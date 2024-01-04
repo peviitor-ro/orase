9feb30b96cd1d584421a58d91a1c0635afe9895f
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-01-04 18:27:34 +0200

    Update index.php

--- ROMANIA/index.php
+++ ROMANIA/index.php
@@ -2,7 +2,7 @@
 // test on https://onlinephp.io/
 // based on https://legislatie.just.ro/Public/DetaliiDocument/189
 // created by sebiboga && dianadascalu2
-
+header("Access-Control-Allow-Origin: *");
 
 
 
