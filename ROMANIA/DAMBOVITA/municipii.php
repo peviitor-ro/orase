985b31a340c66d13ef2223b5a9e48c3ff7baa667
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 15:51:51 +0200

    fix: restore nested structure for TÂRGOVIȘTE in DAMBOVITA

--- ROMANIA/DAMBOVITA/municipii.php
+++ ROMANIA/DAMBOVITA/municipii.php
@@ -2,12 +2,13 @@
 $municipii =
 [
 	 createMunicipiu("TÂRGOVIȘTE", [
-		createLoc("TÂRGOVIȘTE", "oras", createAdresaCompleta("orasul TÂRGOVIȘTE, municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România")),
-		createLoc("Priseaca", "sat", createAdresaCompleta("sat Priseaca, municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România")),
-	], createAdresaCompleta("municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România)),
+		createLoc("TÂRGOVIȘTE", "oras", [
+			createLoc("Priseaca", "sat", createAdresaCompleta("sat Priseaca, municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România")),
+		], createAdresaCompleta("orasul TÂRGOVIȘTE, municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("municipiul TÂRGOVIȘTE, judetul DÂMBOVIȚA, România")),
 	 createMunicipiu("MORENI", [
 		createLoc("MORENI", "oras", createAdresaCompleta("orasul MORENI, municipiul MORENI, judetul DÂMBOVIȚA, România")),
-	], createAdresaCompleta("municipiul MORENI, judetul DÂMBOVIȚA, România)),
+	], createAdresaCompleta("municipiul MORENI, judetul DÂMBOVIȚA, România")),
 			
 ];
 ?>
