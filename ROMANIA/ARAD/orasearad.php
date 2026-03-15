6ff83f899b808ea43eeddd5b1815fe296d1c6675
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 10:54:54 +0200

    feat: aplicare adresaCompleta pentru judetul ARAD
    - Am adaugat adresaCompleta pentru toate localitatile din judetul ARAD
    - Municipii, orase, comune si sate

--- ROMANIA/ARAD/orasearad.php
+++ ROMANIA/ARAD/orasearad.php
@@ -1,45 +1,45 @@
 <?php
 $orase=[
-        createOras("CHIȘINEU-CRIȘ",[
-			createLoc("CHIȘINEU-CRIȘ", "oras"),
-			createLoc("Nădab", "sat"),
-						   ]),
-		createOras("CURTICI",[
-			createLoc("CURTICI", "oras"),
-						   ]),
-		createOras("INEU",[
-			createLoc("INEU", "oras"),
-			createLoc("Mocrea", "sat"),
-						   ]),
-		createOras("LIPOVA",[
-			createLoc("LIPOVA", "oras"),
-			createLoc("Radna", "sat"),
-			createLoc("Șoimoș", "sat"),
-						   ]),
-		createOras("NĂDLAC",[
-			createLoc("NĂDLAC", "oras"),
-						   ]),
-		createOras("PECICA",[
-			createLoc("PECICA", "oras"),
-			createLoc("Bodrogu Vechi", "sat"),
-			createLoc("Sederhat", "sat"),
-			createLoc("Turnu", "sat"),
-						   ]),
-		createOras("PÂNCOTA",[
-			createLoc("PÂNCOTA", "oras"),
-			createLoc("Măderat", "sat"),
-						   ]),
-		createOras("SÂNTANA",[
-			createLoc("SÂNTANA", "oras"),
-			createLoc("Caporal Alexa", "sat"),
-						   ]),
-		createOras("SEBIȘ",[
-			createLoc("SEBIȘ", "oras"),
-			createLoc("Donceni", "sat"),
-			createLoc("Prunișor", "sat"),
-			createLoc("Sălăjeni", "sat"),
-						   ]),
-			
+        createOras("CHIȘINEU-CRIȘ", [
+            createLoc("CHIȘINEU-CRIȘ", "oras", createAdresaCompleta("orasul CHIȘINEU-CRIȘ, judetul ARAD, România")),
+            createLoc("Nădab", "sat", createAdresaCompleta("sat Nădab, orasul CHIȘINEU-CRIȘ, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul CHIȘINEU-CRIȘ, judetul ARAD, România")),
+        createOras("CURTICI", [
+            createLoc("CURTICI", "oras", createAdresaCompleta("orasul CURTICI, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul CURTICI, judetul ARAD, România")),
+        createOras("INEU", [
+            createLoc("INEU", "oras", createAdresaCompleta("orasul INEU, judetul ARAD, România")),
+            createLoc("Mocrea", "sat", createAdresaCompleta("sat Mocrea, orasul INEU, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul INEU, judetul ARAD, România")),
+        createOras("LIPOVA", [
+            createLoc("LIPOVA", "oras", createAdresaCompleta("orasul LIPOVA, judetul ARAD, România")),
+            createLoc("Radna", "sat", createAdresaCompleta("sat Radna, orasul LIPOVA, judetul ARAD, România")),
+            createLoc("Șoimoș", "sat", createAdresaCompleta("sat Șoimoș, orasul LIPOVA, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul LIPOVA, judetul ARAD, România")),
+        createOras("NĂDLAC", [
+            createLoc("NĂDLAC", "oras", createAdresaCompleta("orasul NĂDLAC, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul NĂDLAC, judetul ARAD, România")),
+        createOras("PECICA", [
+            createLoc("PECICA", "oras", createAdresaCompleta("orasul PECICA, judetul ARAD, România")),
+            createLoc("Bodrogu Vechi", "sat", createAdresaCompleta("sat Bodrogu Vechi, orasul PECICA, judetul ARAD, România")),
+            createLoc("Sederhat", "sat", createAdresaCompleta("sat Sederhat, orasul PECICA, judetul ARAD, România")),
+            createLoc("Turnu", "sat", createAdresaCompleta("sat Turnu, orasul PECICA, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul PECICA, judetul ARAD, România")),
+        createOras("PÂNCOTA", [
+            createLoc("PÂNCOTA", "oras", createAdresaCompleta("orasul PÂNCOTA, judetul ARAD, România")),
+            createLoc("Măderat", "sat", createAdresaCompleta("sat Măderat, orasul PÂNCOTA, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul PÂNCOTA, judetul ARAD, România")),
+        createOras("SÂNTANA", [
+            createLoc("SÂNTANA", "oras", createAdresaCompleta("orasul SÂNTANA, judetul ARAD, România")),
+            createLoc("Caporal Alexa", "sat", createAdresaCompleta("sat Caporal Alexa, orasul SÂNTANA, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul SÂNTANA, judetul ARAD, România")),
+        createOras("SEBIȘ", [
+            createLoc("SEBIȘ", "oras", createAdresaCompleta("orasul SEBIȘ, judetul ARAD, România")),
+            createLoc("Donceni", "sat", createAdresaCompleta("sat Donceni, orasul SEBIȘ, judetul ARAD, România")),
+            createLoc("Prunișor", "sat", createAdresaCompleta("sat Prunișor, orasul SEBIȘ, judetul ARAD, România")),
+            createLoc("Sălăjeni", "sat", createAdresaCompleta("sat Sălăjeni, orasul SEBIȘ, judetul ARAD, România")),
+        ], createAdresaCompleta("orasul SEBIȘ, judetul ARAD, România")),
+            
 ];
 
 ?>
