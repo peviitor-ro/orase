3bd692de5a3ac2b627e2ca583b14ce0c9f844ccc
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 12:04:49 +0200

    Add adresaCompleta to BIHOR county

--- ROMANIA/BIHOR/municipii.php
+++ ROMANIA/BIHOR/municipii.php
@@ -2,20 +2,20 @@
 $municipii =
 [
 		createMunicipiu("ORADEA", [
-                    createLoc("ORADEA", "oras"),
-                ]),	
+                    createLoc("ORADEA", "oras", createAdresaCompleta("municipiul ORADEA, judetul BIHOR, România")),
+                ], createAdresaCompleta("municipiul ORADEA, judetul BIHOR, România")),	
 		createMunicipiu("BEIUȘ", [
-                    createLoc("BEIUȘ", "oras"),
-                    createLoc("Delani", "sat"),
-                ]),	
+                    createLoc("BEIUȘ", "oras", createAdresaCompleta("municipiul BEIUȘ, judetul BIHOR, România")),
+                    createLoc("Delani", "sat", createAdresaCompleta("sat Delani, municipiul BEIUȘ, judetul BIHOR, România")),
+                ], createAdresaCompleta("municipiul BEIUȘ, judetul BIHOR, România")),	
 		createMunicipiu("MARGHITA", [
-                    createLoc("MARGHITA", "oras"),
-                    createLoc("Cheț", "sat"),
-					createLoc("Ghenetea", "sat"),
-                ]),	
+                    createLoc("MARGHITA", "oras", createAdresaCompleta("municipiul MARGHITA, judetul BIHOR, România")),
+                    createLoc("Cheț", "sat", createAdresaCompleta("sat Cheț, municipiul MARGHITA, judetul BIHOR, România")),
+					createLoc("Ghenetea", "sat", createAdresaCompleta("sat Ghenetea, municipiul MARGHITA, judetul BIHOR, România")),
+                ], createAdresaCompleta("municipiul MARGHITA, judetul BIHOR, România")),	
 		createMunicipiu("SALONTA", [
-                    createLoc("SALONTA", "oras"),
-                ]),
+                    createLoc("SALONTA", "oras", createAdresaCompleta("municipiul SALONTA, judetul BIHOR, România")),
+                ], createAdresaCompleta("municipiul SALONTA, judetul BIHOR, România")),
 			
 ];
 ?>
