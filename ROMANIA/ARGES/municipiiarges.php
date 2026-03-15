b2d3b81719e42aabb81c33eaf47780546a76a7d4
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 11:00:25 +0200

    feat: aplicare adresaCompleta pentru judetul ARGES
    - municipiiarges.php - actualizat cu adresaCompleta
    - orasearges.php - actualizat cu adresaCompleta

--- ROMANIA/ARGES/municipiiarges.php
+++ ROMANIA/ARGES/municipiiarges.php
@@ -1,17 +1,17 @@
 <?php
 $municipii =
 [
-			createMunicipiu("PITEȘTI", [
-				createLoc("PITEȘTI", "oras"),
-			]),
-			createMunicipiu("CÂMPULUNG", [
-				createLoc("CÂMPULUNG", "oras"),
-				createLoc("Valea Rumâneștilor", "oras"),
-			]),
-			createMunicipiu("CURTEA DE ARGEȘ", [
-				createLoc("CURTEA DE ARGEȘ", "oras"),
-				createLoc("Noapteș", "oras"),
-			]),
-			
+        createMunicipiu("PITEȘTI", [
+            createLoc("PITEȘTI", "oras", createAdresaCompleta("orasul PITEȘTI, municipiul PITEȘTI, judetul ARGEȘ, România")),
+        ], createAdresaCompleta("municipiul PITEȘTI, judetul ARGEȘ, România")),
+        createMunicipiu("CÂMPULUNG", [
+            createLoc("CÂMPULUNG", "oras", createAdresaCompleta("orasul CÂMPULUNG, municipiul CÂMPULUNG, judetul ARGEȘ, România")),
+            createLoc("Valea Rumâneștilor", "oras", createAdresaCompleta("orasul Valea Rumâneștilor, municipiul CÂMPULUNG, judetul ARGEȘ, România")),
+        ], createAdresaCompleta("municipiul CÂMPULUNG, judetul ARGEȘ, România")),
+        createMunicipiu("CURTEA DE ARGEȘ", [
+            createLoc("CURTEA DE ARGEȘ", "oras", createAdresaCompleta("orasul CURTEA DE ARGEȘ, municipiul CURTEA DE ARGEȘ, judetul ARGEȘ, România")),
+            createLoc("Noapteș", "oras", createAdresaCompleta("orasul Noapteș, municipiul CURTEA DE ARGEȘ, judetul ARGEȘ, România")),
+        ], createAdresaCompleta("municipiul CURTEA DE ARGEȘ, judetul ARGEȘ, România")),
+        
 ];
 ?>
