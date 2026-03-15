27c0de6cee9c31b9d665532aabeb9ac375f6edd4
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 15:14:32 +0200

    feat: add adresaCompleta to DAMBOVITA county

--- ROMANIA/DAMBOVITA/orase.php
+++ ROMANIA/DAMBOVITA/orase.php
@@ -1,49 +1,42 @@
 <?php
 $orase=[
-            createOras("FIENI",[
-			    createLoc("FIENI", "oras",[
-						createLoc("Berevoești", "sat"),
-						createLoc("Costești", "sat"),
-										]),
-				
-				  ]),
-            createOras("GĂEȘTI",[
-			    createLoc("GĂEȘTI", "oras"),
-				
-				  ]),
-            createOras("PUCIOASA",[
-			    createLoc("PUCIOASA", "oras"),
-				createLoc("Bela", "sat"),
-				createLoc("Diaconești", "sat"),
-				createLoc("Glodeni", "sat"),
-				createLoc("Malurile", "sat"),
-				createLoc("Miculești", "sat"),
-				createLoc("Pucioasa-Sat", "sat"),
-										]),
-										
-			createOras("RĂCARI",[
-			    createLoc("RĂCARI", "oras"),
-				createLoc("Bălănești", "sat"),
-				createLoc("Colacu", "sat"),
-				createLoc("Ghergani", "sat"),
-				createLoc("Ghimpați", "sat"),
-				createLoc("Mavrodin", "sat"),
-				createLoc("Săbiești", "sat"),
-				createLoc("Stănești", "sat"),
-										
-				
-				  ]),
-            createOras("TITU",[
-			    createLoc("TITU", "oras",[
-						createLoc("Fusea", "sat"),
-						createLoc("Hagioaica", "sat"),
-						createLoc("Mereni", "sat"),
-						createLoc("Plopu", "sat"),
-						createLoc("Sălcuța", "sat"),
-										]),
-				
-				  ]),				  
-			
+	createOras("FIENI", [
+		createLoc("FIENI", "oras", [
+			createLoc("Berevoești", "sat", createAdresaCompleta("sat Berevoești, orasul FIENI, judetul DÂMBOVIȚA, România")),
+			createLoc("Costești", "sat", createAdresaCompleta("sat Costești, orasul FIENI, judetul DÂMBOVIȚA, România")),
+		], createAdresaCompleta("orasul FIENI, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("orasul FIENI, judetul DÂMBOVIȚA, România")),
+	createOras("GĂEȘTI", [
+		createLoc("GĂEȘTI", "oras", createAdresaCompleta("orasul GĂEȘTI, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("orasul GĂEȘTI, judetul DÂMBOVIȚA, România")),
+	createOras("PUCIOASA", [
+		createLoc("PUCIOASA", "oras", createAdresaCompleta("orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Bela", "sat", createAdresaCompleta("sat Bela, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Diaconești", "sat", createAdresaCompleta("sat Diaconești, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Glodeni", "sat", createAdresaCompleta("sat Glodeni, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Malurile", "sat", createAdresaCompleta("sat Malurile, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Miculești", "sat", createAdresaCompleta("sat Miculești, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+		createLoc("Pucioasa-Sat", "sat", createAdresaCompleta("sat Pucioasa-Sat, orasul PUCIOASA, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("orasul PUCIOASA, judetul DÂMBOVIȚA, România)),
+	createOras("RĂCARI", [
+		createLoc("RĂCARI", "oras", createAdresaCompleta("orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Bălănești", "sat", createAdresaCompleta("sat Bălănești, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Colacu", "sat", createAdresaCompleta("sat Colacu, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Ghergani", "sat", createAdresaCompleta("sat Ghergani, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Ghimpați", "sat", createAdresaCompleta("sat Ghimpați, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Mavrodin", "sat", createAdresaCompleta("sat Mavrodin, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Săbiești", "sat", createAdresaCompleta("sat Săbiești, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+		createLoc("Stănești", "sat", createAdresaCompleta("sat Stănești, orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("orasul RĂCARI, judetul DÂMBOVIȚA, România")),
+	createOras("TITU", [
+		createLoc("TITU", "oras", [
+			createLoc("Fusea", "sat", createAdresaCompleta("sat Fusea, orasul TITU, judetul DÂMBOVIȚA, România")),
+			createLoc("Hagioaica", "sat", createAdresaCompleta("sat Hagioaica, orasul TITU, judetul DÂMBOVIȚA, România")),
+			createLoc("Mereni", "sat", createAdresaCompleta("sat Mereni, orasul TITU, judetul DÂMBOVIȚA, România")),
+			createLoc("Plopu", "sat", createAdresaCompleta("sat Plopu, orasul TITU, judetul DÂMBOVIȚA, România")),
+			createLoc("Sălcuța", "sat", createAdresaCompleta("sat Sălcuța, orasul TITU, judetul DÂMBOVIȚA, România")),
+		], createAdresaCompleta("orasul TITU, judetul DÂMBOVIȚA, România")),
+	], createAdresaCompleta("orasul TITU, judetul DÂMBOVIȚA, România")),
 ];
 
 ?>
