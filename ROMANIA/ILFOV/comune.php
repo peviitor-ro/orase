6727b9df0e071e14f87a44c5979eb84d12136ce2
Boga Sebastian <sebitestb@gmail.com>
2026-03-15 15:22:00 +0200

    feat: add adresaCompleta to ILFOV county

--- ROMANIA/ILFOV/comune.php
+++ ROMANIA/ILFOV/comune.php
@@ -2,24 +2,29 @@
 $comune =[
 
     createComuna("1 Decembrie", [
+				createAdresaCompleta("ILFOV"),
         createLoc("1 Decembrie", "sat")
     ]),
     
     createComuna("Afumați", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Afumați", "sat")
     ]),
     
     createComuna("Balotești", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Balotești", "sat"),
         createLoc("Dumbrăveni", "sat"),
         createLoc("Săftica", "sat")
     ]),
     
     createComuna("Berceni", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Berceni", "sat")
     ]),
     
     createComuna("Brănești", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Brănești", "sat"),
         createLoc("Islaz", "sat"),
         createLoc("Pasărea", "sat"),
@@ -27,6 +32,7 @@ $comune =[
     ]),
     
     createComuna("Cernica", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Cernica", "sat"),
         createLoc("Bălăceanca", "sat"),
         createLoc("Căldăraru", "sat"),
@@ -35,12 +41,14 @@ $comune =[
     ]),
     
     createComuna("Chiajna", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Chiajna", "sat"),
         createLoc("Dudu", "sat"),
         createLoc("Roșu", "sat")
     ]),
     
     createComuna("Ciolpani", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Ciolpani", "sat"),
         createLoc("Izvorani", "sat"),
         createLoc("Lupăria", "sat"),
@@ -48,20 +56,24 @@ $comune =[
     ]),
     
     createComuna("Ciorogârla", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Ciorogârla", "sat"),
         createLoc("Dârvari", "sat")
     ]),
     
     createComuna("Clinceni", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Clinceni", "sat"),
         createLoc("Olteni", "sat"),
         createLoc("Ordoreanu", "sat")
     ]),
     createComuna("Copăceni", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Copăceni", "sat")
     ]),
     
     createComuna("Corbeanca", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Corbeanca", "sat"),
         createLoc("Ostratu", "sat"),
         createLoc("Petrești", "sat"),
@@ -69,11 +81,13 @@ $comune =[
     ]),
     
     createComuna("Cornetu", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Cornetu", "sat"),
         createLoc("Buda", "sat")
     ]),
     
     createComuna("Dascălu", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Dascălu", "sat"),
         createLoc("Creața", "sat"),
         createLoc("Gagu", "sat"),
@@ -81,26 +95,31 @@ $comune =[
     ]),
     
     createComuna("Dărăști-Ilfov", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Dărăști-Ilfov", "sat")
     ]),
     
     createComuna("Dobroești", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Dobroești", "sat"),
         createLoc("Fundeni", "sat")
     ]),
     
     createComuna("Domnești", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Domnești", "sat"),
         createLoc("Țegheș", "sat")
     ]),
     
     createComuna("Dragomirești-Vale", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Dragomirești-Vale", "sat"),
         createLoc("Dragomirești-Deal", "sat"),
         createLoc("Zurbaua", "sat")
     ]),
     
     createComuna("Găneasa", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Găneasa", "sat"),
         createLoc("Cozieni", "sat"),
         createLoc("Moara Domnească", "sat"),
@@ -109,16 +128,19 @@ $comune =[
     ]),
     
     createComuna("Glina", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Glina", "sat"),
         createLoc("Cățelu", "sat"),
         createLoc("Manolache", "sat")
     ]),
     createComuna("Grădiștea", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Grădiștea", "sat"),
         createLoc("Sitaru", "sat")
     ]),
     
     createComuna("Gruiu", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Gruiu", "sat"),
         createLoc("Lipia", "sat"),
         createLoc("Siliștea Snagovului", "sat"),
@@ -126,19 +148,23 @@ $comune =[
     ]),
     
     createComuna("Jilava", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Jilava", "sat")
     ]),
     
     createComuna("Moara Vlăsiei", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Moara Vlăsiei", "sat"),
         createLoc("Căciulați", "sat")
     ]),
     
     createComuna("Mogoșoaia", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Mogoșoaia", "sat")
     ]),
     
     createComuna("Nuci", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Nuci", "sat"),
         createLoc("Balta Neagră", "sat"),
         createLoc("Merii Petchii", "sat"),
@@ -147,12 +173,14 @@ $comune =[
     ]),
     
     createComuna("Periș", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Periș", "sat"),
         createLoc("Bălteni", "sat"),
         createLoc("Buriaș", "sat")
     ]),
     
     createComuna("Petrăchioaia", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Petrăchioaia", "sat"),
         createLoc("Măineasca", "sat"),
         createLoc("Surlari", "sat"),
@@ -160,6 +188,7 @@ $comune =[
     ]),
     
     createComuna("Snagov", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Snagov", "sat"),
         createLoc("Ciofliceni", "sat"),
         createLoc("Ghermănești", "sat"),
@@ -168,16 +197,19 @@ $comune =[
     ]),
     
     createComuna("Ștefăneștii de Jos", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Ștefăneștii de Jos", "sat"),
         createLoc("Crețuleasca", "sat"),
         createLoc("Ștefăneștii de Sus", "sat")
     ]),
     createComuna("Tunari", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Tunari", "sat"),
         createLoc("Dimieni", "sat")
     ]),
     
     createComuna("Vidra", [
+				createAdresaCompleta("ILFOV"),
         createLoc("Vidra", "sat"),
         createLoc("Crețești", "sat"),
         createLoc("Sintești", "sat")
