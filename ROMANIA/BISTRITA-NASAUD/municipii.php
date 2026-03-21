094ae9aa02c0a61b5e9e4ab1813724fcb0aa5fb3
Boga Sebastian <sebitestb@gmail.com>
2026-03-21 19:49:39 +0200

    fix: BISTRITA-NASAUD - add missing adresaCompleta to sate
    - Fixed BISTRIȚA municipiu sate (Ghinda, Sărata, Sigmir, etc)
    - Fixed BECLEAN oras sate (Coldău, Figa, Rusu de Jos)
    - Fixed NĂSĂUD oras and sate
    - Fixed SÂNGEORZ-BĂI oras and sate

--- ROMANIA/BISTRITA-NASAUD/municipii.php
+++ ROMANIA/BISTRITA-NASAUD/municipii.php
@@ -3,13 +3,13 @@ $municipii =
 [
 		createMunicipiu("BISTRIȚA", [
                     createAdresaCompleta("municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania"),
-                    createLoc("BISTRIȚA", "oras"),
-                    createLoc("Ghinda", "sat"),
-					createLoc("Sărata", "sat"),
-                    createLoc("Sigmir", "sat"),
-					createLoc("Slătinița", "sat"),
-                    createLoc("Unirea", "sat"),
-					createLoc("Viișoara", "sat"),
+                    createLoc("BISTRIȚA", "oras", createAdresaCompleta("orasul BISTRIȚA, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "orasul BISTRIȚA, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+                    createLoc("Ghinda", "sat", createAdresaCompleta("sat Ghinda, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Ghinda, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+					createLoc("Sărata", "sat", createAdresaCompleta("sat Sărata, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sarata, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+                    createLoc("Sigmir", "sat", createAdresaCompleta("sat Sigmir, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sigmir, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+					createLoc("Slătinița", "sat", createAdresaCompleta("sat Slătinița, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Slatınita, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+                    createLoc("Unirea", "sat", createAdresaCompleta("sat Unirea, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Unirea, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+					createLoc("Viișoara", "sat", createAdresaCompleta("sat Viișoara, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Viisoara, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
                 ]),				
 			
 ];
--- ROMANIA/BISTRITA-NASAUD/orase.php
+++ ROMANIA/BISTRITA-NASAUD/orase.php
@@ -3,23 +3,23 @@ $orase=[
             createOras("BECLEAN",[
 			    createAdresaCompleta("orasul BECLEAN, judetul BISTRITA-NASAUD, România", "orasul BECLEAN, judetul BISTRITA-NASAUD, Romania"),
 			    createLoc("BECLEAN", "oras" ,[
-							createLoc("Coldău", "sat"),
-							createLoc("Figa", "sat"),
-							createLoc("Rusu de Jos", "sat"),				
+							createLoc("Coldău", "sat", createAdresaCompleta("sat Coldău, orasul BECLEAN, judetul BISTRITA-NASAUD, România", "sat Coldau, orasul BECLEAN, judetul BISTRITA-NASAUD, Romania")),
+							createLoc("Figa", "sat", createAdresaCompleta("sat Figa, orasul BECLEAN, judetul BISTRITA-NASAUD, România", "sat Figa, orasul BECLEAN, judetul BISTRITA-NASAUD, Romania")),
+							createLoc("Rusu de Jos", "sat", createAdresaCompleta("sat Rusu de Jos, orasul BECLEAN, judetul BISTRITA-NASAUD, România", "sat Rusu de Jos, orasul BECLEAN, judetul BISTRITA-NASAUD, Romania")),				
 						]),
 
 			                   ]),
             createOras("NĂSĂUD",[
-			    createAdresaCompleta("orasul BECLEAN, judetul BISTRITA-NASAUD, România", "orasul BECLEAN, judetul BISTRITA-NASAUD, Romania"),
-			    createLoc("NĂSĂUD", "oras"),
-				createLoc("Liviu Rebreanu", "sat"),
-				createLoc("Lușca", "sat"),
+			    createAdresaCompleta("orasul NĂSĂUD, judetul BISTRITA-NASAUD, România", "orasul NASĂUD, judetul BISTRITA-NASAUD, Romania"),
+			    createLoc("NĂSĂUD", "oras", createAdresaCompleta("orasul NĂSĂUD, judetul BISTRITA-NASAUD, România", "orasul NASĂUD, judetul BISTRITA-NASAUD, Romania")),
+				createLoc("Liviu Rebreanu", "sat", createAdresaCompleta("sat Liviu Rebreanu, orasul NĂSĂUD, judetul BISTRITA-NASAUD, România", "sat Liviu Rebreanu, orasul NASĂUD, judetul BISTRITA-NASAUD, Romania")),
+				createLoc("Lușca", "sat", createAdresaCompleta("sat Lușca, orasul NĂSĂUD, judetul BISTRITA-NASAUD, România", "sat Lusca, orasul NASĂUD, judetul BISTRITA-NASAUD, Romania")),
 			                   ]),
             createOras("SÂNGEORZ-BĂI",[
-			    createAdresaCompleta("orasul BECLEAN, judetul BISTRITA-NASAUD, România", "orasul BECLEAN, judetul BISTRITA-NASAUD, Romania"),
-			    createLoc("SÂNGEORZ-BĂI", "oras"),
-				createLoc("Cormaia", "sat"),
-				createLoc("Valea Borcutului", "sat"),
+			    createAdresaCompleta("orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, România", "orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, Romania"),
+			    createLoc("SÂNGEORZ-BĂI", "oras", createAdresaCompleta("orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, România", "orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, Romania")),
+				createLoc("Cormaia", "sat", createAdresaCompleta("sat Cormaia, orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, România", "sat Cormaia, orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, Romania")),
+				createLoc("Valea Borcutului", "sat", createAdresaCompleta("sat Valea Borcutului, orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, România", "sat Valea Borcutului, orasul SÂNGEORZ-BĂI, judetul BISTRITA-NASAUD, Romania")),
 			                   ]),								   
 			
 ];
