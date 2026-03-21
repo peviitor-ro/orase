ffc3b4691735b28c164201cb8d1b9b9335cd69b8
Boga Sebastian <sebitestb@gmail.com>
2026-03-21 19:55:51 +0200

    fix: BOTOSANI - add missing adresaCompleta to sate
    - Fixed BOTOSANI and DOROHOI municipiu
    - Fixed BUCECEA, DARABANI, FLĂMÂNZI, SĂVENI, ȘTEFĂNEȘTI orase

--- ROMANIA/BOTOSANI/municipii.php
+++ ROMANIA/BOTOSANI/municipii.php
@@ -3,14 +3,14 @@ $municipii =
 [
 		createMunicipiu("BOTOȘANI", [
                     createAdresaCompleta("municipiul BOTOȘANI, judetul BOTOSANI, România", "municipiul BOTOSANI, judetul BOTOSANI, Romania"),
-                    createLoc("BOTOȘANI", "oras"),
+                    createLoc("BOTOȘANI", "oras", createAdresaCompleta("orasul BOTOȘANI, municipiul BOTOȘANI, judetul BOTOSANI, România", "orasul BOTOSANI, municipiul BOTOSANI, judetul BOTOSANI, Romania")),
                 ]),	
 		createMunicipiu("DOROHOI", [
-                    createAdresaCompleta("municipiul BOTOȘANI, judetul BOTOSANI, România", "municipiul BOTOSANI, judetul BOTOSANI, Romania"),
-                    createLoc("DOROHOI", "oras"),
-                    createLoc("Dealu Mare", "sat"),
-					createLoc("Loturi Enescu", "sat"),
-                    createLoc("Progresul", "sat"),
+                    createAdresaCompleta("municipiul DOROHOI, judetul BOTOSANI, România", "municipiul DOROHOI, judetul BOTOSANI, Romania"),
+                    createLoc("DOROHOI", "oras", createAdresaCompleta("orasul DOROHOI, municipiul DOROHOI, judetul BOTOSANI, România", "orasul DOROHOI, municipiul DOROHOI, judetul BOTOSANI, Romania")),
+                    createLoc("Dealu Mare", "sat", createAdresaCompleta("sat Dealu Mare, municipiul DOROHOI, judetul BOTOSANI, România", "sat Dealu Mare, municipiul DOROHOI, judetul BOTOSANI, Romania")),
+					createLoc("Loturi Enescu", "sat", createAdresaCompleta("sat Loturi Enescu, municipiul DOROHOI, judetul BOTOSANI, România", "sat Loturi Enescu, municipiul DOROHOI, judetul BOTOSANI, Romania")),
+                    createLoc("Progresul", "sat", createAdresaCompleta("sat Progresul, municipiul DOROHOI, judetul BOTOSANI, România", "sat Progresul, municipiul DOROHOI, judetul BOTOSANI, Romania")),
                 ]),					
 			
 ];
