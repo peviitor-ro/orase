9dd2d346324ef8359ff91812c6a3b413989b2fbb
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-21 19:55:57 +0200

    fix: BOTOSANI - add missing adresaCompleta to sate (#350)
    - Fixed BOTOSANI and DOROHOI municipiu
    - Fixed BUCECEA, DARABANI, FLĂMÂNZI, SĂVENI, ȘTEFĂNEȘTI orase

--- ROMANIA/BOTOSANI/orase.php
+++ ROMANIA/BOTOSANI/orase.php
@@ -3,49 +3,49 @@ $orase=[
             createOras("BUCECEA",[
 			    createAdresaCompleta("orasul BUCECEA, judetul BOTOSANI, România", "orasul BUCECEA, judetul BOTOSANI, Romania"),
 			    createLoc("BUCECEA", "oras",[
-						createLoc("Bohoghina", "sat"),
+						createLoc("Bohoghina", "sat", createAdresaCompleta("sat Bohoghina, orasul BUCECEA, judetul BOTOSANI, România", "sat Bohoghina, orasul BUCECEA, judetul BOTOSANI, Romania")),
 				  ]),
-				createLoc("Călinești", "sat"),
+				createLoc("Călinești", "sat", createAdresaCompleta("sat Călinești, orasul BUCECEA, judetul BOTOSANI, România", "sat Calinesti, orasul BUCECEA, judetul BOTOSANI, Romania")),
 			                   ]),
 							   
             createOras("DARABANI",[
-			    createAdresaCompleta("orasul BUCECEA, judetul BOTOSANI, România", "orasul BUCECEA, judetul BOTOSANI, Romania"),
+			    createAdresaCompleta("orasul DARABANI, judetul BOTOSANI, România", "orasul DARABANI, judetul BOTOSANI, Romania"),
 			    createLoc("DARABANI", "oras", [
-						createLoc("Bajura", "sat"),
-						createLoc("Eșanca", "sat"),
-						createLoc("Lișmănița", "sat"),
+						createLoc("Bajura", "sat", createAdresaCompleta("sat Bajura, orasul DARABANI, judetul BOTOSANI, România", "sat Bajura, orasul DARABANI, judetul BOTOSANI, Romania")),
+						createLoc("Eșanca", "sat", createAdresaCompleta("sat Eșanca, orasul DARABANI, judetul BOTOSANI, România", "sat Esanca, orasul DARABANI, judetul BOTOSANI, Romania")),
+						createLoc("Lișmănița", "sat", createAdresaCompleta("sat Lișmănița, orasul DARABANI, judetul BOTOSANI, România", "sat Lismanita, orasul DARABANI, judetul BOTOSANI, Romania")),
 				]),
 			                   ]),
 							   
             createOras("FLĂMÂNZI",[
-			    createAdresaCompleta("orasul BUCECEA, judetul BOTOSANI, România", "orasul BUCECEA, judetul BOTOSANI, Romania"),
+			    createAdresaCompleta("orasul FLĂMÂNZI, judetul BOTOSANI, România", "orasul FLĂMÂNZI, judetul BOTOSANI, Romania"),
 			    createLoc("FLĂMÂNZI", "oras",[
-						createLoc("Chițoveni", "sat"),
-						createLoc("Prisăcani", "sat"),
+						createLoc("Chițoveni", "sat", createAdresaCompleta("sat Chițoveni, orasul FLĂMÂNZI, judetul BOTOSANI, România", "sat Chitoveni, orasul FLĂMÂNZI, judetul BOTOSANI, Romania")),
+						createLoc("Prisăcani", "sat", createAdresaCompleta("sat Prisăcani, orasul FLĂMÂNZI, judetul BOTOSANI, România", "sat Prisacani, orasul FLĂMÂNZI, judetul BOTOSANI, Romania")),
 				]),
-				createLoc("Nicolae Bălcescu", "sat"),
-				createLoc("Poiana", "sat"),
+				createLoc("Nicolae Bălcescu", "sat", createAdresaCompleta("sat Nicolae Bălcescu, orasul FLĂMÂNZI, judetul BOTOSANI, România", "sat Nicolae Balcescu, orasul FLĂMÂNZI, judetul BOTOSANI, Romania")),
+				createLoc("Poiana", "sat", createAdresaCompleta("sat Poiana, orasul FLĂMÂNZI, judetul BOTOSANI, România", "sat Poiana, orasul FLĂMÂNZI, judetul BOTOSANI, Romania")),
 			                   ]),
 							   
             createOras("SĂVENI",[
-			    createAdresaCompleta("orasul BUCECEA, judetul BOTOSANI, România", "orasul BUCECEA, judetul BOTOSANI, Romania"),
+			    createAdresaCompleta("orasul SĂVENI, judetul BOTOSANI, România", "orasul SĂVENI, judetul BOTOSANI, Romania"),
 			    createLoc("SĂVENI", "oras", [
-						createLoc("Bodeasa", "sat"),
-						createLoc("Bozieni", "sat"),
-						createLoc("Chișcăreni", "sat"),
-						createLoc("Petricani", "sat"),
-						createLoc("Sat Nou", "sat"),
+						createLoc("Bodeasa", "sat", createAdresaCompleta("sat Bodeasa, orasul SĂVENI, judetul BOTOSANI, România", "sat Bodeasa, orasul SĂVENI, judetul BOTOSANI, Romania")),
+						createLoc("Bozieni", "sat", createAdresaCompleta("sat Bozieni, orasul SĂVENI, judetul BOTOSANI, România", "sat Bozieni, orasul SĂVENI, judetul BOTOSANI, Romania")),
+						createLoc("Chișcăreni", "sat", createAdresaCompleta("sat Chișcăreni, orasul SĂVENI, judetul BOTOSANI, România", "sat Chiscareni, orasul SĂVENI, judetul BOTOSANI, Romania")),
+						createLoc("Petricani", "sat", createAdresaCompleta("sat Petricani, orasul SĂVENI, judetul BOTOSANI, România", "sat Petricani, orasul SĂVENI, judetul BOTOSANI, Romania")),
+						createLoc("Sat Nou", "sat", createAdresaCompleta("sat Sat Nou, orasul SĂVENI, judetul BOTOSANI, România", "sat Sat Nou, orasul SĂVENI, judetul BOTOSANI, Romania")),
 				]),
 				
 			                   ]),							   
 
             createOras("ȘTEFĂNEȘTI",[
-			    createAdresaCompleta("orasul BUCECEA, judetul BOTOSANI, România", "orasul BUCECEA, judetul BOTOSANI, Romania"),
+			    createAdresaCompleta("orasul ȘTEFĂNEȘTI, judetul BOTOSANI, România", "orasul ȘTEFĂNEȘTI, judetul BOTOSANI, Romania"),
 			    createLoc("ȘTEFĂNEȘTI", "oras", [
-						createLoc("Bădiuți", "sat"),
-						createLoc("Bobulești", "sat"),
-						createLoc("Stânca", "sat"),
-						createLoc("Ștefănești-Sat", "sat"),
+						createLoc("Bădiuți", "sat", createAdresaCompleta("sat Bădiuți, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, România", "sat Badiuti, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, Romania")),
+						createLoc("Bobulești", "sat", createAdresaCompleta("sat Bobulești, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, România", "sat Bobulesti, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, Romania")),
+						createLoc("Stânca", "sat", createAdresaCompleta("sat Stânca, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, România", "sat Stanca, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, Romania")),
+						createLoc("Ștefănești-Sat", "sat", createAdresaCompleta("sat Ștefănești-Sat, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, România", "sat Ștefănești-Sat, orasul ȘTEFĂNEȘTI, judetul BOTOSANI, Romania")),
 				]),
 				
 			                   ]),		
