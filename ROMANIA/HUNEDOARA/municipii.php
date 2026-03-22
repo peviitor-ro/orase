ae403507ae63a78e6d81175b1aae28cb1ccb90a9
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-22 08:48:02 +0200

    Fix HUNEDOARA county - add adresaCompleta for all sate (#364)
    - Fix adresaCompleta in municipii.php and orase.php
    - Add adresaCompleta to all sate in comune.php
    - Add HUNEDOARA to E2E workflow

--- ROMANIA/HUNEDOARA/municipii.php
+++ ROMANIA/HUNEDOARA/municipii.php
@@ -1,60 +1,55 @@
 <?php
-$municipii =
-	[
-		createMunicipiu("DEVA", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("DEVA", "oras", [
-				createLoc("Archia", "sat"),
-				createLoc("Bârcea Mică", "sat"),
-				createLoc("Cristur", "sat"),
-			]),
-			createLoc("Sântuhalm", "sat")
-		]),
-		createMunicipiu("BRAD", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("BRAD", "oras", [
-				createLoc("Mesteacăn", "sat"),
-				createLoc("Potingani", "sat"),
-				createLoc("Ruda-Brad", "sat"),
-				createLoc("Țărățel", "sat"),
-				createLoc("Valea Bradului", "sat"),
-			]),
-		]),
-		createMunicipiu("HUNEDOARA", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("HUNEDOARA", "oras", [
-				createLoc("Boș", "sat"),
-				createLoc("Groș", "sat"),
-				createLoc("Hășdat", "sat"),
-				createLoc("Peștișu Mare", "sat"),
-			]),
-			createLoc("Răcăștia", "sat")
-		]),
-		createMunicipiu("LUPENI", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("LUPENI", "oras"),
-
-		]),
-		createMunicipiu("ORĂȘTIE", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("ORĂȘTIE", "oras"),
-
-		]),
-		createMunicipiu("PETROȘANI", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("PETROȘANI", "oras"),
-			createLoc("Dâlja Mare", "sat"),
-			createLoc("Dâlja Mică", "sat"),
-			createLoc("Peștera", "sat"),
-			createLoc("Slătinioara", "sat"),
-
-		]),
-		createMunicipiu("VULCAN", [
-				createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
-			createLoc("VULCAN", "oras"),
-			createLoc("Dealu Babii", "sat"),
-			createLoc("Jiu-Paroșeni", "sat"),
-
-		]),
-
-	];
+$municipii = [
+    createMunicipiu("DEVA", [
+        createAdresaCompleta("municipiul DEVA, judetul HUNEDOARA, România", "municipiul DEVA, judetul HUNEDOARA, Romania"),
+        createLoc("DEVA", "oras", [
+            createLoc("Archia", "sat", createAdresaCompleta("sat ArchIA, municipiul DEVA, judetul Hunedoara, România", "sat ArchIA, municipiul DEVA, judetul Hunedoara, Romania")),
+            createLoc("Bârcea Mică", "sat", createAdresaCompleta("sat Bârcea Mică, municipiul DEVA, judetul Hunedoara, România", "sat Barcea Mica, municipiul DEVA, judetul Hunedoara, Romania")),
+            createLoc("Cristur", "sat", createAdresaCompleta("sat Cristur, municipiul DEVA, judetul Hunedoara, România", "sat Cristur, municipiul DEVA, judetul Hunedoara, Romania")),
+        ]),
+        createLoc("Sântuhalm", "sat", createAdresaCompleta("sat Sântuhalm, municipiul DEVA, judetul Hunedoara, România", "sat Santuhalm, municipiul DEVA, judetul Hunedoara, Romania")),
+    ]),
+    createMunicipiu("BRAD", [
+        createAdresaCompleta("municipiul BRAD, judetul HUNEDOARA, România", "municipiul BRAD, judetul HUNEDOARA, Romania"),
+        createLoc("BRAD", "oras", [
+            createLoc("Mesteacăn", "sat", createAdresaCompleta("sat Mesteacăn, municipiul BRAD, judetul Hunedoara, România", "sat Mesteacan, municipiul BRAD, judetul Hunedoara, Romania")),
+            createLoc("Potingani", "sat", createAdresaCompleta("sat Potingani, municipiul BRAD, judetul Hunedoara, România", "sat Potingani, municipiul BRAD, judetul Hunedoara, Romania")),
+            createLoc("Ruda-Brad", "sat", createAdresaCompleta("sat Ruda-Brad, municipiul BRAD, judetul Hunedoara, România", "sat Ruda-Brad, municipiul BRAD, judetul Hunedoara, Romania")),
+            createLoc("Țărățel", "sat", createAdresaCompleta("sat Țărățel, municipiul BRAD, judetul Hunedoara, România", "sat Taratel, municipiul BRAD, judetul Hunedoara, Romania")),
+            createLoc("Valea Bradului", "sat", createAdresaCompleta("sat Valea Bradului, municipiul BRAD, judetul Hunedoara, România", "sat Valea Bradului, municipiul BRAD, judetul Hunedoara, Romania")),
+        ]),
+    ]),
+    createMunicipiu("HUNEDOARA", [
+        createAdresaCompleta("municipiul HUNEDOARA, judetul HUNEDOARA, România", "municipiul HUNEDOARA, judetul HUNEDOARA, Romania"),
+        createLoc("HUNEDOARA", "oras", [
+            createLoc("Boș", "sat", createAdresaCompleta("sat Boș, municipiul HUNEDOARA, judetul Hunedoara, România", "sat Bos, municipiul HUNEDOARA, judetul Hunedoara, Romania")),
+            createLoc("Groș", "sat", createAdresaCompleta("sat Groș, municipiul HUNEDOARA, judetul Hunedoara, România", "sat Gros, municipiul HUNEDOARA, judetul Hunedoara, Romania")),
+            createLoc("Hășdat", "sat", createAdresaCompleta("sat Hășdat, municipiul HUNEDOARA, judetul Hunedoara, România", "sat Hasdat, municipiul HUNEDOARA, judetul Hunedoara, Romania")),
+            createLoc("Peștișu Mare", "sat", createAdresaCompleta("sat Peștișu Mare, municipiul HUNEDOARA, judetul Hunedoara, România", "sat Pestisu Mare, municipiul HUNEDOARA, judetul Hunedoara, Romania")),
+        ]),
+        createLoc("Răcăștia", "sat", createAdresaCompleta("sat Răcăștia, municipiul HUNEDOARA, judetul Hunedoara, România", "sat Racastia, municipiul HUNEDOARA, judetul Hunedoara, Romania")),
+    ]),
+    createMunicipiu("LUPENI", [
+        createAdresaCompleta("municipiul LUPENI, judetul HUNEDOARA, România", "municipiul LUPENI, judetul HUNEDOARA, Romania"),
+        createLoc("LUPENI", "oras"),
+    ]),
+    createMunicipiu("ORĂȘTIE", [
+        createAdresaCompleta("municipiul ORĂȘTIE, judetul HUNEDOARA, România", "municipiul ORASTIE, judetul HUNEDOARA, Romania"),
+        createLoc("ORĂȘTIE", "oras"),
+    ]),
+    createMunicipiu("PETROȘANI", [
+        createAdresaCompleta("municipiul PETROȘANI, judetul HUNEDOARA, România", "municipiul PETROSANI, judetul HUNEDOARA, Romania"),
+        createLoc("PETROȘANI", "oras"),
+        createLoc("Dâlja Mare", "sat", createAdresaCompleta("sat Dâlja Mare, municipiul PETROȘANI, judetul Hunedoara, România", "sat Dalja Mare, municipiul PETROSANI, judetul Hunedoara, Romania")),
+        createLoc("Dâlja Mică", "sat", createAdresaCompleta("sat Dâlja Mică, municipiul PETROȘANI, judetul Hunedoara, România", "sat Dalja Mica, municipiul PETROSANI, judetul Hunedoara, Romania")),
+        createLoc("Peștera", "sat", createAdresaCompleta("sat Peștera, municipiul PETROȘANI, judetul Hunedoara, România", "sat Pestera, municipiul PETROSANI, judetul Hunedoara, Romania")),
+        createLoc("Slătinioara", "sat", createAdresaCompleta("sat Slătinioara, municipiul PETROȘANI, judetul Hunedoara, România", "sat Slatinioara, municipiul PETROSANI, judetul Hunedoara, Romania")),
+    ]),
+    createMunicipiu("VULCAN", [
+        createAdresaCompleta("municipiul VULCAN, judetul HUNEDOARA, România", "municipiul VULCAN, judetul HUNEDOARA, Romania"),
+        createLoc("VULCAN", "oras"),
+        createLoc("Dealu Babii", "sat", createAdresaCompleta("sat Dealu Babii, municipiul VULCAN, judetul Hunedoara, România", "sat Dealu Babii, municipiul VULCAN, judetul Hunedoara, Romania")),
+        createLoc("Jiu-Paroșeni", "sat", createAdresaCompleta("sat Jiu-Paroșeni, municipiul VULCAN, judetul Hunedoara, România", "sat Jiu-Paroseni, municipiul VULCAN, judetul Hunedoara, Romania")),
+    ]),
+];
+?>
--- ROMANIA/HUNEDOARA/orase.php
+++ ROMANIA/HUNEDOARA/orase.php
@@ -1,83 +1,81 @@
 <?php
 $orase = [
     createOras("ANINOASA", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
         createLoc("ANINOASA", "oras", [
-            createLoc("Iscroni", "sat"),
+            createLoc("Iscroni", "sat", createAdresaCompleta("sat Iscroni, orasul ANINOASA, judetul Hunedoara, România", "sat Iscroni, orasul ANINOASA, judetul Hunedoara, Romania")),
         ]),
     ]),
 
     createOras("CĂLAN", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul CĂLAN, judetul HUNEDOARA, România", "orasul CALAN, judetul HUNEDOARA, Romania"),
         createLoc("CĂLAN", "oras", [
-            createLoc("Batiz", "sat"),      
-            createLoc("Călanu Mic", "sat"),
-            createLoc("Grid", "sat"),
-            createLoc("Nădăștia de Jos", "sat"),
-            createLoc("Nădăștia de Sus", "sat"),
-            createLoc("Ohaba Streiului", "sat"),
-            createLoc("Sâncrai", "sat"),
-            createLoc("Sântămăria de Piatră", "sat"),
-            createLoc("Strei", "sat"),
-            createLoc("Strei-Săcel", "sat"),
-            createLoc("Valea Sângeorgiului", "sat"),
+            createLoc("Batiz", "sat", createAdresaCompleta("sat Batiz, orasul CĂLAN, judetul Hunedoara, România", "sat Batiz, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Călanu Mic", "sat", createAdresaCompleta("sat Călanu Mic, orasul CĂLAN, judetul Hunedoara, România", "sat Calanu Mic, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Grid", "sat", createAdresaCompleta("sat Grid, orasul CĂLAN, judetul Hunedoara, România", "sat Grid, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Nădăștia de Jos", "sat", createAdresaCompleta("sat Nădăștia de Jos, orasul CĂLAN, judetul Hunedoara, România", "sat Nadastia de Jos, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Nădăștia de Sus", "sat", createAdresaCompleta("sat Nădăștia de Sus, orasul CĂLAN, judetul Hunedoara, România", "sat Nadastia de Sus, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Ohaba Streiului", "sat", createAdresaCompleta("sat Ohaba Streiului, orasul CĂLAN, judetul Hunedoara, România", "sat Ohaba Streiului, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Sâncrai", "sat", createAdresaCompleta("sat Sâncrai, orasul CĂLAN, judetul Hunedoara, România", "sat Sancrai, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Sântămăria de Piatră", "sat", createAdresaCompleta("sat Sântămăria de Piatră, orasul CĂLAN, judetul Hunedoara, România", "sat Sântămăria de Piatră, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Strei", "sat", createAdresaCompleta("sat Strei, orasul CĂLAN, judetul Hunedoara, România", "sat Strei, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Strei-Săcel", "sat", createAdresaCompleta("sat Strei-Săcel, orasul CĂLAN, judetul Hunedoara, România", "sat Strei-Sacel, orasul CALAN, judetul Hunedoara, Romania")),
+            createLoc("Valea Sângeorgiului", "sat", createAdresaCompleta("sat Valea Sângeorgiului, orasul CĂLAN, judetul Hunedoara, România", "sat Valea Sangeorgiului, orasul CALAN, judetul Hunedoara, Romania")),
         ]),
-        createLoc("Streisângeorgiu", "sat"),
+        createLoc("Streisângeorgiu", "sat", createAdresaCompleta("sat Streisângeorgiu, orasul CĂLAN, judetul Hunedoara, România", "sat Streisangeorgiu, orasul CALAN, judetul Hunedoara, Romania")),
     ]),
 
     createOras("GEOAGIU", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul GEOAGIU, judetul HUNEDOARA, România", "orasul GEOAGIU, judetul HUNEDOARA, Romania"),
         createLoc("GEOAGIU", "oras", [
-            createLoc("Aurel Vlaicu", "sat"),
-            createLoc("Băcâia", "sat"),
-            createLoc("Bozeș", "sat"),
-            createLoc("Cigmău", "sat"),
-            createLoc("Gelmar", "sat"),
-            createLoc("Geoagiu-Băi", "sat"),
-            createLoc("Homorod", "sat"),
-            createLoc("Mermezeu-Văleni", "sat"),
-            createLoc("Renghet", "sat"),
-            createLoc("Văleni", "sat"),
+            createLoc("Aurel Vlaicu", "sat", createAdresaCompleta("sat Aurel Vlaicu, orasul GEOAGIU, judetul Hunedoara, România", "sat Aurel Vlaicu, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Băcâia", "sat", createAdresaCompleta("sat Băcâia, orasul GEOAGIU, judetul Hunedoara, România", "sat Bacâia, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Bozeș", "sat", createAdresaCompleta("sat Bozeș, orasul GEOAGIU, judetul Hunedoara, România", "sat Bozés, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Cigmău", "sat", createAdresaCompleta("sat Cigmău, orasul GEOAGIU, judetul Hunedoara, România", "sat Cigmău, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Gelmar", "sat", createAdresaCompleta("sat Gelmar, orasul GEOAGIU, judetul Hunedoara, România", "sat Gelmar, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Geoagiu-Băi", "sat", createAdresaCompleta("sat Geoagiu-Băi, orasul GEOAGIU, judetul Hunedoara, România", "sat Geoagiu-Băi, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Homorod", "sat", createAdresaCompleta("sat Homorod, orasul GEOAGIU, judetul Hunedoara, România", "sat Homorod, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Mermezeu-Văleni", "sat", createAdresaCompleta("sat Mermezeu-Văleni, orasul GEOAGIU, judetul Hunedoara, România", "sat Mermezeu-Văleni, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Renghet", "sat", createAdresaCompleta("sat Renghet, orasul GEOAGIU, judetul Hunedoara, România", "sat Renghet, orasul GEOAGIU, judetul Hunedoara, Romania")),
+            createLoc("Văleni", "sat", createAdresaCompleta("sat Văleni, orasul GEOAGIU, judetul Hunedoara, România", "sat Văleni, orasul GEOAGIU, judetul Hunedoara, Romania")),
         ]),
     ]),
 
     createOras("HAȚEG", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul HAȚEG, judetul HUNEDOARA, România", "orasul HATEG, judetul HUNEDOARA, Romania"),
         createLoc("HAȚEG", "oras", [
-            createLoc("Silvașu de Jos", "sat"),           
-            createLoc("Silvașu de Sus", "sat"),
+            createLoc("Silvașu de Jos", "sat", createAdresaCompleta("sat Silvașu de Jos, orasul HAȚEG, judetul Hunedoara, România", "sat Silvasu de Jos, orasul HATEG, judetul Hunedoara, Romania")),
+            createLoc("Silvașu de Sus", "sat", createAdresaCompleta("sat Silvașu de Sus, orasul HAȚEG, judetul Hunedoara, România", "sat Silvasu de Sus, orasul HATEG, judetul Hunedoara, Romania")),
         ]),
-        createLoc("Nălațvad", "sat"),
+        createLoc("Nălațvad", "sat", createAdresaCompleta("sat Nălațvad, orasul HAȚEG, judetul Hunedoara, România", "sat Nălațvad, orasul HATEG, judetul Hunedoara, Romania")),
     ]),
 
     createOras("PETRILA", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul PETRILA, judetul HUNEDOARA, România", "orasul PETRILA, judetul HUNEDOARA, Romania"),
         createLoc("PETRILA", "oras"),
-        createLoc("Cimpa", "sat"),
-        createLoc("Jieț", "sat"),
-        createLoc("Răscoala", "sat"),
-        createLoc("Tirici", "sat"),
+        createLoc("Cimpa", "sat", createAdresaCompleta("sat Cimpa, orasul PETRILA, judetul Hunedoara, România", "sat Cimpa, orasul PETRILA, judetul Hunedoara, Romania")),
+        createLoc("Jieț", "sat", createAdresaCompleta("sat Jieț, orasul PETRILA, judetul Hunedoara, România", "sat Jieț, orasul PETRILA, judetul Hunedoara, Romania")),
+        createLoc("Răscoala", "sat", createAdresaCompleta("sat Răscoala, orasul PETRILA, judetul Hunedoara, România", "sat Răscoala, orasul PETRILA, judetul Hunedoara, Romania")),
+        createLoc("Tirici", "sat", createAdresaCompleta("sat Tirici, orasul PETRILA, judetul Hunedoara, România", "sat Tirici, orasul PETRILA, judetul Hunedoara, Romania")),
     ]),
 
     createOras("SIMERIA", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul SIMERIA, judetul HUNEDOARA, România", "orasul SIMERIA, judetul HUNEDOARA, Romania"),
         createLoc("SIMERIA", "oras", [
-            createLoc("Bârcea Mare", "sat"),
-            createLoc("Cărpiniș", "sat"),
-            createLoc("Simeria Veche", "sat"),
-            createLoc("Sântandrei", "sat"),
-            createLoc("Șăulești", "sat"),
-            createLoc("Uroi", "sat"),
+            createLoc("Bârcea Mare", "sat", createAdresaCompleta("sat Bârscea Mare, orasul SIMERIA, judetul Hunedoara, România", "sat Barcea Mare, orasul SIMERIA, judetul Hunedoara, Romania")),
+            createLoc("Cărpiniș", "sat", createAdresaCompleta("sat Cărpiniș, orasul SIMERIA, judetul Hunedoara, România", "sat Cărpiniș, orasul SIMERIA, judetul Hunedoara, Romania")),
+            createLoc("Simeria Veche", "sat", createAdresaCompleta("sat Simeria Veche, orasul SIMERIA, judetul Hunedoara, România", "sat Simeria Veche, orasul SIMERIA, judetul Hunedoara, Romania")),
+            createLoc("Sântandrei", "sat", createAdresaCompleta("sat Sântandrei, orasul SIMERIA, judetul Hunedoara, România", "sat Sântandrei, orasul SIMERIA, judetul Hunedoara, Romania")),
+            createLoc("Șăulești", "sat", createAdresaCompleta("sat Șăulești, orasul SIMERIA, judetul Hunedoara, România", "sat Șăulești, orasul SIMERIA, judetul Hunedoara, Romania")),
+            createLoc("Uroi", "sat", createAdresaCompleta("sat Uroi, orasul SIMERIA, judetul Hunedoara, România", "sat Uroi, orasul SIMERIA, judetul Hunedoara, Romania")),
         ]),
     ]),
 
     createOras("URICANI", [
-				createAdresaCompleta("orasul ANINOASA, judetul HUNEDOARA, România", "orasul ANINOASA, judetul HUNEDOARA, Romania"),
+        createAdresaCompleta("orasul URICANI, judetul HUNEDOARA, România", "orasul URICANI, judetul HUNEDOARA, Romania"),
         createLoc("URICANI", "oras"),
-        createLoc("Câmpu lui Neag", "sat"),
-        createLoc("Valea de Brazi", "sat"),
+        createLoc("Câmpu lui Neag", "sat", createAdresaCompleta("sat Câmpu lui Neag, orasul URICANI, judetul Hunedoara, România", "sat Câmpu lui Neag, orasul URICANI, judetul Hunedoara, Romania")),
+        createLoc("Valea de Brazi", "sat", createAdresaCompleta("sat Valea de Brazi, orasul URICANI, judetul Hunedoara, România", "sat Valea de Brazi, orasul URICANI, judetul Hunedoara, Romania")),
     ]),
 ];
-
-
 ?>
