4ced59575d1d0d308574078c201c424860728e27
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 09:15:55 +0200

    Fix BISTRITA-NASAUD - remove duplicate municipiul in oras adresaCompleta

--- ROMANIA/BISTRITA-NASAUD/municipii.php
+++ ROMANIA/BISTRITA-NASAUD/municipii.php
@@ -1,16 +1,15 @@
 <?php
 $municipii =
 [
-		createMunicipiu("BISTRIȚA", [
-                    createAdresaCompleta("municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania"),
-                    createLoc("BISTRIȚA", "oras", createAdresaCompleta("orasul BISTRIȚA, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "orasul BISTRIȚA, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-                    createLoc("Ghinda", "sat", createAdresaCompleta("sat Ghinda, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Ghinda, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-					createLoc("Sărata", "sat", createAdresaCompleta("sat Sărata, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sarata, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-                    createLoc("Sigmir", "sat", createAdresaCompleta("sat Sigmir, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sigmir, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-					createLoc("Slătinița", "sat", createAdresaCompleta("sat Slătinița, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Slatınita, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-                    createLoc("Unirea", "sat", createAdresaCompleta("sat Unirea, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Unirea, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-					createLoc("Viișoara", "sat", createAdresaCompleta("sat Viișoara, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Viisoara, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
-                ]),				
-			
+    createMunicipiu("BISTRIȚA", [
+        createAdresaCompleta("municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania"),
+        createLoc("BISTRIȚA", "oras", createAdresaCompleta("orasul BISTRIȚA, judetul BISTRITA-NASAUD, România", "orasul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Ghinda", "sat", createAdresaCompleta("sat Ghinda, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Ghinda, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Sărata", "sat", createAdresaCompleta("sat Sărata, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sarata, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Sigmir", "sat", createAdresaCompleta("sat Sigmir, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Sigmir, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Slătinița", "sat", createAdresaCompleta("sat Slătinița, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Slatınita, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Unirea", "sat", createAdresaCompleta("sat Unirea, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Unirea, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+        createLoc("Viișoara", "sat", createAdresaCompleta("sat Viișoara, municipiul BISTRIȚA, judetul BISTRITA-NASAUD, România", "sat Viisoara, municipiul BISTRITA, judetul BISTRITA-NASAUD, Romania")),
+    ]),				
 ];
 ?>
