9b0f8fbe281ba46465ec1dd359f764f4a9e081c6
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 09:13:41 +0200

    Fix ARAD - remove duplicate municipiul in oras adresaCompleta

--- ROMANIA/ARAD/municipii.php
+++ ROMANIA/ARAD/municipii.php
@@ -1,8 +1,8 @@
 <?php
 $municipii =
 [
-        createMunicipiu("ARAD", [
-            createLoc("ARAD", "oras", createAdresaCompleta("orasul ARAD, municipiul ARAD, judetul ARAD, România", "orasul ARAD, municipiul ARAD, judetul ARAD, Romania")),
-        ], createAdresaCompleta("municipiul ARAD, judetul ARAD, România", "municipiul ARAD, judetul ARAD, Romania")),
+    createMunicipiu("ARAD", [
+        createLoc("ARAD", "oras", createAdresaCompleta("orasul ARAD, judetul ARAD, România", "orasul ARAD, judetul ARAD, Romania")),
+    ], createAdresaCompleta("municipiul ARAD, judetul ARAD, România", "municipiul ARAD, judetul ARAD, Romania")),
 ];
 ?>
