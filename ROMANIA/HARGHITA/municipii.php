2bff4b2c3e79a9220890b5c99be86084fa6327cd
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 08:43:03 +0200

    Fix HARGHITA county - add adresaCompleta for all sate
    - Fix adresaCompleta in municipii.php
    - Add adresaCompleta to all sate in comune.php
    - Add HARGHITA to E2E workflow

--- ROMANIA/HARGHITA/municipii.php
+++ ROMANIA/HARGHITA/municipii.php
@@ -1,41 +1,36 @@
 <?php
 $municipii =
 [
-		createMunicipiu("MIERCUREA-CIUC", [
-				createAdresaCompleta("municipiul MIERCUREA-CIUC, judetul HARGHITA, România", "municipiul MIERCUREA-CIUC, judetul HARGHITA, Romania"),
-                    createLoc("MIERCUREA-CIUC", "oras"),
-					createLoc("Ciba", "sat"),
-					createLoc("Harghita-Băi", "sat"),
-					createLoc("Jigodin-Băi", "sat"),
-					
-                ]),
-		createMunicipiu("GHEORGHENI", [
-				createAdresaCompleta("municipiul MIERCUREA-CIUC, judetul HARGHITA, România", "municipiul MIERCUREA-CIUC, judetul HARGHITA, Romania"),
-                    createLoc("GHEORGHENI", "oras"),
-					createLoc("Covacipeter", "sat"),
-					createLoc("Lacu Roșu", "sat"),
-					createLoc("Vargatac", "sat"),
-					createLoc("Visafolio", "sat"),
-					
-                ]),
-		createMunicipiu("ODORHEIU SECUIESC", [
-				createAdresaCompleta("municipiul MIERCUREA-CIUC, judetul HARGHITA, România", "municipiul MIERCUREA-CIUC, judetul HARGHITA, Romania"),
-                    createLoc("ODORHEIU SECUIESC", "oras"),
-					
-                ]),
-		createMunicipiu("TOPLIȚA", [
-				createAdresaCompleta("municipiul MIERCUREA-CIUC, judetul HARGHITA, România", "municipiul MIERCUREA-CIUC, judetul HARGHITA, Romania"),
-                    createLoc("TOPLIȚA", "oras"),
-					createLoc("Călimănel", "sat"),
-					createLoc("Luncani", "sat"),
-					createLoc("Măgheruș", "sat"),
-					createLoc("Moglănești", "sat"),
-					createLoc("Secu", "sat"),
-					createLoc("Vale", "sat"),
-					createLoc("Vâgani", "sat"),
-					createLoc("Zencani", "sat"),
-					
-                ]),				
-			
+    createMunicipiu("MIERCUREA-CIUC", [
+        createAdresaCompleta("municipiul MIERCUREA-CIUC, judetul HARGHITA, România", "municipiul MIERCUREA-CIUC, judetul HARGHITA, Romania"),
+        createLoc("MIERCUREA-CIUC", "oras"),
+        createLoc("Ciba", "sat", createAdresaCompleta("sat Ciba, municipiul MIERCUREA-CIUC, judetul Harghita, România", "sat Ciba, municipiul MIERCUREA-CIUC, judetul Harghita, Romania")),
+        createLoc("Harghita-Băi", "sat", createAdresaCompleta("sat Harghita-Băi, municipiul MIERCUREA-CIUC, judetul Harghita, România", "sat Harghita-Bai, municipiul MIERCUREA-CIUC, judetul Harghita, Romania")),
+        createLoc("Jigodin-Băi", "sat", createAdresaCompleta("sat Jigodin-Băi, municipiul MIERCUREA-CIUC, judetul Harghita, România", "sat Jigodin-Bai, municipiul MIERCUREA-CIUC, judetul Harghita, Romania")),
+    ]),
+    createMunicipiu("GHEORGHENI", [
+        createAdresaCompleta("municipiul GHEORGHENI, judetul HARGHITA, România", "municipiul GHEORGHENI, judetul HARGHITA, Romania"),
+        createLoc("GHEORGHENI", "oras"),
+        createLoc("Covacipeter", "sat", createAdresaCompleta("sat Covacipeter, municipiul GHEORGHENI, judetul Harghita, România", "sat Covacipeter, municipiul GHEORGHENI, judetul Harghita, Romania")),
+        createLoc("Lacu Roșu", "sat", createAdresaCompleta("sat Lacu Roșu, municipiul GHEORGHENI, judetul Harghita, România", "sat Lacu Rosu, municipiul GHEORGHENI, judetul Harghita, Romania")),
+        createLoc("Vargatac", "sat", createAdresaCompleta("sat Vargatac, municipiul GHEORGHENI, judetul Harghita, România", "sat Vargatac, municipiul GHEORGHENI, judetul Harghita, Romania")),
+        createLoc("Visafolio", "sat", createAdresaCompleta("sat Visafolio, municipiul GHEORGHENI, judetul Harghita, România", "sat Visafolio, municipiul GHEORGHENI, judetul Harghita, Romania")),
+    ]),
+    createMunicipiu("ODORHEIU SECUIESC", [
+        createAdresaCompleta("municipiul ODORHEIU SECUIESC, judetul HARGHITA, România", "municipiul ODORHEIU SECUIESC, judetul HARGHITA, Romania"),
+        createLoc("ODORHEIU SECUIESC", "oras"),
+    ]),
+    createMunicipiu("TOPLIȚA", [
+        createAdresaCompleta("municipiul TOPLIȚA, judetul HARGHITA, România", "municipiul TOPLITA, judetul HARGHITA, Romania"),
+        createLoc("TOPLIȚA", "oras"),
+        createLoc("Călimănel", "sat", createAdresaCompleta("sat Călimănel, municipiul TOPLIȚA, judetul Harghita, România", "sat Calimanel, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Luncani", "sat", createAdresaCompleta("sat Luncani, municipiul TOPLIȚA, judetul Harghita, România", "sat Luncani, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Măgheruș", "sat", createAdresaCompleta("sat Măgheruș, municipiul TOPLIȚA, judetul Harghita, România", "sat Magherus, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Moglănești", "sat", createAdresaCompleta("sat Moglănești, municipiul TOPLIȚA, judetul Harghita, România", "sat Moglanesti, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Secu", "sat", createAdresaCompleta("sat Secu, municipiul TOPLIȚA, judetul Harghita, România", "sat Secu, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Vale", "sat", createAdresaCompleta("sat Vale, municipiul TOPLIȚA, judetul Harghita, România", "sat Vale, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Vâgani", "sat", createAdresaCompleta("sat Vâgani, municipiul TOPLIȚA, judetul Harghita, România", "sat Vagani, municipiul TOPLITA, judetul Harghita, Romania")),
+        createLoc("Zencani", "sat", createAdresaCompleta("sat Zencani, municipiul TOPLIȚA, judetul Harghita, România", "sat Zencani, municipiul TOPLITA, judetul Harghita, Romania")),
+    ]),
 ];
 ?>
