24d88994ec4afbea6852a7c972f6478575ce1fcb
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 09:12:17 +0200

    Fix ALBA county - adresaCompleta structural issues
    Fixed issues:
    1. Fixed adresaCompleta for orase inside municipii - removed duplicate "municipiul"
       - ALBA IULIA: "orasul ALBA IULIA, judetul ALBA" (was "orasul ALBA IULIA, municipiul ALBA IULIA, judetul ALBA")
       - AIUD: "orasul AIUD, judetul ALBA" (was "orasul AIUD, municipiul AIUD, judetul ALBA")
       - BLAJ: "orasul BLAJ, judetul ALBA" (was "orasul BLAJ, municipiul BLAJ, judetul ALBA")
       - SEBES: "orasul SEBES, judetul ALBA" (was "orasul SEBES, municipiul SEBES, judetul ALBA")
    
    2. Fixed adresaCompleta for sate in orase inside municipii - removed "municipiul"
       - Example: "sat Ciumbrud, orasul AIUD, judetul ALBA" (was "sat Ciumbrud, orasul AIUD, municipiul AIUD, judetul ALBA")
    
    3. Fixed orase.php:
       - ABRUD: removed duplicate "orasul ABRUD, orasul ABRUD" -> "orasul ABRUD, judetul ALBA"

--- ROMANIA/ALBA/municipii.php
+++ ROMANIA/ALBA/municipii.php
@@ -1,47 +1,45 @@
 <?php
 $municipii =
 [
-                createMunicipiu("ALBA IULIA", [
-                    createLoc("ALBA IULIA", "oras", createAdresaCompleta("orasul ALBA IULIA, municipiul ALBA IULIA, judetul ALBA, România", "orasul ALBA IULIA, municipiul ALBA IULIA, judetul ALBA, Romania")),
-                    createLoc("Bărăbanț", "sat", createAdresaCompleta("sat Bărăbanț, municipiul ALBA IULIA, judetul ALBA, România", "sat Barabant, municipiul ALBA IULIA, judetul ALBA, Romania")),
-                    createLoc("Micești", "sat", createAdresaCompleta("sat Micești, municipiul ALBA IULIA, judetul ALBA, România", "sat Micesti, municipiul ALBA IULIA, judetul ALBA, Romania")),
-                    createLoc("Oarda", "sat", createAdresaCompleta("sat Oarda, municipiul ALBA IULIA, judetul ALBA, România", "sat Oarda, municipiul ALBA IULIA, judetul ALBA, Romania")),
-                    createLoc("Pâclișa", "sat", createAdresaCompleta("sat Pâclișa, municipiul ALBA IULIA, judetul ALBA, România", "sat Paclisa, municipiul ALBA IULIA, judetul ALBA, Romania")),
-                ], createAdresaCompleta("municipiul ALBA IULIA, judetul ALBA, România", "municipiul ALBA IULIA, judetul ALBA, Romania")),
-                createMunicipiu("AIUD", [
-                    createLoc("AIUD", "oras", [
-                        createLoc("Ciumbrud", "sat", createAdresaCompleta("sat Ciumbrud, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Ciumbrud, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                        createLoc("Gârbova de Jos", "sat", createAdresaCompleta("sat Gârbova de Jos, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Garbova de Jos, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                        createLoc("Gârbova de Sus", "sat", createAdresaCompleta("sat Gârbova de Sus, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Garbova de Sus, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                        createLoc("Gârbovița", "sat", createAdresaCompleta("sat Gârbovița, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Garbovita, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                        createLoc("Sâncrai", "sat", createAdresaCompleta("sat Sâncrai, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Sancrai, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                        createLoc("Țifra", "sat", createAdresaCompleta("sat Țifra, orasul AIUD, municipiul AIUD, judetul ALBA, România", "sat Tifra, orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                    ], createAdresaCompleta("orasul AIUD, municipiul AIUD, judetul ALBA, România", "orasul AIUD, municipiul AIUD, judetul ALBA, Romania")),
-                    createLoc("Aiudul de Sus", "sat", createAdresaCompleta("sat Aiudul de Sus, municipiul AIUD, judetul ALBA, România", "sat Aiudul de Sus, municipiul AIUD, judetul ALBA, Romania")),
-                    createLoc("Gâmbaș", "sat", createAdresaCompleta("sat Gâmbaș, municipiul AIUD, judetul ALBA, România", "sat Gambas, municipiul AIUD, judetul ALBA, Romania")),
-                    createLoc("Măgina", "sat", createAdresaCompleta("sat Măgina, municipiul AIUD, judetul ALBA, România", "sat Magina, municipiul AIUD, judetul ALBA, Romania")),
-                    createLoc("Păgida", "sat", createAdresaCompleta("sat Păgida, municipiul AIUD, judetul ALBA, România", "sat Pagida, municipiul AIUD, judetul ALBA, Romania")),
-                ], createAdresaCompleta("municipiul AIUD, judetul ALBA, România", "municipiul AIUD, judetul ALBA, Romania")),
-                createMunicipiu("BLAJ", [
-                    createLoc("BLAJ", "oras", [
-                        createLoc("Mănărade", "sat", createAdresaCompleta("sat Mănărade, orasul BLAJ, municipiul BLAJ, judetul ALBA, România", "sat Manarade, orasul BLAJ, municipiul BLAJ, judetul ALBA, Romania")),
-                        createLoc("Spătac", "sat", createAdresaCompleta("sat Spătac, orasul BLAJ, municipiul BLAJ, judetul ALBA, România", "sat Spatac, orasul BLAJ, municipiul BLAJ, judetul ALBA, Romania")),
-                    ], createAdresaCompleta("orasul BLAJ, municipiul BLAJ, judetul ALBA, România", "orasul BLAJ, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Deleni-Obârșie", "sat", createAdresaCompleta("sat Deleni-Obârșie, municipiul BLAJ, judetul ALBA, România", "sat Deleni-Obarsie, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Flitești", "sat", createAdresaCompleta("sat Flitești, municipiul BLAJ, judetul ALBA, România", "sat Flitesti, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Izvoarele", "sat", createAdresaCompleta("sat Izvoarele, municipiul BLAJ, judetul ALBA, România", "sat Izvoarele, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Petrisat", "sat", createAdresaCompleta("sat Petrisat, municipiul BLAJ, judetul ALBA, România", "sat Petrisat, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Tiur", "sat", createAdresaCompleta("sat Tiur, municipiul BLAJ, judetul ALBA, România", "sat Tiur, municipiul BLAJ, judetul ALBA, Romania")),
-                    createLoc("Veza", "sat", createAdresaCompleta("sat Veza, municipiul BLAJ, judetul ALBA, România", "sat Veza, municipiul BLAJ, judetul ALBA, Romania")),
-                ], createAdresaCompleta("municipiul BLAJ, judetul ALBA, România", "municipiul BLAJ, judetul ALBA, Romania")),
-                createMunicipiu("SEBEȘ", [
-                    createLoc("SEBEȘ", "oras", [
-                        createLoc("Răhău", "sat", createAdresaCompleta("sat Răhău, orasul SEBEȘ, municipiul SEBEȘ, judetul ALBA, România", "sat Rahau, orasul SEBES, municipiul SEBES, judetul ALBA, Romania")),
-                    ], createAdresaCompleta("orasul SEBEȘ, municipiul SEBEȘ, judetul ALBA, România", "orasul SEBES, municipiul SEBES, judetul ALBA, Romania")),
-                    createLoc("Lancrăm", "sat", createAdresaCompleta("sat Lancrăm, municipiul SEBEȘ, judetul ALBA, România", "sat Lancram, municipiul SEBES, judetul ALBA, Romania")),
-                    createLoc("Petrești", "sat", createAdresaCompleta("sat Petrești, municipiul SEBEȘ, judetul ALBA, România", "sat Petresti, municipiul SEBES, judetul ALBA, Romania")),
-                ], createAdresaCompleta("municipiul SEBEȘ, judetul ALBA, România", "municipiul SEBES, judetul ALBA, Romania")),
-                
-            ];
-
+    createMunicipiu("ALBA IULIA", [
+        createLoc("ALBA IULIA", "oras", createAdresaCompleta("orasul ALBA IULIA, judetul ALBA, România", "orasul ALBA IULIA, judetul ALBA, Romania")),
+        createLoc("Bărăbanț", "sat", createAdresaCompleta("sat Bărăbanț, municipiul ALBA IULIA, judetul ALBA, România", "sat Barabant, municipiul ALBA IULIA, judetul ALBA, Romania")),
+        createLoc("Micești", "sat", createAdresaCompleta("sat Micești, municipiul ALBA IULIA, judetul ALBA, România", "sat Micesti, municipiul ALBA IULIA, judetul ALBA, Romania")),
+        createLoc("Oarda", "sat", createAdresaCompleta("sat Oarda, municipiul ALBA IULIA, judetul ALBA, România", "sat Oarda, municipiul ALBA IULIA, judetul ALBA, Romania")),
+        createLoc("Pâclișa", "sat", createAdresaCompleta("sat Pâclișa, municipiul ALBA IULIA, judetul ALBA, România", "sat Paclisa, municipiul ALBA IULIA, judetul ALBA, Romania")),
+    ], createAdresaCompleta("municipiul ALBA IULIA, judetul ALBA, România", "municipiul ALBA IULIA, judetul ALBA, Romania")),
+    createMunicipiu("AIUD", [
+        createLoc("AIUD", "oras", [
+            createLoc("Ciumbrud", "sat", createAdresaCompleta("sat Ciumbrud, orasul AIUD, judetul ALBA, România", "sat Ciumbrud, orasul AIUD, judetul ALBA, Romania")),
+            createLoc("Gârbova de Jos", "sat", createAdresaCompleta("sat Gârbova de Jos, orasul AIUD, judetul ALBA, România", "sat Garbova de Jos, orasul AIUD, judetul ALBA, Romania")),
+            createLoc("Gârbova de Sus", "sat", createAdresaCompleta("sat Gârbova de Sus, orasul AIUD, judetul ALBA, România", "sat Garbova de Sus, orasul AIUD, judetul ALBA, Romania")),
+            createLoc("Gârbovița", "sat", createAdresaCompleta("sat Gârbovița, orasul AIUD, judetul ALBA, România", "sat Garbovita, orasul AIUD, judetul ALBA, Romania")),
+            createLoc("Sâncrai", "sat", createAdresaCompleta("sat Sâncrai, orasul AIUD, judetul ALBA, România", "sat Sancrai, orasul AIUD, judetul ALBA, Romania")),
+            createLoc("Țifra", "sat", createAdresaCompleta("sat Țifra, orasul AIUD, judetul ALBA, România", "sat Tifra, orasul AIUD, judetul ALBA, Romania")),
+        ], createAdresaCompleta("orasul AIUD, judetul ALBA, România", "orasul AIUD, judetul ALBA, Romania")),
+        createLoc("Aiudul de Sus", "sat", createAdresaCompleta("sat Aiudul de Sus, municipiul AIUD, judetul ALBA, România", "sat Aiudul de Sus, municipiul AIUD, judetul ALBA, Romania")),
+        createLoc("Gâmbaș", "sat", createAdresaCompleta("sat Gâmbaș, municipiul AIUD, judetul ALBA, România", "sat Gambas, municipiul AIUD, judetul ALBA, Romania")),
+        createLoc("Măgina", "sat", createAdresaCompleta("sat Măgina, municipiul AIUD, judetul ALBA, România", "sat Magina, municipiul AIUD, judetul ALBA, Romania")),
+        createLoc("Păgida", "sat", createAdresaCompleta("sat Păgida, municipiul AIUD, judetul ALBA, România", "sat Pagida, municipiul AIUD, judetul ALBA, Romania")),
+    ], createAdresaCompleta("municipiul AIUD, judetul ALBA, România", "municipiul AIUD, judetul ALBA, Romania")),
+    createMunicipiu("BLAJ", [
+        createLoc("BLAJ", "oras", [
+            createLoc("Mănărade", "sat", createAdresaCompleta("sat Mănărade, orasul BLAJ, judetul ALBA, România", "sat Manarade, orasul BLAJ, judetul ALBA, Romania")),
+            createLoc("Spătac", "sat", createAdresaCompleta("sat Spătac, orasul BLAJ, judetul ALBA, România", "sat Spatac, orasul BLAJ, judetul ALBA, Romania")),
+        ], createAdresaCompleta("orasul BLAJ, judetul ALBA, România", "orasul BLAJ, judetul ALBA, Romania")),
+        createLoc("Deleni-Obârșie", "sat", createAdresaCompleta("sat Deleni-Obârșie, municipiul BLAJ, judetul ALBA, România", "sat Deleni-Obarsie, municipiul BLAJ, judetul ALBA, Romania")),
+        createLoc("Flitești", "sat", createAdresaCompleta("sat Flitești, municipiul BLAJ, judetul ALBA, România", "sat Flitesti, municipiul BLAJ, judetul ALBA, Romania")),
+        createLoc("Izvoarele", "sat", createAdresaCompleta("sat Izvoarele, municipiul BLAJ, judetul ALBA, România", "sat Izvoarele, municipiul BLAJ, judetul ALBA, Romania")),
+        createLoc("Petrisat", "sat", createAdresaCompleta("sat Petrisat, municipiul BLAJ, judetul ALBA, România", "sat Petrisat, municipiul BLAJ, judetul ALBA, Romania")),
+        createLoc("Tiur", "sat", createAdresaCompleta("sat Tiur, municipiul BLAJ, judetul ALBA, România", "sat Tiur, municipiul BLAJ, judetul ALBA, Romania")),
+        createLoc("Veza", "sat", createAdresaCompleta("sat Veza, municipiul BLAJ, judetul ALBA, România", "sat Veza, municipiul BLAJ, judetul ALBA, Romania")),
+    ], createAdresaCompleta("municipiul BLAJ, judetul ALBA, România", "municipiul BLAJ, judetul ALBA, Romania")),
+    createMunicipiu("SEBEȘ", [
+        createLoc("SEBEȘ", "oras", [
+            createLoc("Răhău", "sat", createAdresaCompleta("sat Răhău, orasul SEBEȘ, judetul ALBA, România", "sat Rahau, orasul SEBES, judetul ALBA, Romania")),
+        ], createAdresaCompleta("orasul SEBEȘ, judetul ALBA, România", "orasul SEBES, judetul ALBA, Romania")),
+        createLoc("Lancrăm", "sat", createAdresaCompleta("sat Lancrăm, municipiul SEBEȘ, judetul ALBA, România", "sat Lancram, municipiul SEBES, judetul ALBA, Romania")),
+        createLoc("Petrești", "sat", createAdresaCompleta("sat Petrești, municipiul SEBEȘ, judetul ALBA, România", "sat Petresti, municipiul SEBES, judetul ALBA, Romania")),
+    ], createAdresaCompleta("municipiul SEBEȘ, judetul ALBA, România", "municipiul SEBES, judetul ALBA, Romania")),
+];
 ?>
