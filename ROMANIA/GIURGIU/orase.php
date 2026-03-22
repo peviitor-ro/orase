7fbce4bd85c9f0beca1d081383b942325e7ee871
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 08:53:57 +0200

    Fix GIURGIU and HARGHITA orase.php - add adresaCompleta for sate

--- ROMANIA/GIURGIU/orase.php
+++ ROMANIA/GIURGIU/orase.php
@@ -1,20 +1,20 @@
 <?php
 $orase=[
-	        createOras("BOLINTIN-VALE",[
-			    createLoc("BOLINTIN-VALE", "oras",[
-						createLoc("Crivina", "sat"),
-						createLoc("Malu Spart", "sat"),
-				      		createLoc("Suseni ", "sat"),
-				  ]),				  
-			]),
-		createOras("MIHĂILEȘTI",[
-			    createLoc("MIHĂILEȘTI", "oras",[
-						createLoc("Drăgănescu", "sat"),
-						createLoc("Novaci", "sat"),
-				      		createLoc("Popești ", "sat"),
-				  ]),				  
-			]),						  
-			
+    createOras("BOLINTIN-VALE",[
+        createAdresaCompleta("orasul BOLINTIN-VALE, judetul GIURGIU, România", "orasul BOLINTIN-VALE, judetul GIURGIU, Romania"),
+        createLoc("BOLINTIN-VALE", "oras",[
+            createLoc("Crivina", "sat", createAdresaCompleta("sat Crivina, orasul BOLINTIN-VALE, judetul Giurgiu, România", "sat Crivina, orasul BOLINTIN-VALE, judetul Giurgiu, Romania")),
+            createLoc("Malu Spart", "sat", createAdresaCompleta("sat Malu Spart, orasul BOLINTIN-VALE, judetul Giurgiu, România", "sat Malu Spart, orasul BOLINTIN-VALE, judetul Giurgiu, Romania")),
+            createLoc("Suseni", "sat", createAdresaCompleta("sat Suseni, orasul BOLINTIN-VALE, judetul Giurgiu, România", "sat Suseni, orasul BOLINTIN-VALE, judetul Giurgiu, Romania")),
+        ]),
+    ]),
+    createOras("MIHĂILEȘTI",[
+        createAdresaCompleta("orasul MIHĂILEȘTI, judetul GIURGIU, România", "orasul MIHAILESTI, judetul GIURGIU, Romania"),
+        createLoc("MIHĂILEȘTI", "oras",[
+            createLoc("Drăgănescu", "sat", createAdresaCompleta("sat Drăgănescu, orasul MIHĂILEȘTI, judetul Giurgiu, România", "sat Drăgănescu, orasul MIHAILESTI, judetul Giurgiu, Romania")),
+            createLoc("Novaci", "sat", createAdresaCompleta("sat Novaci, orasul MIHĂILEȘTI, judetul Giurgiu, România", "sat Novaci, orasul MIHAILESTI, judetul Giurgiu, Romania")),
+            createLoc("Popești", "sat", createAdresaCompleta("sat Popești, orasul MIHĂILEȘTI, judetul Giurgiu, România", "sat Popesti, orasul MIHAILESTI, judetul Giurgiu, Romania")),
+        ]),
+    ]),
 ];
-
 ?>
--- ROMANIA/HARGHITA/orase.php
+++ ROMANIA/HARGHITA/orase.php
@@ -1,31 +1,34 @@
 <?php
 $orase=[
-		createOras("BĂILE TUȘNAD",[
-			    createLoc("BĂILE TUȘNAD", "oras",[
-				createLoc("Carpitus", "sat"),
-				  ]),				  
-			]),
+    createOras("BĂILE TUȘNAD",[
+        createAdresaCompleta("orasul BĂILE TUȘNAD, judetul HARGHITA, România", "orasul BAILE TUSNAD, judetul HARGHITA, Romania"),
+        createLoc("BĂILE TUȘNAD", "oras",[
+            createLoc("Carpitus", "sat", createAdresaCompleta("sat Carpitus, orasul BĂILE TUȘNAD, judetul Harghita, România", "sat Carpitus, orasul BAILE TUSNAD, judetul Harghita, Romania")),
+        ]),
+    ]),
 
-		createOras("BĂLAN",[
-			    createLoc("BĂLAN", "oras"),				  
-			]),
-		createOras("BORSEC",[
-			    createLoc("BORSEC", "oras"),				  
-			]),
-		createOras("CRISTURU SECUIESC",[
-			    createLoc("CRISTURU SECUIESC", "oras",[
-				createLoc("Betești", "sat"),
-				createLoc("Filiaș", "sat"),
-				  ]),				  
-			]),
+    createOras("BĂLAN",[
+        createAdresaCompleta("orasul BĂLAN, judetul HARGHITA, România", "orasul BALAN, judetul HARGHITA, Romania"),
+        createLoc("BĂLAN", "oras"),
+    ]),
+    createOras("BORSEC",[
+        createAdresaCompleta("orasul BORSEC, judetul HARGHITA, România", "orasul BORSEC, judetul HARGHITA, Romania"),
+        createLoc("BORSEC", "oras"),
+    ]),
+    createOras("CRISTURU SECUIESC",[
+        createAdresaCompleta("orasul CRISTURU SECUIESC, judetul HARGHITA, România", "orasul CRISTURU SECUIESC, judetul HARGHITA, Romania"),
+        createLoc("CRISTURU SECUIESC", "oras",[
+            createLoc("Betești", "sat", createAdresaCompleta("sat Betești, orasul CRISTURU SECUIESC, judetul Harghita, România", "sat Betesti, orasul CRISTURU SECUIESC, judetul Harghita, Romania")),
+            createLoc("Filiaș", "sat", createAdresaCompleta("sat Filiaș, orasul CRISTURU SECUIESC, judetul Harghita, România", "sat Filias, orasul CRISTURU SECUIESC, judetul Harghita, Romania")),
+        ]),
+    ]),
 
-		createOras("VLĂHIȚA",[
-			    createLoc("VLĂHIȚA", "oras",[
-				createLoc("Băile Homorod", "sat"),
-				createLoc("Minele Lueta", "sat"),
-				  ]),				  
-			]),
-			
+    createOras("VLĂHIȚA",[
+        createAdresaCompleta("orasul VLĂHIȚA, judetul HARGHITA, România", "orasul VLAHITA, judetul HARGHITA, Romania"),
+        createLoc("VLĂHIȚA", "oras",[
+            createLoc("Băile Homorod", "sat", createAdresaCompleta("sat Băile Homorod, orasul VLĂHIȚA, judetul Harghita, România", "sat Băile Homorod, orasul VLAHITA, judetul Harghita, Romania")),
+            createLoc("Minele Lueta", "sat", createAdresaCompleta("sat Minele Lueta, orasul VLĂHIȚA, judetul Harghita, România", "sat Minele Lueta, orasul VLAHITA, judetul Harghita, Romania")),
+        ]),
+    ]),
 ];
-
 ?>
