b9e42405c11f091513b6a0d736b9dbab58f1b077
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 09:17:20 +0200

    Fix more counties - remove duplicate municipiul in oras adresaCompleta
    - BOTOSANI: Fix BOTOȘANI and DOROHOI orase
    - DAMBOVITA: Fix TÂRGOVIȘTE and MORENI orase
    - COVASNA: Fix SFÂNTU GHEORGHE and TÂRGU SECUIESC orase
    - CONSTANTA: Fix CONSTANȚA, MANGALIA and MEDGIDIA orase
    - CARAS-SEVERIN: Fix REȘIȚA and CARANSEBEȘ orase

--- ROMANIA/COVASNA/municipii.php
+++ ROMANIA/COVASNA/municipii.php
@@ -2,12 +2,12 @@
 $municipii =
 [
 	 createMunicipiu("SFÂNTU GHEORGHE", [
-		createLoc("SFÂNTU GHEORGHE", "oras", createAdresaCompleta("orasul SFÂNTU GHEORGHE, municipiul SFÂNTU GHEORGHE, judetul COVASNA, România", "orasul SFANTU GHEORGHE, municipiul SFANTU GHEORGHE, judetul COVASNA, Romania")),
+		createLoc("SFÂNTU GHEORGHE", "oras", createAdresaCompleta("orasul SFÂNTU GHEORGHE, judetul COVASNA, România", "orasul SFANTU GHEORGHE, judetul COVASNA, Romania")),
 		createLoc("Chilieni", "sat", createAdresaCompleta("sat Chilieni, municipiul SFÂNTU GHEORGHE, judetul COVASNA, România", "sat Chilieni, municipiul SFANTU GHEORGHE, judetul COVASNA, Romania")),
 		createLoc("Coșeni", "sat", createAdresaCompleta("sat Coșeni, municipiul SFÂNTU GHEORGHE, judetul COVASNA, România", "sat Coseni, municipiul SFANTU GHEORGHE, judetul COVASNA, Romania")),
 	], createAdresaCompleta("municipiul SFÂNTU GHEORGHE, judetul COVASNA, România", "municipiul SFANTU GHEORGHE, judetul COVASNA, Romania")),
 	 createMunicipiu("TÂRGU SECUIESC", [
-		createLoc("TÂRGU SECUIESC", "oras", createAdresaCompleta("orasul TÂRGU SECUIESC, municipiul TÂRGU SECUIESC, judetul COVASNA, România", "orasul TARGU SECUIESC, municipiul TARGU SECUIESC, judetul COVASNA, Romania")),
+		createLoc("TÂRGU SECUIESC", "oras", createAdresaCompleta("orasul TÂRGU SECUIESC, judetul COVASNA, România", "orasul TARGU SECUIESC, judetul COVASNA, Romania")),
 		createLoc("Lunga", "sat", createAdresaCompleta("sat Lunga, municipiul TÂRGU SECUIESC, judetul COVASNA, România", "sat Lunga, municipiul TARGU SECUIESC, judetul COVASNA, Romania")),
 	], createAdresaCompleta("municipiul TÂRGU SECUIESC, judetul COVASNA, România", "municipiul TARGU SECUIESC, judetul COVASNA, Romania")),
 			
