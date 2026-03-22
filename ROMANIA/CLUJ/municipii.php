f3edb559c711fd584d0ff04a215a6afeb2343085
Boga Sebastian <sebitestb@gmail.com>
2026-03-22 09:14:16 +0200

    Fix CLUJ - remove duplicate municipiul in oras adresaCompleta

--- ROMANIA/CLUJ/municipii.php
+++ ROMANIA/CLUJ/municipii.php
@@ -1,27 +1,26 @@
 <?php
 $municipii =
 [
-	 createMunicipiu("CLUJ-NAPOCA", [
-		createLoc("CLUJ-NAPOCA", "oras", createAdresaCompleta("orasul CLUJ-NAPOCA, municipiul CLUJ-NAPOCA, judetul CLUJ, România", "orasul CLUJ-NAPOCA, municipiul CLUJ-NAPOCA, judetul CLUJ, Romania")),
-	], createAdresaCompleta("municipiul CLUJ-NAPOCA, judetul CLUJ, România", "municipiul CLUJ-NAPOCA, judetul CLUJ, Romania")),
-	 createMunicipiu("CÂMPIA TURZII", [
-		createLoc("CÂMPIA TURZII", "oras", createAdresaCompleta("orasul CÂMPIA TURZII, municipiul CÂMPIA TURZII, judetul CLUJ, România", "orasul CAMPIA TURZII, municipiul CAMPIA TURZII, judetul CLUJ, Romania")),
-	], createAdresaCompleta("municipiul CÂMPIA TURZII, judetul CLUJ, România", "municipiul CAMPIA TURZII, judetul CLUJ, Romania")),
-	 createMunicipiu("DEJ", [
-		createLoc("DEJ", "oras", createAdresaCompleta("orasul DEJ, municipiul DEJ, judetul CLUJ, România", "orasul DEJ, municipiul DEJ, judetul CLUJ, Romania")),
-		createLoc("Peștera", "sat", createAdresaCompleta("sat Peștera, municipiul DEJ, judetul CLUJ, România", "sat Pestera, municipiul DEJ, judetul CLUJ, Romania")),
-		createLoc("Pintic", "sat", createAdresaCompleta("sat Pintic, municipiul DEJ, judetul CLUJ, România", "sat Pintic, municipiul DEJ, judetul CLUJ, Romania")),
-		createLoc("Șomcutu Mic", "sat", createAdresaCompleta("sat Șomcutu Mic, municipiul DEJ, judetul CLUJ, România", "sat Somcutu Mic, municipiul DEJ, judetul CLUJ, Romania")),
-	], createAdresaCompleta("municipiul DEJ, judetul CLUJ, România", "municipiul DEJ, judetul CLUJ, Romania")),
-	 createMunicipiu("GHERLA", [
-		createLoc("GHERLA", "oras", createAdresaCompleta("orasul GHERLA, municipiul GHERLA, judetul CLUJ, România", "orasul GHERLA, municipiul GHERLA, judetul CLUJ, Romania")),
-		createLoc("Băița", "sat", createAdresaCompleta("sat Băița, municipiul GHERLA, judetul CLUJ, România", "sat Baita, municipiul GHERLA, judetul CLUJ, Romania")),
-		createLoc("Hășdate", "sat", createAdresaCompleta("sat Hășdate, municipiul GHERLA, judetul CLUJ, România", "sat Hasdate, municipiul GHERLA, judetul CLUJ, Romania")),
-		createLoc("Silivaș", "sat", createAdresaCompleta("sat Silivaș, municipiul GHERLA, judetul CLUJ, România", "sat Silivas, municipiul GHERLA, judetul CLUJ, Romania")),
-	], createAdresaCompleta("municipiul GHERLA, judetul CLUJ, România", "municipiul GHERLA, judetul CLUJ, Romania")),
-	 createMunicipiu("TURDA", [
-		createLoc("TURDA", "oras", createAdresaCompleta("orasul TURDA, municipiul TURDA, judetul CLUJ, România", "orasul TURDA, municipiul TURDA, judetul CLUJ, Romania")),
-	], createAdresaCompleta("municipiul TURDA, judetul CLUJ, România", "municipiul TURDA, judetul CLUJ, Romania")),
-			
+    createMunicipiu("CLUJ-NAPOCA", [
+        createLoc("CLUJ-NAPOCA", "oras", createAdresaCompleta("orasul CLUJ-NAPOCA, judetul CLUJ, România", "orasul CLUJ-NAPOCA, judetul CLUJ, Romania")),
+    ], createAdresaCompleta("municipiul CLUJ-NAPOCA, judetul CLUJ, România", "municipiul CLUJ-NAPOCA, judetul CLUJ, Romania")),
+    createMunicipiu("CÂMPIA TURZII", [
+        createLoc("CÂMPIA TURZII", "oras", createAdresaCompleta("orasul CÂMPIA TURZII, judetul CLUJ, România", "orasul CAMPIA TURZII, judetul CLUJ, Romania")),
+    ], createAdresaCompleta("municipiul CÂMPIA TURZII, judetul CLUJ, România", "municipiul CAMPIA TURZII, judetul CLUJ, Romania")),
+    createMunicipiu("DEJ", [
+        createLoc("DEJ", "oras", createAdresaCompleta("orasul DEJ, judetul CLUJ, România", "orasul DEJ, judetul CLUJ, Romania")),
+        createLoc("Peștera", "sat", createAdresaCompleta("sat Peștera, municipiul DEJ, judetul CLUJ, România", "sat Pestera, municipiul DEJ, judetul CLUJ, Romania")),
+        createLoc("Pintic", "sat", createAdresaCompleta("sat Pintic, municipiul DEJ, judetul CLUJ, România", "sat Pintic, municipiul DEJ, judetul CLUJ, Romania")),
+        createLoc("Șomcutu Mic", "sat", createAdresaCompleta("sat Șomcutu Mic, municipiul DEJ, judetul CLUJ, România", "sat Somcutu Mic, municipiul DEJ, judetul CLUJ, Romania")),
+    ], createAdresaCompleta("municipiul DEJ, judetul CLUJ, România", "municipiul DEJ, judetul CLUJ, Romania")),
+    createMunicipiu("GHERLA", [
+        createLoc("GHERLA", "oras", createAdresaCompleta("orasul GHERLA, judetul CLUJ, România", "orasul GHERLA, judetul CLUJ, Romania")),
+        createLoc("Băița", "sat", createAdresaCompleta("sat Băița, municipiul GHERLA, judetul CLUJ, România", "sat Baita, municipiul GHERLA, judetul CLUJ, Romania")),
+        createLoc("Hășdate", "sat", createAdresaCompleta("sat Hășdate, municipiul GHERLA, judetul CLUJ, România", "sat Hasdate, municipiul GHERLA, judetul CLUJ, Romania")),
+        createLoc("Silivaș", "sat", createAdresaCompleta("sat Silivaș, municipiul GHERLA, judetul CLUJ, România", "sat Silivas, municipiul GHERLA, judetul CLUJ, Romania")),
+    ], createAdresaCompleta("municipiul GHERLA, judetul CLUJ, România", "municipiul GHERLA, judetul CLUJ, Romania")),
+    createMunicipiu("TURDA", [
+        createLoc("TURDA", "oras", createAdresaCompleta("orasul TURDA, judetul CLUJ, România", "orasul TURDA, judetul CLUJ, Romania")),
+    ], createAdresaCompleta("municipiul TURDA, judetul CLUJ, România", "municipiul TURDA, judetul CLUJ, Romania")),
 ];
 ?>
