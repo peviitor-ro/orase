270838bbad3fff0ac38605d7c9b62d96ad417835
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-22 08:39:12 +0200

    Fix GALATI county - add adresaCompleta for all sate (#360)
    - Fix adresaCompleta in municipii.php and orase.php
    - Fix adresaCompleta in comune.php
    - Add GALATI to E2E workflow

--- ROMANIA/GALATI/municipii.php
+++ ROMANIA/GALATI/municipii.php
@@ -1,16 +1,13 @@
 <?php
 $municipii =
 [
-		createMunicipiu("GALAȚI", [
-				createAdresaCompleta("municipiul GALAȚI, judetul GALATI, România", "municipiul GALATI, judetul GALATI, Romania"),
-                    createLoc("GALAȚI", "oras",[
-							]),
-                ]),		
-		createMunicipiu("TECUCI", [
-				createAdresaCompleta("municipiul GALAȚI, judetul GALATI, România", "municipiul GALATI, judetul GALATI, Romania"),
-                    createLoc("TECUCI", "oras",[
-							]),
-                ]),	
-			
+    createMunicipiu("GALAȚI", [
+        createAdresaCompleta("municipiul GALAȚI, judetul GALATI, România", "municipiul GALATI, judetul GALATI, Romania"),
+        createLoc("GALAȚI", "oras"),
+    ]),		
+    createMunicipiu("TECUCI", [
+        createAdresaCompleta("municipiul TECUCI, judetul GALATI, România", "municipiul TECUCI, judetul GALATI, Romania"),
+        createLoc("TECUCI", "oras"),
+    ]),			
 ];
 ?>
