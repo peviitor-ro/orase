d716c789bf76cb2760491766a7da0c206ef16ca6
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-22 09:05:53 +0200

    Fix IALOMITA and HUNEDOARA counties (#369)
    IALOMITA:
    - Fix adresaCompleta for all municipii (SLOBOZIA, FETESTI, URZICENI were copy-pasted)
    - Add adresaCompleta for all sate in municipii
    - Fix adresaCompleta for all orase (CAZANESTI, FIERBINTI-TARG, TANDAREI were copy-pasted from AMARA)
    - Add adresaCompleta for all sate in orase
    - Fix adresaCompleta for all 54 comune (all showed 'comuna Adancata' instead of their own name)
    
    HUNEDOARA:
    - Fix typo: ArchIA -> Archia in DEVA municipiu

--- ROMANIA/IALOMITA/municipii.php
+++ ROMANIA/IALOMITA/municipii.php
@@ -1,26 +1,25 @@
 <?php
 $municipii =
 [
-		createMunicipiu("SLOBOZIA", [
-				createAdresaCompleta("municipiul SLOBOZIA, judetul IALOMITA, România", "municipiul SLOBOZIA, judetul IALOMITA, Romania"),
-                    createLoc("SLOBOZIA", "oras"),
-					createLoc("Bora", "sat"),
-					createLoc("Slobozia Nouă", "sat"),
-					
-                ]),	
-		createMunicipiu("FETEȘTI", [
-				createAdresaCompleta("municipiul SLOBOZIA, judetul IALOMITA, România", "municipiul SLOBOZIA, judetul IALOMITA, Romania"),
-                    createLoc("FETEȘTI", "oras"),
-					createLoc("Buliga", "sat"),
-					createLoc("Fetești-Gară", "sat"),
-					createLoc("Vlașca", "sat"),
-					
-                ]),	
-		createMunicipiu("URZICENI", [
-				createAdresaCompleta("municipiul SLOBOZIA, judetul IALOMITA, România", "municipiul SLOBOZIA, judetul IALOMITA, Romania"),
-                    createLoc("URZICENI", "oras"),
-					
-                ]),					
-			
+    createMunicipiu("SLOBOZIA", [
+        createAdresaCompleta("municipiul SLOBOZIA, judetul IALOMITA, România", "municipiul SLOBOZIA, judetul IALOMITA, Romania"),
+        createLoc("SLOBOZIA", "oras"),
+        createLoc("Bora", "sat", createAdresaCompleta("sat Bora, municipiul SLOBOZIA, judetul Ialomita, România", "sat Bora, municipiul SLOBOZIA, judetul Ialomita, Romania")),
+        createLoc("Slobozia Nouă", "sat", createAdresaCompleta("sat Slobozia Nouă, municipiul SLOBOZIA, judetul Ialomita, România", "sat Slobozia Noua, municipiul SLOBOZIA, judetul Ialomita, Romania")),
+    ]),	
+
+    createMunicipiu("FETEȘTI", [
+        createAdresaCompleta("municipiul FETEȘTI, judetul IALOMITA, România", "municipiul FETESTI, judetul IALOMITA, Romania"),
+        createLoc("FETEȘTI", "oras"),
+        createLoc("Buliga", "sat", createAdresaCompleta("sat Buliga, municipiul FETEȘTI, judetul Ialomita, România", "sat Buliga, municipiul FETESTI, judetul Ialomita, Romania")),
+        createLoc("Fetești-Gară", "sat", createAdresaCompleta("sat Fetești-Gară, municipiul FETEȘTI, judetul Ialomita, România", "sat Fetesti-Gara, municipiul FETESTI, judetul Ialomita, Romania")),
+        createLoc("Vlașca", "sat", createAdresaCompleta("sat Vlașca, municipiul FETEȘTI, judetul Ialomita, România", "sat Vlasca, municipiul FETESTI, judetul Ialomita, Romania")),
+    ]),	
+
+    createMunicipiu("URZICENI", [
+        createAdresaCompleta("municipiul URZICENI, judetul IALOMITA, România", "municipiul URZICENI, judetul IALOMITA, Romania"),
+        createLoc("URZICENI", "oras"),
+    ]),					
+    
 ];
 ?>
--- ROMANIA/IALOMITA/orase.php
+++ ROMANIA/IALOMITA/orase.php
@@ -1,31 +1,29 @@
 <?php
 $orase = [
     createOras("AMARA", [
-				createAdresaCompleta("orasul AMARA, judetul IALOMITA, România", "orasul AMARA, judetul IALOMITA, Romania"),
+        createAdresaCompleta("orasul AMARA, judetul IALOMITA, România", "orasul AMARA, judetul IALOMITA, Romania"),
         createLoc("AMARA", "oras", [
-            createLoc("Amara Nouă", "sat"),
+            createLoc("Amara Nouă", "sat", createAdresaCompleta("sat Amara Nouă, orasul AMARA, judetul Ialomita, România", "sat Amara Noua, orasul AMARA, judetul Ialomita, Romania")),
         ]),
     ]),
 
     createOras("CĂZĂNEȘTI", [
-				createAdresaCompleta("orasul AMARA, judetul IALOMITA, România", "orasul AMARA, judetul IALOMITA, Romania"),
+        createAdresaCompleta("orasul CĂZĂNEȘTI, judetul IALOMITA, România", "orasul CAZANESTI, judetul IALOMITA, Romania"),
         createLoc("CĂZĂNEȘTI", "oras"),
     ]),
 
     createOras("FIERBINȚI-TÂRG", [
-				createAdresaCompleta("orasul AMARA, judetul IALOMITA, România", "orasul AMARA, judetul IALOMITA, Romania"),
+        createAdresaCompleta("orasul FIERBINȚI-TÂRG, judetul IALOMITA, România", "orasul FIERBINTI-TARG, judetul IALOMITA, Romania"),
         createLoc("FIERBINȚI-TÂRG", "oras", [
-            createLoc("Fierbinții de Jos", "sat"),
-            createLoc("Fierbinții de Sus", "sat"),
-            createLoc("Grecii de Jos", "sat"),
+            createLoc("Fierbinții de Jos", "sat", createAdresaCompleta("sat Fierbinții de Jos, orasul FIERBINȚI-TÂRG, judetul Ialomita, România", "sat Fierbintii de Jos, orasul FIERBINTI-TARG, judetul Ialomita, Romania")),
+            createLoc("Fierbinții de Sus", "sat", createAdresaCompleta("sat Fierbinții de Sus, orasul FIERBINȚI-TÂRG, judetul Ialomita, România", "sat Fierbintii de Sus, orasul FIERBINTI-TARG, judetul Ialomita, Romania")),
+            createLoc("Grecii de Jos", "sat", createAdresaCompleta("sat Grecii de Jos, orasul FIERBINȚI-TÂRG, judetul Ialomita, România", "sat Grecii de Jos, orasul FIERBINTI-TARG, judetul Ialomita, Romania")),
         ]),
     ]),
 
     createOras("ȚĂNDĂREI", [
-				createAdresaCompleta("orasul AMARA, judetul IALOMITA, România", "orasul AMARA, judetul IALOMITA, Romania"),
+        createAdresaCompleta("orasul ȚĂNDĂREI, judetul IALOMITA, România", "orasul TANDAREI, judetul IALOMITA, Romania"),
         createLoc("ȚĂNDĂREI", "oras"),
     ]),
 ];
-
-
 ?>
