c96e7f1f578fce60ad490df4743b48ee3b20e0ff
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-22 08:29:13 +0200

    Fix BUZAU county - add adresaCompleta for all sate (#352)
    - Add adresaCompleta to all sate in comune.php
    - Add BUZAU test to E2E workflow

--- ROMANIA/BUZAU/municipii.php
+++ ROMANIA/BUZAU/municipii.php
@@ -2,13 +2,13 @@
 $municipii =
 [
 		createMunicipiu("BUZĂU", [
-                    createAdresaCompleta("municipiul BUZĂU, judetul BUZAU, România", "municipiul BUZAU, judetul BUZAU, Romania"),
-                    createLoc("BUZĂU", "oras"),
-                ]),	
+            createAdresaCompleta("municipiul BUZĂU, judetul BUZAU, România", "municipiul BUZAU, judetul BUZAU, Romania"),
+            createLoc("BUZĂU", "oras"),
+        ]),	
 		createMunicipiu("RÂMNICU SĂRAT", [
-                    createAdresaCompleta("municipiul BUZĂU, judetul BUZAU, România", "municipiul BUZAU, judetul BUZAU, Romania"),
-                    createLoc("RÂMNICU SĂRAT", "oras"),
-                ]),					
+            createAdresaCompleta("municipiul RÂMNICU SĂRAT, judetul BUZAU, România", "municipiul RAMNICU SARAT, judetul BUZAU, Romania"),
+            createLoc("RÂMNICU SĂRAT", "oras"),
+        ]),					
 			
 ];
 ?>
