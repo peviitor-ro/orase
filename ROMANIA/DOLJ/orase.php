ac0e9128501b5ff790d8734615af62ff7a15033c
Boga Sebastian Nicolae <sebitestb@gmail.com>
2026-03-22 08:37:56 +0200

    Fix DOLJ county - add adresaCompleta for all sate (#359)
    - Fix adresaCompleta in municipii.php and orase.php
    - Add adresaCompleta to all sate in comune.php
    - Add DOLJ to E2E workflow

--- ROMANIA/DOLJ/orase.php
+++ ROMANIA/DOLJ/orase.php
@@ -1,33 +1,29 @@
 <?php
 $orase=[
-
-			createOras("BECHET", [
-				createAdresaCompleta("orasul BECHET, judetul DOLJ, România", "orasul BECHET, judetul DOLJ, Romania"),
-			    createLoc("BECHET", "oras"),
-			                     ]),
-	
-			createOras("DĂBULENI", [
-				createAdresaCompleta("orasul BECHET, judetul DOLJ, România", "orasul BECHET, judetul DOLJ, Romania"),
-			    createLoc("DĂBULENI", "oras",[
-					createLoc("Chiașu", "sat"),
-							]),
-			                     ]),
-			createOras("FILIAȘI", [
-				createAdresaCompleta("orasul BECHET, judetul DOLJ, România", "orasul BECHET, judetul DOLJ, Romania"),
-			    createLoc("FILIAȘI", "oras",[
-					createLoc("Almăjel", "sat"),
-				        createLoc("Bâlta", "sat"),
-				      	createLoc("Braniște", "sat"),
-				      	createLoc("Fratoștița", "sat"),
-				      	createLoc("Răcarii de Sus", "sat"),
-				      	createLoc("Uscăci", "sat"),
-							]),
-			                     ]),
-			createOras("ȘEGARCEA", [
-				createAdresaCompleta("orasul BECHET, judetul DOLJ, România", "orasul BECHET, judetul DOLJ, Romania"),
-			    createLoc("ȘEGARCEA", "oras"),
-			                     ]),
-			
+    createOras("BECHET", [
+        createAdresaCompleta("orasul BECHET, judetul DOLJ, România", "orasul BECHET, judetul DOLJ, Romania"),
+        createLoc("BECHET", "oras"),
+    ]),	
+    createOras("DĂBULENI", [
+        createAdresaCompleta("orasul DĂBULENI, judetul DOLJ, România", "orasul DABULENI, judetul DOLJ, Romania"),
+        createLoc("DĂBULENI", "oras", [
+            createLoc("Chiașu", "sat", createAdresaCompleta("sat Chiașu, orasul DĂBULENI, judetul Dolj, România", "sat Chiasu, orasul DABULENI, judetul Dolj, Romania")),
+        ]),
+    ]),
+    createOras("FILIAȘI", [
+        createAdresaCompleta("orasul FILIAȘI, judetul DOLJ, România", "orasul FILIASI, judetul DOLJ, Romania"),
+        createLoc("FILIAȘI", "oras", [
+            createLoc("Almăjel", "sat", createAdresaCompleta("sat Almăjel, orasul FILIAȘI, judetul Dolj, România", "sat Almajel, orasul FILIASI, judetul Dolj, Romania")),
+            createLoc("Bâlta", "sat", createAdresaCompleta("sat Bâlta, orasul FILIAȘI, judetul Dolj, România", "sat Balta, orasul FILIASI, judetul Dolj, Romania")),
+            createLoc("Braniște", "sat", createAdresaCompleta("sat Braniște, orasul FILIAȘI, judetul Dolj, România", "sat Braniste, orasul FILIASI, judetul Dolj, Romania")),
+            createLoc("Fratoștița", "sat", createAdresaCompleta("sat Fratoștița, orasul FILIAȘI, judetul Dolj, România", "sat Fratostita, orasul FILIASI, judetul Dolj, Romania")),
+            createLoc("Răcarii de Sus", "sat", createAdresaCompleta("sat Răcarii de Sus, orasul FILIAȘI, judetul Dolj, România", "sat Racarii de Sus, orasul FILIASI, judetul Dolj, Romania")),
+            createLoc("Uscăci", "sat", createAdresaCompleta("sat Uscăci, orasul FILIAȘI, judetul Dolj, România", "sat Uscaaci, orasul FILIASI, judetul Dolj, Romania")),
+        ]),
+    ]),
+    createOras("ȘEGARCEA", [
+        createAdresaCompleta("orasul ȘEGARCEA, judetul DOLJ, România", "orasul SEGARCEA, judetul DOLJ, Romania"),
+        createLoc("ȘEGARCEA", "oras"),
+    ]),			
 ];
-
 ?>
