92e528d8e043650e5c7b1b417636f274a51de8aa
Boga Sebastian <sebitestb@gmail.com>
2026-03-23 10:38:08 +0200

    Fix ARGES orase - add adresaCompleta at top level

--- ROMANIA/ARGES/orase.php
+++ ROMANIA/ARGES/orase.php
@@ -4,12 +4,12 @@ $orase=[
             createLoc("COSTEȘTI", "oras", [
                 createLoc("Broșteni", "sat", createAdresaCompleta("sat Broșteni, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Brosteni, orasul COSTESTI, judetul ARGES, Romania")),
                 createLoc("Lăceni", "sat", createAdresaCompleta("sat Lăceni, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Laceni, orasul COSTESTI, judetul ARGES, Romania")),
-                createLoc("Pârvu Roșu", "sat", createAdresaCompleta("sat Pârvu Roșu, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Parvu Rosu, orasul COSTESTI, judetul ARGES, Romania")),
+                createLoc("Pârvu Roșu", "sat", createAdresaCompleta("sat pârvu Roșu, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Parvu Rosu, orasul COSTESTI, judetul ARGES, Romania")),
                 createLoc("Podu Broșteni", "sat", createAdresaCompleta("sat Podu Broșteni, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Podu Brosteni, orasul COSTESTI, judetul ARGES, Romania")),
                 createLoc("Smei", "sat", createAdresaCompleta("sat Smei, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Smei, orasul COSTESTI, judetul ARGES, Romania")),
                 createLoc("Stârci", "sat", createAdresaCompleta("sat Stârci, orasul COSTEȘTI, judetul ARGEȘ, România", "sat Starci, orasul COSTESTI, judetul ARGES, Romania")),
             ], createAdresaCompleta("orasul COSTEȘTI, judetul ARGEȘ, România", "orasul COSTESTI, judetul ARGES, Romania")),
-        ]), 
+        ], createAdresaCompleta("orasul COSTEȘTI, judetul ARGEȘ, România", "orasul COSTESTI, judetul ARGES, Romania")), 
         createOras("MIOVENI", [
             createLoc("MIOVENI", "oras", [
                 createLoc("Clucereasa", "sat", createAdresaCompleta("sat Clucereasa, orasul MIOVENI, judetul ARGEȘ, România", "sat Clucereasa, orasul MIOVENI, judetul ARGES, Romania")),
@@ -17,7 +17,7 @@ $orase=[
                 createLoc("Făgetu", "sat", createAdresaCompleta("sat Făgetu, orasul MIOVENI, judetul ARGEȘ, România", "sat Fagetu, orasul MIOVENI, judetul ARGES, Romania")),
                 createLoc("Racovița", "sat", createAdresaCompleta("sat Racovița, orasul MIOVENI, judetul ARGEȘ, România", "sat Racovita, orasul MIOVENI, judetul ARGES, Romania")),
             ], createAdresaCompleta("orasul MIOVENI, judetul ARGEȘ, România", "orasul MIOVENI, judetul ARGES, Romania")),
-        ]),
+        ], createAdresaCompleta("orasul MIOVENI, judetul ARGEȘ, România", "orasul MIOVENI, judetul ARGES, Romania")),
         createOras("ȘTEFĂNEȘTI", [
             createLoc("ȘTEFĂNEȘTI", "oras", [
                 createLoc("Enculești", "sat", createAdresaCompleta("sat Enculești, orasul ȘTEFĂNEȘTI, judetul ARGEȘ, România", "sat Enculesti, orasul STEFANESTI, judetul ARGES, Romania")),
@@ -28,7 +28,7 @@ $orase=[
                 createLoc("Viișoara", "sat", createAdresaCompleta("sat Viișoara, orasul ȘTEFĂNEȘTI, judetul ARGEȘ, România", "sat Viisoara, orasul STEFANESTI, judetul ARGES, Romania")),
                 createLoc("Zăvoi", "sat", createAdresaCompleta("sat Zăvoi, orasul ȘTEFĂNEȘTI, judetul ARGEȘ, România", "sat Zavoi, orasul STEFANESTI, judetul ARGES, Romania")),
             ], createAdresaCompleta("orasul ȘTEFĂNEȘTI, judetul ARGEȘ, România", "orasul STEFANESTI, judetul ARGES, Romania")),
-        ]),
+        ], createAdresaCompleta("orasul ȘTEFĂNEȘTI, judetul ARGEȘ, România", "orasul STEFANESTI, judetul ARGES, Romania")),
         createOras("TOPOLOVENI", [
             createLoc("TOPOLOVENI", "oras", [
                 createLoc("Boțârcani", "sat", createAdresaCompleta("sat Boțârcani, orasul TOPOLOVENI, judetul ARGEȘ, România", "sat Botarcani, orasul TOPOLOVENI, judetul ARGES, Romania")),
@@ -36,7 +36,5 @@ $orase=[
                 createLoc("Gorănești", "sat", createAdresaCompleta("sat Gorănești, orasul TOPOLOVENI, judetul ARGEȘ, România", "sat Goranesti, orasul TOPOLOVENI, judetul ARGES, Romania")),
                 createLoc("Țigănești", "sat", createAdresaCompleta("sat Țigănești, orasul TOPOLOVENI, judetul ARGEȘ, România", "sat Tiganesti, orasul TOPOLOVENI, judetul ARGES, Romania")),
             ], createAdresaCompleta("orasul TOPOLOVENI, judetul ARGEȘ, România", "orasul TOPOLOVENI, judetul ARGES, Romania")),
-        ]),
+        ], createAdresaCompleta("orasul TOPOLOVENI, judetul ARGEȘ, România", "orasul TOPOLOVENI, judetul ARGES, Romania")),
 ];
-
-
