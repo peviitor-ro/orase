0e32a7d1801d7001069d3158783dd69f62ac2a40
Boga Sebastian <sebitestb@gmail.com>
2026-03-25 19:54:04 +0200

    Fix BRAILA: adresaCompleta pattern bug and add test data
    - Fixed municipii.php: moved createAdresaCompleta to 3rd parameter
    - Fixed orase.php: moved createAdresaCompleta to 3rd parameter
    - Added test data: municipii.txt, orase.txt, comune.txt, sate.txt
    - Added run_tests.php and braila-tests.yml workflow

--- ROMANIA/BRAILA/municipii.php
+++ ROMANIA/BRAILA/municipii.php
@@ -1,10 +1,8 @@
 <?php
 $municipii =
 [
-		createMunicipiu("BRĂILA", [
-                    createAdresaCompleta("municipiul BRĂILA, judetul BRAILA, România", "municipiul BRAILA, judetul BRAILA, Romania"),
-                    createLoc("BRĂILA", "oras"),
-                ]),			
-			
+    createMunicipiu("BRĂILA", [
+        createLoc("BRĂILA", "oras", createAdresaCompleta("municipiul BRĂILA, judetul BRAILA, România", "municipiul BRAILA, judetul BRAILA, Romania")),
+    ], createAdresaCompleta("municipiul BRĂILA, judetul BRAILA, România", "municipiul BRAILA, judetul BRAILA, Romania")),
 ];
 ?>
