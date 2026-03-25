5b41b5eced686abd071d4d731b800f8b5c911d09
Boga Sebastian <sebitestb@gmail.com>
2026-03-25 19:46:25 +0200

    Fix BUZAU: Move createAdresaCompleta to 3rd parameter for createMunicipiu, createOras, and createComuna
    This fixes the adresaCompleta field not appearing in API responses for BUZAU county. The createAdresaCompleta was incorrectly placed inside the locuri array instead of as the 3rd parameter.

--- ROMANIA/BUZAU/orase.php
+++ ROMANIA/BUZAU/orase.php
@@ -1,44 +1,40 @@
 <?php
-$orase=[
-            createOras("NEHOIU",[
-			    createAdresaCompleta("orasul NEHOIU, judetul BUZAU, România", "orasul NEHOIU, judetul BUZAU, Romania"),
-			    createLoc("NEHOIU", "oras",[
-					createLoc("Bâsca Rozilei", "sat", createAdresaCompleta("sat Bâsca Rozilei, orasul NEHOIU, judetul Buzau, România", "sat Basca Rozilei, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Chirlești", "sat", createAdresaCompleta("sat Chirlești, orasul NEHOIU, judetul Buzau, România", "sat Chirlesti, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Curmătura", "sat", createAdresaCompleta("sat Curmătura, orasul NEHOIU, judetul Buzau, România", "sat Curmatura, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Lunca Priporului", "sat", createAdresaCompleta("sat Lunca Priporului, orasul NEHOIU, judetul Buzau, România", "sat Lunca Priporului, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Mlăjet", "sat", createAdresaCompleta("sat Mlăjet, orasul NEHOIU, judetul Buzau, România", "sat Mlajet, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Nehoiașu", "sat", createAdresaCompleta("sat Nehoiașu, orasul NEHOIU, judetul Buzau, România", "sat Nehoiasu, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Păltineni", "sat", createAdresaCompleta("sat Păltineni, orasul NEHOIU, judetul Buzau, România", "sat Paltineni, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Stănila", "sat", createAdresaCompleta("sat Stănila, orasul NEHOIU, judetul Buzau, România", "sat Stanila, orasul NEHOIU, judetul Buzau, Romania")),
-					createLoc("Vinețișu", "sat", createAdresaCompleta("sat Vinețișu, orasul NEHOIU, judetul Buzau, România", "sat Vinetisu, orasul NEHOIU, judetul Buzau, Romania")),
-				]),
-			]),
-            createOras("PĂTÂRLAGELE",[
-			    createAdresaCompleta("orasul PĂTÂRLAGELE, judetul BUZAU, România", "orasul PATARLAGELE, judetul BUZAU, Romania"),
-			    createLoc("PĂTÂRLAGELE", "oras",[
-					createLoc("Mușcel", "sat", createAdresaCompleta("sat Mușcel, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Muscel, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Poienile", "sat", createAdresaCompleta("sat Poienile, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Poienile, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Sibiciu de Sus", "sat", createAdresaCompleta("sat Sibiciu de Sus, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Sibiciu de Sus, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Stroești", "sat", createAdresaCompleta("sat Stroești, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Stroesti, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Valea Lupului", "sat", createAdresaCompleta("sat Valea Lupului, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Valea Lupului, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Valea Sibiciului", "sat", createAdresaCompleta("sat Valea Sibiciului, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Valea Sibiciului, orasul PATARLAGELE, judetul Buzau, Romania")),
-					createLoc("Valea Viei", "sat", createAdresaCompleta("sat Valea Viei, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Valea Viei, orasul PATARLAGELE, judetul Buzau, Romania")),
-				]),
-				createLoc("Calea Chiojdului", "sat", createAdresaCompleta("sat Calea Chiojdului, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Calea Chiojdului, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Crâng", "sat", createAdresaCompleta("sat Crâng, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Crang, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Fundăturile", "sat", createAdresaCompleta("sat Fundăturile, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Fundaturile, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Gornet", "sat", createAdresaCompleta("sat Gornet, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Gornet, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Lunca", "sat", createAdresaCompleta("sat Lunca, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Lunca, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Mănăstirea", "sat", createAdresaCompleta("sat Mănăstirea, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Manastirea, orasul PATARLAGELE, judetul Buzau, Romania")),
-				createLoc("Mărunțișu", "sat", createAdresaCompleta("sat Mărunțișu, orasul PĂTÂRLAGELE, judetul Buzau, România", "sat Maruntisu, orasul PATARLAGELE, judetul Buzau, Romania")),
-			]),
-
-            createOras("POGOANELE",[
-			    createAdresaCompleta("orasul POGOANELE, judetul BUZAU, România", "orasul POGOANELE, judetul BUZAU, Romania"),
-			    createLoc("POGOANELE", "oras",[
-					createLoc("Căldărăști", "sat", createAdresaCompleta("sat Căldărăști, orasul POGOANELE, judetul Buzau, România", "sat Caldărasti, orasul POGOANELE, judetul Buzau, Romania")),
-				]),				  
-			]),
+$orase = [
+    createOras("NEHOIU", [
+        createLoc("NEHOIU", "oras", [
+            createLoc("Bâsca Rozilei", "sat", createAdresaCompleta("sat Bâsca Rozilei, orasul NEHOIU, judetul BUZAU, România", "sat Basca Rozilei, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Chirlești", "sat", createAdresaCompleta("sat Chirlești, orasul NEHOIU, judetul BUZAU, România", "sat Chirlesti, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Curmătura", "sat", createAdresaCompleta("sat Curmătura, orasul NEHOIU, judetul BUZAU, România", "sat Curmatura, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Lunca Priporului", "sat", createAdresaCompleta("sat Lunca Priporului, orasul NEHOIU, judetul BUZAU, România", "sat Lunca Priporului, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Mlăjet", "sat", createAdresaCompleta("sat Mlăjet, orasul NEHOIU, judetul BUZAU, România", "sat Mlajet, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Nehoiașu", "sat", createAdresaCompleta("sat Nehoiașu, orasul NEHOIU, judetul BUZAU, România", "sat Nehoiasu, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Păltineni", "sat", createAdresaCompleta("sat Păltineni, orasul NEHOIU, judetul BUZAU, România", "sat Paltineni, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Stănila", "sat", createAdresaCompleta("sat Stănila, orasul NEHOIU, judetul BUZAU, România", "sat Stanila, orasul NEHOIU, judetul BUZAU, Romania")),
+            createLoc("Vinețișu", "sat", createAdresaCompleta("sat Vinețișu, orasul NEHOIU, judetul BUZAU, România", "sat Vinetisu, orasul NEHOIU, judetul BUZAU, Romania")),
+        ], createAdresaCompleta("orasul NEHOIU, judetul BUZAU, România", "orasul NEHOIU, judetul BUZAU, Romania")),
+    ]),
+    createOras("PĂTÂRLAGELE", [
+        createLoc("PĂTÂRLAGELE", "oras", [
+            createLoc("Mușcel", "sat", createAdresaCompleta("sat Mușcel, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Muscel, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Poienile", "sat", createAdresaCompleta("sat Poienile, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Poienile, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Sibiciu de Sus", "sat", createAdresaCompleta("sat Sibiciu de Sus, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Sibiciu de Sus, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Stroești", "sat", createAdresaCompleta("sat Stroești, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Stroesti, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Valea Lupului", "sat", createAdresaCompleta("sat Valea Lupului, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Valea Lupului, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Valea Sibiciului", "sat", createAdresaCompleta("sat Valea Sibiciului, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Valea Sibiciului, orasul PATARLAGELE, judetul BUZAU, Romania")),
+            createLoc("Valea Viei", "sat", createAdresaCompleta("sat Valea Viei, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Valea Viei, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        ], createAdresaCompleta("orasul PĂTÂRLAGELE, judetul BUZAU, România", "orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Calea Chiojdului", "sat", createAdresaCompleta("sat Calea Chiojdului, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Calea Chiojdului, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Crâng", "sat", createAdresaCompleta("sat Crâng, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Crang, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Fundăturile", "sat", createAdresaCompleta("sat Fundăturile, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Fundaturile, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Gornet", "sat", createAdresaCompleta("sat Gornet, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Gornet, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Lunca", "sat", createAdresaCompleta("sat Lunca, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Lunca, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Mănăstirea", "sat", createAdresaCompleta("sat Mănăstirea, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Manastirea, orasul PATARLAGELE, judetul BUZAU, Romania")),
+        createLoc("Mărunțișu", "sat", createAdresaCompleta("sat Mărunțișu, orasul PĂTÂRLAGELE, judetul BUZAU, România", "sat Maruntisu, orasul PATARLAGELE, judetul BUZAU, Romania")),
+    ], createAdresaCompleta("orasul PĂTÂRLAGELE, judetul BUZAU, România", "orasul PATARLAGELE, judetul BUZAU, Romania")),
+    createOras("POGOANELE", [
+        createLoc("POGOANELE", "oras", [
+            createLoc("Căldărăști", "sat", createAdresaCompleta("sat Căldărăști, orasul POGOANELE, judetul BUZAU, România", "sat Caldărasti, orasul POGOANELE, judetul BUZAU, Romania")),
+        ], createAdresaCompleta("orasul POGOANELE, judetul BUZAU, România", "orasul POGOANELE, judetul BUZAU, Romania")),
+    ]),
 ];
 ?>
